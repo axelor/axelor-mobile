04f32794610f6586d4a53774a3b190a6e64fd67d
fsc-axelor <localhost+fsc-axelor@users.noreply.github.com>
2022-04-21 18:33:08 +0200

    Fix test

---
+++ jest/setup.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.useFakeTimers();
