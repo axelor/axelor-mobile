f75027b1279232c730ee86131921225805431fcb
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-04-27 09:06:14 +0200

    add get request for stock locations

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import {authReducer} from '@/modules/auth/features/authSlice';
 import {productReducer} from '@/modules/stock/features/productSlice';
 import {stockCorrectionReducer} from './modules/stock/features/stockCorrectionSlice';
+import {stockLocationReducer} from './modules/stock/features/stockLocationSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
     stockCorrection: stockCorrectionReducer,
+    stockLocation: stockLocationReducer,
   },
 });
