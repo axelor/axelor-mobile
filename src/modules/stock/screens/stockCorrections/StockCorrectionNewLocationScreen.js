6f00150d206b9de0dd2e45b72902ffe0d1528711
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-04-27 10:29:58 +0200

    Change searchBar to autocomplete searchBar

--- src/modules/stock/screens/stockCorrections/StockCorrectionNewLocationScreen.js
+++ src/modules/stock/screens/stockCorrections/StockCorrectionNewLocationScreen.js
@@ -1,15 +1,27 @@
 import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import {Screen, Text} from '@/components/atoms';
-import {SearchBar} from '@/components/molecules';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchStockLocations} from '@/modules/stock/features/stockLocationSlice';
+import {Screen} from '@/components/atoms';
+import {AutocompleteSearch} from '@/components/organisms';
 
 const StockCorrectionNewLocationScreen = () => {
+  const {loadingLocations, stockLocationList} = useSelector(
+    state => state.stockLocation,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchStockLocations());
+  }, [dispatch]);
+
   return (
     <Screen style={styles.container}>
-      <SearchBar
-        style={styles.searchBar}
-        placeholder="Stock Location"
-        onSearchPress={() => {}}
+      <AutocompleteSearch
+        objectList={stockLocationList}
+        searchName="Stock Location"
+        searchParam="name"
+        setValueSearch={() => {}}
       />
     </Screen>
   );
