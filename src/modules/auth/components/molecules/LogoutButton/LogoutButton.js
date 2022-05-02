3da91932b0651f38eb2a8a63eea1c8535037a940
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-05-02 17:06:14 +0200

    Add Logout button component

---
+++ src/modules/auth/components/molecules/LogoutButton/LogoutButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Button} from '@/components/atoms';
+
+const LogoutButton = ({onPress}) => {
+  return (
+    <View style={styles.container}>
+      <Button
+        style={styles.button}
+        styleTxt={styles.title}
+        title="LOGOUT"
+        onPress={onPress}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 15,
+    backgroundColor: '#3ECF8E',
+    borderRadius: 20,
+    width: '30%',
+    elevation: 5,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+});
+
+export default LogoutButton;
