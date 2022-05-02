2652422067a5853a9d115a21038d9d90186a406c
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-05-02 10:46:30 +0200

    Fix stock locations request with type internal only

--- src/modules/stock/api/stock-location-api.js
+++ src/modules/stock/api/stock-location-api.js
@@ -1,5 +1,39 @@
 import axios from 'axios';
 
+TYPE_INTERNAL = 1;
+TYPE_EXTERNAL = 2;
+TYPE_VIRTUAL = 3;
+
 export async function searchStockLocation() {
-  return axios.get('/ws/rest/com.axelor.apps.stock.db.StockLocation/');
+  return axios.post('/ws/rest/com.axelor.apps.stock.db.StockLocation/search', {
+    data: {
+      // Criteria from request /stock.root.products/list/1
+      criteria: [
+        {
+          operator: 'and',
+          criteria: [
+            {
+              fieldName: 'typeSelect',
+              operator: '=',
+              value: TYPE_INTERNAL,
+            },
+            {
+              fieldName: 'isWorkshop',
+              operator: '=',
+              value: false,
+            },
+            {
+              fieldName: 'company.id',
+              operator: '=',
+              value: 1,
+            },
+          ],
+        },
+      ],
+    },
+    fields: ['name', 'id'],
+    sortBy: ['id', 'name'],
+    limit: 20,
+    offset: 0,
+  });
 }
