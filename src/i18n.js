97b72ea60cae7ee15e9cfd93a05eb3dbad4373d4
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-05-23 12:02:13 +0200

    Initialisation of i18n

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18next from 'i18next';
+import enTranslation from './i18n/en/translations.json';
+import frTranslation from './i18n/fr/translations.json';
+
+i18next.init({
+  compatibilityJSON: 'v3',
+  lng: 'en',
+  debug: true,
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+    fr: {
+      translation: frTranslation,
+    },
+  },
+});
+
+export default lng => i18next.getFixedT(lng);
