3ee6e5783de436e3e9f6b4da5cd99eeb8e710144
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2022-08-16 09:16:51 +0200

    fix: modify inventory screens export

--- src/modules/stock/screens/inventories/index.js
+++ src/modules/stock/screens/inventories/index.js
@@ -1,8 +1,7 @@
-export {default as InternalMoveDetailsGeneralScreen} from './InternalMoveDetailsGeneralScreen';
-export {default as InternalMoveLineDetailsScreen} from './InternalMoveLineDetailsScreen';
-export {default as InternalMoveLineListScreen} from './InternalMoveLineListScreen';
-export {default as InternalMoveListScreen} from './InternalMoveListScreen';
-export {default as InternalMoveSelectFromLocationScreen} from './InternalMoveSelectFromLocationScreen';
-export {default as InternalMoveSelectProductScreen} from './InternalMoveSelectProductScreen';
-export {default as InternalMoveSelectToLocationScreen} from './InternalMoveSelectToLocationScreen';
-export {default as InternalMoveSelectTrackingScreen} from './InternalMoveSelectTrackingScreen';
+export {default as InventoryListScreen} from './InventoryListScreen';
+export {default as InventoryPlannedDetailsScreen} from './InventoryPlannedDetailsScreen';
+export {default as InventoryStartedDetailsScreen} from './InventoryStartedDetailsScreen';
+export {default as InventorySelectProductScreen} from './InventorySelectProductScreen';
+export {default as InventorySelectTrackingScreen} from './InventorySelectTrackingScreen';
+export {default as InventoryLineListScreen} from './InventoryLineListScreen';
+export {default as InventoryLineDetailsScreen} from './InventoryLineDetailsScreen';
