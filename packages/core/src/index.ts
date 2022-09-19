aff8066b2406df601aec9013400eb967c7f8f767
fsc-axelor <localhost+fsc-axelor@users.noreply.github.com>
2022-09-19 14:41:35 +0200

    Rename index.ts to Storage.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
 export {default as Application} from './Application';
 export {default as Module} from './Module';
-export {storage, useStorage} from './storage';
+export {storage, useStorage} from './storage/Storage';
