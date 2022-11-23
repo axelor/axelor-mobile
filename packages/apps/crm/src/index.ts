fed4879b7e4bc98e4bf83db331bf8779a1ffe9eb
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2022-11-23 16:38:38 +0100

    feat create crmModule

---
+++ packages/apps/crm/src/index.ts
@@ -0,0 +1,15 @@
+import {Module} from '@aos-mobile/core';
+import enTranslations from './i18n/en.json';
+import frTranslations from './i18n/fr.json';
+
+export const CrmModule: Module = {
+  name: 'Crm',
+  title: 'Crm_Crm',
+  icon: 'users',
+  menus: {},
+  screens: {},
+  translations: {
+    en: enTranslations,
+    fr: frTranslations,
+  },
+};
