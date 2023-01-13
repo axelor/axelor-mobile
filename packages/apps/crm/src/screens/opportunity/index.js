0ca7e70a619186a9ef8f51151ff3390465baf7ff
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-01-13 15:13:12 +0100

    feat: add crm opportunity list screen
    * RM#59232

---
+++ packages/apps/crm/src/screens/opportunity/index.js
@@ -0,0 +1,11 @@
+import OpportunityListScreen from './OpportunityListScreen';
+
+export default {
+  OpportunityListScreen: {
+    title: 'Crm_Opportunities',
+    component: OpportunityListScreen,
+    options: {
+      shadedHeader: false,
+    },
+  },
+};
