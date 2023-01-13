7c42005b343b511607de3614d1295bd82a6e4f69
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-01-13 15:13:12 +0100

    feat: add crm opportunity list screen
    * RM#59232

---
+++ packages/apps/crm/src/components/molecules/OpportunityCard/OpportunityCard.tsx
@@ -0,0 +1,112 @@
+import React, {useMemo} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {
+  Card,
+  Icon,
+  LabelText,
+  Text,
+  useThemeColor,
+  StarScore,
+} from '@aos-mobile/ui';
+import {formatDate, useTranslator} from '@aos-mobile/core';
+import {Opportunity} from '../../../types';
+
+interface OpportunityCardProps {
+  amount: string;
+  expectedCloseDate: string;
+  name: string;
+  opportunityScoring: number;
+  reference: string;
+  allOpportunityStatus?: any;
+  currencySymbol?: string;
+  opportunityStatus?: any;
+  style?: any;
+  onPress: () => void;
+}
+
+const OpportunityCard = ({
+  amount,
+  expectedCloseDate,
+  name,
+  opportunityScoring,
+  reference,
+  allOpportunityStatus,
+  opportunityStatus,
+  currencySymbol = '$',
+  style,
+  onPress,
+}: OpportunityCardProps) => {
+  const Colors = useThemeColor();
+  const I18n = useTranslator();
+
+  const _expectedCloseDate = useMemo(
+    () => formatDate(expectedCloseDate, I18n.t('Base_DateFormat')),
+    [expectedCloseDate, I18n],
+  );
+
+  const borderStyle = useMemo(() => {
+    const colorIndex = allOpportunityStatus?.findIndex(
+      status => status.id === opportunityStatus?.id,
+    );
+    return getStyles(Opportunity.getStatusColor(colorIndex, Colors)?.background)
+      ?.border;
+  }, [Colors, allOpportunityStatus, opportunityStatus?.id]);
+
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
+      <Card style={[styles.container, borderStyle, style]}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.txtImportant}>{reference}</Text>
+          <Text style={styles.txtImportant}>{name}</Text>
+          <LabelText
+            title={I18n.t('Crm_Opportunity_Amount')}
+            value={`${amount} ${currencySymbol}`}
+          />
+          <LabelText
+            iconName="clock"
+            title={I18n.t('Crm_Opportunity_ExpectedCloseDate')}
+            value={_expectedCloseDate}
+          />
+        </View>
+        <View>
+          <StarScore score={opportunityScoring} showMissingStar={true} />
+          <Icon
+            name="chevron-right"
+            color={Colors.secondaryColor.background_light}
+            size={20}
+            style={styles.icon}
+          />
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+const getStyles = color =>
+  StyleSheet.create({
+    border: {borderLeftWidth: 7, borderLeftColor: color},
+  });
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingRight: 15,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingRight: 10,
+  },
+  icon: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingRight: 5,
+  },
+  txtImportant: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default OpportunityCard;
