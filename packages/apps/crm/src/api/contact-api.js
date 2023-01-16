2048239fceb71beb2d9d36dd0384d9af7ef9042a
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-01-16 12:12:37 +0100

    feat: add contactListScreen
    * RM#58980

--- packages/apps/crm/src/api/contact-api.js
+++ packages/apps/crm/src/api/contact-api.js
@@ -1,6 +1,14 @@
 import {axiosApiProvider} from '@axelor/aos-mobile-core';
 
-const contactFields = ['simpleFullName', 'fixedPhone', 'emailAddress.address'];
+const contactFields = [
+  'simpleFullName',
+  'fixedPhone',
+  'emailAddress.address',
+  'mainPartner',
+  'mainAddress',
+  'partnerSeq',
+  'picture',
+];
 
 export async function searchContactWithIds(idList) {
   return axiosApiProvider.post({
@@ -16,6 +24,90 @@ export async function searchContactWithIds(idList) {
         ],
       },
       fields: contactFields,
+    },
+  });
+}
+
+const sortByFields = ['name', 'createdOn'];
+
+const createContactCriteria = searchValue => {
+  let criterias = [];
+  criterias.push({
+    operator: 'and',
+    criteria: [
+      {
+        fieldName: 'isContact',
+        operator: '=',
+        value: true,
+      },
+      {
+        operator: 'or',
+        criteria: [
+          {
+            fieldName: 'mainPartner.isCustomer',
+            operator: '=',
+            value: true,
+          },
+          {
+            fieldName: 'mainPartner.isProspect',
+            operator: '=',
+            value: true,
+          },
+        ],
+      },
+    ],
+  });
+  if (searchValue != null) {
+    criterias.push({
+      operator: 'or',
+      criteria: [
+        {
+          fieldName: 'simpleFullName',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'partnerSeq',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'mainAddress.fullName',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'fixedPhone',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'emailAddress.name',
+          operator: 'like',
+          value: searchValue,
+        },
+      ],
+    });
+  }
+  return criterias;
+};
+
+export async function searchContact({searchValue, page = 0}) {
+  return axiosApiProvider.post({
+    url: '/ws/rest/com.axelor.apps.base.db.Partner/search/',
+    data: {
+      data: {
+        criteria: [
+          {
+            operator: 'and',
+            criteria: createContactCriteria(searchValue),
+          },
+        ],
+      },
+      fields: contactFields,
+      sortBy: sortByFields,
+      limit: 10,
+      offset: 10 * page,
     },
   });
 }
--- packages/apps/crm/src/features/contactSlice.js
+++ packages/apps/crm/src/features/contactSlice.js
@@ -1,6 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {handlerApiCall} from '@axelor/aos-mobile-core';
-import {searchContactWithIds} from '../api/contact-api';
+import {searchContactWithIds, searchContact} from '../api/contact-api';
 
 export const searchContactById = createAsyncThunk(
   'crm/searchContactById',
@@ -15,9 +15,26 @@ export const searchContactById = createAsyncThunk(
   },
 );
 
+export const fetchContact = createAsyncThunk(
+  'crm/Contact',
+  async function (data, {getState}) {
+    return handlerApiCall({
+      fetchFunction: searchContact,
+      data,
+      action: 'fetch crm contact',
+      getState,
+      responseOptions: {isArrayResponse: true},
+    });
+  },
+);
+
 const initialState = {
   loading: false,
   listContactById: [],
+  moreLoading: false,
+  loadingContact: false,
+  isListEnd: false,
+  contactList: [],
 };
 
 const contactSlice = createSlice({
@@ -30,6 +47,28 @@ const contactSlice = createSlice({
     builder.addCase(searchContactById.fulfilled, (state, action) => {
       state.loading = false;
       state.listContactById = action.payload;
+    });
+    builder.addCase(fetchContact.pending, (state, action) => {
+      if (action.meta.arg.page === 0) {
+        state.loadingContact = true;
+      } else {
+        state.moreLoading = true;
+      }
+    });
+    builder.addCase(fetchContact.fulfilled, (state, action) => {
+      state.loadingContact = false;
+      state.moreLoading = false;
+      if (action.meta.arg.page === 0 || action.meta.arg.page == null) {
+        state.contactList = action.payload;
+        state.isListEnd = false;
+      } else {
+        if (action.payload != null) {
+          state.isListEnd = false;
+          state.contactList = [...state.contactList, ...action.payload];
+        } else {
+          state.isListEnd = true;
+        }
+      }
     });
   },
 });
