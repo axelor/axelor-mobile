02852b2379c2836eb182eb46aa787bbb152dd759
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-01-16 12:12:37 +0100

    feat: add contactListScreen
    * RM#58980

---
+++ packages/apps/crm/src/screens/contact/index.js
@@ -0,0 +1,11 @@
+import ContactListScreen from './ContactListScreen';
+
+export default {
+  ContactListScreen: {
+    title: 'Crm_Contacts',
+    component: ContactListScreen,
+    options: {
+      shadedHeader: false,
+    },
+  },
+};
