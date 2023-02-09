a0059139840d31ee29e40d285c76c518fce718cb
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-02-09 15:03:26 +0100

    feat: add jobTitle Input with api

--- packages/apps/crm/src/screens/lead/LeadFormScreen.js
+++ packages/apps/crm/src/screens/lead/LeadFormScreen.js
@@ -9,22 +9,26 @@ import {
 } from '@axelor/aos-mobile-ui';
 import {useSelector, useDispatch, useTranslator} from '@axelor/aos-mobile-core';
 import {fetchLeadById} from '../../features/leadSlice';
+import {fetchFunction} from '../../features/functionSlice';
 
 const LeadFormScreen = ({navigation, route}) => {
   const idLead = route.params.idLead;
   const {lead} = useSelector(state => state.lead);
+  const {functionList} = useSelector(state => state.function);
   const dispatch = useDispatch();
   const I18n = useTranslator();
-
   useEffect(() => {
     dispatch(fetchLeadById({leadId: idLead}));
+    dispatch(fetchFunction());
   }, [dispatch, idLead]);
+
   const civilityList = [
     {id: 1, name: 'M.'},
     {id: 2, name: 'Mme.'},
     {id: 3, name: 'Dr'},
     {id: 4, name: 'Prof'},
   ];
+
   return (
     <Screen removeSpaceOnTop={true}>
       <ScrollView>
@@ -84,6 +88,21 @@ const LeadFormScreen = ({navigation, route}) => {
               lead?.enterpriseName !== null ? lead?.enterpriseName : ''
             }
           />
+          <View style={{width: '100%'}}>
+            <Picker
+              pickerStyle={{width: '100%'}}
+              title={I18n.t('Crm_JobTitle')}
+              onValueChange={e => console.log('job', e)}
+              listItems={functionList}
+              labelField="name"
+              valueField="id"
+              defaultValue={
+                functionList.find(
+                  job => job.name === lead.jobTitleFunction.name,
+                )?.id
+              }
+            />
+          </View>
         </View>
       </ScrollView>
     </Screen>
