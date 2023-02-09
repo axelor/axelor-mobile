ce027e88160fe048d3c6d36eb52a10e29b9a8ffb
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-02-09 09:24:33 +0100

    feat: add PanelTabs Component

--- packages/ui/src/components/molecules/index.tsx
+++ packages/ui/src/components/molecules/index.tsx
@@ -42,6 +42,7 @@ export {default as LoadingIndicator} from './LoadingIndicator/LoadingIndicator';
 export {default as MessageBox} from './MessageBox/MessageBox';
 export {default as MovementIndicationCard} from './MovementIndicationCard/MovementIndicationCard';
 export {default as MultiValuePickerButton} from './MultiValuePickerButton/MultiValuePickerButton';
+export {default as PanelTabs} from './PanelTabs/PanelTabs';
 export {default as NotesCard} from './NotesCard/NotesCard';
 export {default as PopUp} from './PopUp/PopUp';
 export {default as RightIconButton} from './RightIconButton/RightIconButton';
