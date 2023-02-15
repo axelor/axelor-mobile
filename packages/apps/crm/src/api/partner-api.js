6425a047af166afdd933f62d4ffdeb3bedc7ed7d
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-02-15 10:29:43 +0100

    feat: add ContactFormScreen (#7)
    * RM#60637

--- packages/apps/crm/src/api/partner-api.js
+++ packages/apps/crm/src/api/partner-api.js
@@ -31,3 +31,99 @@ export async function getPartner({partnerId}) {
     },
   });
 }
+
+const createClientAndProspectCriteria = searchValue => {
+  let criterias = [];
+  criterias.push({
+    operator: 'or',
+    criteria: [
+      {
+        operator: 'and',
+        criteria: [
+          {
+            fieldName: 'isContact',
+            operator: '=',
+            value: false,
+          },
+          {
+            fieldName: 'isCustomer',
+            operator: '=',
+            value: true,
+          },
+        ],
+      },
+      {
+        operator: 'and',
+        criteria: [
+          {
+            fieldName: 'isContact',
+            operator: '=',
+            value: false,
+          },
+          {
+            fieldName: 'isProspect',
+            operator: '=',
+            value: true,
+          },
+        ],
+      },
+    ],
+  });
+  if (searchValue != null) {
+    criterias.push({
+      operator: 'or',
+      criteria: [
+        {
+          fieldName: 'simpleFullName',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'partnerSeq',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'mainAddress.fullName',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'mobilePhone',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'fixedPhone',
+          operator: 'like',
+          value: searchValue,
+        },
+        {
+          fieldName: 'emailAddress.name',
+          operator: 'like',
+          value: searchValue,
+        },
+      ],
+    });
+  }
+  return criterias;
+};
+
+export async function searchClientAndProspect({searchValue, page = 0}) {
+  return axiosApiProvider.post({
+    url: '/ws/rest/com.axelor.apps.base.db.Partner/search/',
+    data: {
+      data: {
+        criteria: [
+          {
+            operator: 'and',
+            criteria: createClientAndProspectCriteria(searchValue),
+          },
+        ],
+      },
+      fields: partnerFields,
+      limit: 10,
+      offset: 10 * page,
+    },
+  });
+}
--- packages/apps/crm/src/features/partnerSlice.js
+++ packages/apps/crm/src/features/partnerSlice.js
@@ -1,6 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {handlerApiCall} from '@axelor/aos-mobile-core';
-import {getPartner} from '../api/partner-api';
+import {getPartner, searchClientAndProspect} from '../api/partner-api';
 
 export const fetchPartner = createAsyncThunk(
   'partner/fetchPartner',
@@ -15,9 +15,26 @@ export const fetchPartner = createAsyncThunk(
   },
 );
 
+export const fetchClientAndProspect = createAsyncThunk(
+  'client/fetchClients',
+  async function (data, {getState}) {
+    return handlerApiCall({
+      fetchFunction: searchClientAndProspect,
+      data,
+      action: 'fetch crm client and prospect',
+      getState,
+      responseOptions: {isArrayResponse: true},
+    });
+  },
+);
+
 const initialState = {
   loadingPartner: true,
   partner: {},
+  clientAndProspectList: [],
+  loading: false,
+  moreLoading: false,
+  isListEnd: false,
 };
 
 const partnerSlice = createSlice({
@@ -30,6 +47,31 @@ const partnerSlice = createSlice({
     builder.addCase(fetchPartner.fulfilled, (state, action) => {
       state.loadingPartner = false;
       state.partner = action.payload;
+    });
+    builder.addCase(fetchClientAndProspect.pending, (state, action) => {
+      if (action.meta.arg.page === 0) {
+        state.loading = true;
+      } else {
+        state.moreLoading = true;
+      }
+    });
+    builder.addCase(fetchClientAndProspect.fulfilled, (state, action) => {
+      state.loading = false;
+      state.moreLoading = false;
+      if (action.meta.arg.page === 0 || action.meta.arg.page == null) {
+        state.clientAndProspectList = action.payload;
+        state.isListEnd = false;
+      } else {
+        if (action.payload != null) {
+          state.isListEnd = false;
+          state.clientAndProspectList = [
+            ...state.clientAndProspectList,
+            ...action.payload,
+          ];
+        } else {
+          state.isListEnd = true;
+        }
+      }
     });
   },
 });
