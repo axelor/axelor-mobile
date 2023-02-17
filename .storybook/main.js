72815e6af50b23ed965b21b3201c03ba3090df20
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-02-17 16:36:03 +0100

    feat: add main.js webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,53 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      // We shim fs for things like the blog slugs component
+      // where we need fs access in the server-side part
+      config.node = {
+        fs: 'empty',
+      };
+    }
+
+    // react-native-web
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
+    };
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions,
+    ];
+
+    config.module.rules.push({
+      test: /\.ttf$/,
+      loader: 'url-loader',
+    });
+
+    // {
+    //   test: /\.bs\.js$/,
+    //   use: options.defaultLoaders.babel,
+    //   include: /node_modules/,
+    // },
+
+    // avoid duplicated react
+    // config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+
+    return config;
+  },
+};
