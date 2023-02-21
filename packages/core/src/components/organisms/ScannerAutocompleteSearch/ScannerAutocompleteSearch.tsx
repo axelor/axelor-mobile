dfa9b4ad22cb272ef24ff0054bdce1bc00226210
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-02-21 11:35:30 +0100

    refactor: simplify integration of CameraScanner
    * Move CameraScanner component to app root
    * Create slice to use camera and get barcode data

--- packages/core/src/components/organisms/ScannerAutocompleteSearch/ScannerAutocompleteSearch.tsx
+++ packages/core/src/components/organisms/ScannerAutocompleteSearch/ScannerAutocompleteSearch.tsx
@@ -16,8 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useState, useEffect, useMemo} from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import {AutoCompleteSearch, useThemeColor} from '@axelor/aos-mobile-ui';
 import {
@@ -25,7 +24,10 @@ import {
   useScannedValueByKey,
   useScannerSelector,
 } from '../../../features/scannerSlice';
-import {CameraScanner} from '../../external';
+import {
+  enableCameraScanner,
+  useCameraScannerValueByKey,
+} from '../../../features/cameraScannerSlice';
 
 interface AutocompleteSearchProps {
   objectList: any[];
@@ -39,7 +41,6 @@ interface AutocompleteSearchProps {
   changeScreenAfter?: boolean;
   navigate?: boolean;
   oneFilter?: boolean;
-  searchBarKey?: number;
   selectLastItem?: boolean;
 }
 
@@ -55,81 +56,55 @@ const ScannerAutocompleteSearch = ({
   changeScreenAfter = false,
   navigate = false,
   oneFilter = false,
-  searchBarKey = 1,
   selectLastItem = true,
 }: AutocompleteSearchProps) => {
-  const [camScan, setCamScan] = useState(false);
-  const [scanData, setScanData] = useState(null);
-  const [viewCoordinate, setViewCoordinate] = useState(null);
   const [searchText, setSearchText] = useState(value);
   const {isEnabled, scanKey} = useScannerSelector();
   const scannedValue = useScannedValueByKey(scanKeySearch);
+  const scanData = useCameraScannerValueByKey(scanKeySearch);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (scannedValue) {
       setSearchText(scannedValue);
-    } else if (scanData != null && scanData.value != null) {
-      setCamScan(false);
+    } else if (scanData?.value != null) {
       setSearchText(scanData.value);
     }
   }, [scanData, scannedValue]);
 
-  const containerPosition = useMemo(() => {
-    return getStyles(searchBarKey, camScan)?.container;
-  }, [searchBarKey, camScan]);
-
   const Colors = useThemeColor();
 
   return (
-    <View
-      style={containerPosition}
-      onLayout={event => {
-        const {x, y} = event.nativeEvent.layout;
-        setViewCoordinate({x: x, y: y});
-      }}>
-      <CameraScanner
-        isActive={camScan}
-        onScan={setScanData}
-        coordinate={viewCoordinate}
-        onClose={() => setCamScan(false)}
-      />
-      <AutoCompleteSearch
-        selectLastItem={selectLastItem}
-        objectList={objectList}
-        value={searchText}
-        onChangeValue={result => {
-          setSearchText(result);
-          onChangeValue(result);
-        }}
-        fetchData={fetchData}
-        displayValue={displayValue}
-        placeholder={placeholder}
-        isFocus={isFocus}
-        changeScreenAfter={changeScreenAfter}
-        navigate={navigate}
-        oneFilter={oneFilter}
-        onSelection={() => {
-          scanKeySearch ? dispatch(enableScan(scanKeySearch)) : undefined;
-        }}
-        onScanPress={() => setCamScan(true)}
-        scanIconColor={
-          isEnabled && scanKey === scanKeySearch
-            ? Colors.primaryColor.background
-            : Colors.secondaryColor_dark.background
-        }
-      />
-    </View>
+    <AutoCompleteSearch
+      selectLastItem={selectLastItem}
+      objectList={objectList}
+      value={searchText}
+      onChangeValue={result => {
+        setSearchText(result);
+        onChangeValue(result);
+      }}
+      fetchData={fetchData}
+      displayValue={displayValue}
+      placeholder={placeholder}
+      isFocus={isFocus}
+      changeScreenAfter={changeScreenAfter}
+      navigate={navigate}
+      oneFilter={oneFilter}
+      onSelection={() => {
+        scanKeySearch ? dispatch(enableScan(scanKeySearch)) : undefined;
+      }}
+      onScanPress={() => {
+        scanKeySearch
+          ? dispatch(enableCameraScanner(scanKeySearch))
+          : undefined;
+      }}
+      scanIconColor={
+        isEnabled && scanKey === scanKeySearch
+          ? Colors.primaryColor.background
+          : Colors.secondaryColor_dark.background
+      }
+    />
   );
 };
-
-const getStyles = (key, isActive) =>
-  StyleSheet.create({
-    container: {
-      position: 'relative',
-      zIndex: isActive ? 20 + key : 20 - key,
-      width: Dimensions.get('window').width,
-    },
-  });
 
 export default ScannerAutocompleteSearch;
