1c390cab5b8d839992f9b033a7e60bf7fadf3082
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-03-01 18:16:09 +0100

    feat: hide header icon actions for small screen

--- packages/ui/src/components/organisms/index.tsx
+++ packages/ui/src/components/organisms/index.tsx
@@ -19,6 +19,7 @@
 export {default as AutoCompleteSearch} from './AutoCompleteSearch/AutoCompleteSearch';
 export {default as ChipSelect} from './ChipSelect/ChipSelect';
 export {default as DropdownCardSwitch} from './DropdownCardSwitch/DropdownCardSwitch';
+export {default as DropdownMenuItem} from './DropdownMenuItem/DropdownMenuItem';
 export {default as FloatingButton} from './FloatingButton/FloatingButton';
 export {default as HeaderContainer} from './HeaderContainer/HeaderContainer';
 export {default as ImageBubble} from './ImageBubble/ImageBubble';
