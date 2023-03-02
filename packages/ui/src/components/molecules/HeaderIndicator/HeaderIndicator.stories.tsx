f76d3eb696fd6dba37d488413e62b854410e0c1d
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-03-02 13:36:43 +0100

    feat: add HeaderIndicator.stories.tsx

---
+++ packages/ui/src/components/molecules/HeaderIndicator/HeaderIndicator.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import HeaderIndicator from './HeaderIndicator';
+
+storiesOf('ui/molecules/HeaderIndicator', module).add('Default', () => (
+  <HeaderIndicator />
+));
