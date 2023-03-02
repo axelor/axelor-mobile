d9e4e6b64c6dc8aa9efd5e6323a35ac3dff1dc9f
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-03-02 16:22:30 +0100

    feat: add Search.stories.tsx

---
+++ packages/ui/src/components/organisms/SearchBar/SearchBar.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import {default as SearchBar} from './SearchBar';
+
+const defaultArgs = {
+  valueTxt: '',
+  placeholder: 'Search',
+  onClearPress: () => {},
+  onChangeTxt: () => {},
+  onSelection: () => {},
+  onEndFocus: () => {},
+  isFocus: false,
+  onScanPress: () => {},
+  scanIconColor: null,
+};
+
+storiesOf('ui/molecules/SearchBar', module).add(
+  'Default',
+  args => {
+    const props = {...defaultArgs, ...args};
+    return <SearchBar {...props} />;
+  },
+  {
+    argTypes: {
+      valueTxt: {control: {type: 'text'}, defaultValue: defaultArgs.valueTxt},
+      placeholder: {
+        control: {type: 'text'},
+        defaultValue: defaultArgs.placeholder,
+      },
+      onClearPress: {action: 'onClearPress'},
+      onChangeTxt: {action: 'onPress'},
+      onSelection: {
+        action: 'onSelection',
+      },
+      onEndFocus: {
+        action: 'onEndFocus',
+      },
+      isFocus: {
+        control: {
+          type: 'boolean',
+        },
+        defaultValue: false,
+      },
+      onScanPress: {
+        action: 'onScanPress',
+      },
+      scanIconColor: {
+        control: {
+          type: 'color',
+        },
+      },
+    },
+  },
+);
