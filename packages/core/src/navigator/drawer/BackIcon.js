a5abc7e4a975f6e3253d0a70cd09d26b1cadfb61
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-03-09 16:30:03 +0100

    fix: increase touchable zone on back button
    RM#61695

--- packages/core/src/navigator/drawer/BackIcon.js
+++ packages/core/src/navigator/drawer/BackIcon.js
@@ -17,27 +17,24 @@
  */
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {StackActions, useNavigation} from '@react-navigation/native';
 import {Icon} from '@axelor/aos-mobile-ui';
 
 const BackIcon = ({tintColor}) => {
   const navigation = useNavigation();
   return (
-    <Icon
-      style={styles.icon}
-      name="angle-left"
-      size={24}
-      color={tintColor}
-      touchable={true}
+    <TouchableOpacity
       onPress={() => navigation.dispatch(StackActions.pop())}
-    />
+      style={styles.icon}>
+      <Icon name="angle-left" size={26} color={tintColor} />
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   icon: {
-    marginHorizontal: 14,
+    paddingHorizontal: 14,
   },
 });
 
