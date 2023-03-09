55242fdf175454f8f13d1dab060a1d7fc786b7d1
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-03-09 17:14:05 +0100

    feat: add InventoryStartedHeader component

--- packages/apps/stock/src/components/organisms/index.js
+++ packages/apps/stock/src/components/organisms/index.js
@@ -45,3 +45,4 @@ export {default as StockCorrectionBody} from './StockCorrectionBody/StockCorrect
 export {default as StockCorrectionFixedItems} from './StockCorrectionFixedItems/StockCorrectionFixedItems';
 export {default as InternalMoveGeneralFixedItems} from './InternalMoveGeneralFixedItems/InternalMoveGeneralFixedItems';
 export {default as InternalMoveGeneralBody} from './InternalMoveGeneralBody/InternalMoveGeneralBody';
+export {default as InventoryStartedHeader} from './InventoryStartedHeader/InventoryStartedHeader';
