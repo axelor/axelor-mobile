a98a7fd50ba3808acc1e28d0fddbec2c1f5b310c
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-03-16 14:01:39 +0100

    refactor: simplify FormHtmlInput with onFocus and onBlur props of HtmlInput

--- packages/ui/src/components/molecules/FormHtmlInput/FormHtmlInput.tsx
+++ packages/ui/src/components/molecules/FormHtmlInput/FormHtmlInput.tsx
@@ -16,16 +16,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {StyleSheet, View, TouchableWithoutFeedback} from 'react-native';
 import {HtmlInput, Text} from '../../atoms';
 import {useThemeColor} from '../../../theme/ThemeContext';
 import {ThemeColors} from '../../../theme/themes';
 import {getCommonStyles} from '../../../utils/commons-styles';
-import {
-  OUTSIDE_INDICATOR,
-  useClickOutside,
-} from '../../../hooks/use-click-outside';
 
 interface FormHtmlInputProps {
   title: string;
@@ -51,12 +47,6 @@ const FormHtmlInput = ({
   const [value, setValue] = useState(defaultValue);
   const [isFocused, setIsFocused] = useState(false);
 
-  const wrapperRef = useRef(null);
-  const clickOutside = useClickOutside({
-    wrapperRef,
-    visible: isFocused,
-  });
-
   const _required = useMemo(
     () => required && (value == null || value === ''),
     [required, value],
@@ -70,24 +60,29 @@ const FormHtmlInput = ({
     [onChange],
   );
 
-  const commonStyles = useMemo(() => getCommonStyles(Colors), [Colors]);
+  const commonStyles = useMemo(
+    () => getCommonStyles(Colors, _required),
+    [Colors, _required],
+  );
+
   const styles = useMemo(
     () => getStyles(Colors, _required),
     [Colors, _required],
   );
 
-  useEffect(() => {
-    if (clickOutside === OUTSIDE_INDICATOR) {
-      setIsFocused(false);
-    }
-  }, [clickOutside]);
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
 
   return (
     <View style={style}>
       <Text style={styles.title}>{title}</Text>
       <TouchableWithoutFeedback onPress={() => setIsFocused(true)}>
         <View
-          ref={wrapperRef}
           style={[
             commonStyles.filter,
             styles.content,
@@ -100,6 +95,8 @@ const FormHtmlInput = ({
             readonly={readonly}
             style={styles.input}
             styleToolbar={styles.htmlToolBar}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
           />
         </View>
       </TouchableWithoutFeedback>
