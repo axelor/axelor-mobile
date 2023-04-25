885221d4e51bb1510490ff4207b82a549f32a7f9
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-04-25 09:21:00 +0200

    feat: add storybook in ui package (#14)
    * RM#57377

---
+++ packages/ui/stories/molecules/Button.stories.tsx
@@ -0,0 +1,71 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {storiesOf} from '@storybook/react-native';
+import {Button} from '../../src/components/molecules';
+import {lightTheme} from '../../src/theme';
+
+storiesOf('ui/molecules/Button', module).add(
+  'Default',
+  args => {
+    return (
+      <View style={styles.container}>
+        <Button title={'Press me'} {...args} />
+      </View>
+    );
+  },
+  {
+    argTypes: {
+      title: {
+        type: 'string',
+        defaultValue: 'Press me',
+        control: {type: 'text'},
+      },
+      color: {
+        options: Object.keys(lightTheme.colors),
+        mapping: lightTheme.colors,
+        control: {
+          type: 'select',
+          labels: {
+            primary: 'Primary',
+            secondary: 'Secondary',
+          },
+        },
+      },
+      disabled: {
+        type: 'boolean',
+        defaultValue: false,
+        control: {type: 'boolean'},
+      },
+      onPress: {
+        action: 'clicked',
+        table: {disable: true},
+      },
+    },
+  },
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
