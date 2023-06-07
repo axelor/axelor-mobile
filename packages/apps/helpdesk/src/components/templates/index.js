2ccdba8010c25d2e3314a6854cdde85b31ac6777
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-06-07 10:27:14 +0200

    feat: add menu entry myTickets in Helpdesk module (#72)
    * RM#64915

--- packages/apps/helpdesk/src/components/templates/index.js
+++ packages/apps/helpdesk/src/components/templates/index.js
@@ -16,5 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from './molecules';
-export * from './templates';
+export {default as TicketSearchBar} from './TicketSearchBar/TicketSearchBar';
