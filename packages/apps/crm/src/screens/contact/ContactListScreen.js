af1d88a2c860f633bfba2c9d9671636658674abe
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-06-28 16:15:27 +0200

    feat: make main partner required on contact form view (#91)
    * RM#66062

--- packages/apps/crm/src/screens/contact/ContactListScreen.js
+++ packages/apps/crm/src/screens/contact/ContactListScreen.js
@@ -84,7 +84,11 @@ const ContactListScreen = ({navigation}) => {
               rightTitle={I18n.t('Crm_AssignedToMe')}
               onSwitch={() => setAssigned(!assigned)}
             />
-            <ContactSearchBar showDetailsPopup={false} oneFilter={true} />
+            <ContactSearchBar
+              showDetailsPopup={false}
+              oneFilter={true}
+              showTitle={false}
+            />
           </View>
         }
       />
