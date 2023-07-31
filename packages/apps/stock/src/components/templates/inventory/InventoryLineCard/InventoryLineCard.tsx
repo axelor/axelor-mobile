37cfad35be2643236bdd88d1d63761d4e2c12d5d
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-07-31 17:45:28 +0200

    refactor: simplify stock object cards (#98)
    * RM#66782

--- packages/apps/stock/src/components/templates/inventory/InventoryLineCard/InventoryLineCard.tsx
+++ packages/apps/stock/src/components/templates/inventory/InventoryLineCard/InventoryLineCard.tsx
@@ -17,12 +17,10 @@
  */
 
 import React, {useMemo} from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {
-  Card,
-  Icon,
-  LabelText,
-  Text,
+  ObjectCard,
+  checkNullString,
   useThemeColor,
 } from '@axelor/aos-mobile-ui';
 import {useTranslator} from '@axelor/aos-mobile-core';
@@ -62,43 +60,38 @@ const InventoryLineCard = ({
   }, [Colors, currentQty, realQty]);
 
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
-      <Card style={[styles.container, borderStyle, style]}>
-        <View style={styles.textContainer}>
-          <Text style={styles.txtImportant}>{productName}</Text>
-          <LabelText
-            title={`${I18n.t('Stock_DatabaseQty')} :`}
-            value={`${parseFloat(currentQty.toString()).toFixed(2)} ${unit}`}
-          />
-          {realQty && (
-            <LabelText
-              title={`${I18n.t('Stock_PhysicalQty')} :`}
-              value={`${parseFloat(realQty.toString()).toFixed(2)} ${unit}`}
-            />
-          )}
-          {locker != null && (
-            <LabelText
-              title={`${I18n.t('Stock_Locker')} :`}
-              value={locker}
-              iconName="map-marker-alt"
-            />
-          )}
-          {trackingNumber != null && (
-            <LabelText
-              title={`${I18n.t('Stock_TrackingNumber')} :`}
-              value={trackingNumber?.trackingNumberSeq}
-              iconName="qrcode"
-              FontAwesome5={false}
-            />
-          )}
-        </View>
-        <Icon
-          name="chevron-right"
-          color={Colors.secondaryColor.background_light}
-          size={20}
-        />
-      </Card>
-    </TouchableOpacity>
+    <ObjectCard
+      onPress={onPress}
+      showArrow={true}
+      style={[borderStyle, style]}
+      lowerTexts={{
+        items: [
+          {displayText: productName, isTitle: true},
+          {
+            displayText: `${parseFloat(currentQty.toString()).toFixed(
+              2,
+            )} ${unit}`,
+            indicatorText: `${I18n.t('Stock_DatabaseQty')} :`,
+          },
+          {
+            displayText: `${parseFloat(realQty.toString()).toFixed(2)} ${unit}`,
+            indicatorText: `${I18n.t('Stock_PhysicalQty')} :`,
+          },
+          {
+            displayText: locker,
+            indicatorText: `${I18n.t('Stock_Locker')} :`,
+            hideIf: checkNullString(locker),
+            iconName: 'map-marker-alt',
+          },
+          {
+            displayText: trackingNumber?.trackingNumberSeq,
+            indicatorText: `${I18n.t('Stock_TrackingNumber')} :`,
+            hideIf: trackingNumber?.trackingNumberSeq == null,
+            iconName: 'qrcode',
+          },
+        ],
+      }}
+    />
   );
 };
 
@@ -109,22 +102,5 @@ const getStyles = color =>
       borderColor: color,
     },
   });
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    marginBottom: '2%',
-    paddingRight: 8,
-  },
-  textContainer: {
-    width: '90%',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-  txtImportant: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
 
 export default InventoryLineCard;
