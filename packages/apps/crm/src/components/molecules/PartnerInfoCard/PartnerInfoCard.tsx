0c09b4955c6ffae981272f0d43d4e4a6a23a5b2d
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-08-01 09:05:34 +0200

    refactor: simplify crm object cards (#105)
    * RM#66784

--- packages/apps/crm/src/components/molecules/PartnerInfoCard/PartnerInfoCard.tsx
+++ packages/apps/crm/src/components/molecules/PartnerInfoCard/PartnerInfoCard.tsx
@@ -17,9 +17,9 @@
  */
 
 import React from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
-import {Card, Icon, Text, useThemeColor} from '@axelor/aos-mobile-ui';
-import {AOSImage} from '@axelor/aos-mobile-core';
+import {StyleSheet} from 'react-native';
+import {ObjectCard} from '@axelor/aos-mobile-ui';
+import {useMetafileUri} from '@axelor/aos-mobile-core';
 
 interface PartnerInfoCardProps {
   fullName: string;
@@ -36,63 +36,37 @@ const PartnerInfoCard = ({
   style,
   onPress = () => {},
 }: PartnerInfoCardProps) => {
-  const Colors = useThemeColor();
+  const formatMetaFile = useMetafileUri();
 
   if (fullName == null || partnerSeq == null) {
     return null;
   }
 
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
-      <Card style={[styles.container, style]}>
-        <View style={styles.imageContainer}>
-          <AOSImage
-            generalStyle={styles.imageSize}
-            imageSize={styles.imageSize}
-            defaultIconSize={50}
-            metaFile={picture}
-            resizeMode="contain"
-          />
-        </View>
-        <View style={styles.infoContainer}>
-          <Text fontSize={16} style={styles.textImportant}>
-            {fullName}
-          </Text>
-          <Text fontSize={16}>{partnerSeq}</Text>
-        </View>
-        <Icon
-          name="chevron-right"
-          color={Colors.secondaryColor.background_light}
-          size={20}
-          style={styles.icon}
-        />
-      </Card>
-    </TouchableOpacity>
+    <ObjectCard
+      style={style}
+      onPress={onPress}
+      image={{
+        generalStyle: styles.imageSize,
+        imageSize: styles.imageSize,
+        resizeMode: 'contain',
+        defaultIconSize: 50,
+        source: formatMetaFile(picture?.id),
+      }}
+      upperTexts={{
+        items: [
+          {displayText: fullName, isTitle: true},
+          {indicatorText: partnerSeq, hideIfNull: true},
+        ],
+      }}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    marginLeft: 5,
-    marginRight: 10,
-    marginBottom: 10,
-  },
-  icon: {
-    justifyContent: 'center',
-  },
   imageSize: {
     height: 50,
     width: 50,
-  },
-  imageContainer: {
-    marginRight: 15,
-  },
-  infoContainer: {
-    flex: 1,
-  },
-  textImportant: {
-    fontWeight: 'bold',
   },
 });
 
