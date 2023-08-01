244181cc14dee9331d536824459c7ef70eef7f82
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-08-01 13:41:37 +0200

    refactor: simplify manufacturing object cards (#107)
    * RM#66783

--- packages/apps/manufacturing/src/components/organisms/ConsumedProductCard/ConsumedProductCard.tsx
+++ packages/apps/manufacturing/src/components/organisms/ConsumedProductCard/ConsumedProductCard.tsx
@@ -17,14 +17,8 @@
  */
 
 import React, {useMemo} from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
-import {
-  Badge,
-  Card,
-  LabelText,
-  Text,
-  useThemeColor,
-} from '@axelor/aos-mobile-ui';
+import {StyleSheet, View} from 'react-native';
+import {ObjectCard, useThemeColor} from '@axelor/aos-mobile-ui';
 import {useTranslator} from '@axelor/aos-mobile-core';
 
 interface ConsumedProductCardProps {
@@ -70,93 +64,72 @@ const ConsumedProductCard = ({
   }, [Colors, consumedQty, missingQty, plannedQty]);
 
   return (
-    <TouchableOpacity style={style} onPress={onPress} activeOpacity={0.9}>
-      <Card style={[styles.cardContainer, borderStyle]}>
-        <View style={styles.topContainer}>
-          <Text style={styles.txtImportant}>{productName}</Text>
-          {increment.incrementVisible && (
-            <Badge
-              title={`+ ${increment.addedQty}`}
-              style={styles.badge}
-              color={Colors.priorityColor}
-              txtStyle={styles.badgeInfos}
-            />
-          )}
-        </View>
-        <LabelText
-          title={`${I18n.t('Manufacturing_PlannedQty')}:`}
-          value={`${parseFloat(plannedQty.toString()).toFixed(2)} ${
-            unitName != null ? unitName : ''
-          }`}
-        />
-        <LabelText
-          title={`${I18n.t('Manufacturing_ConsumedQty')}:`}
-          value={`${
-            consumedQty == null
-              ? parseFloat('0').toFixed(2)
-              : parseFloat(consumedQty.toString()).toFixed(2)
-          } ${unitName != null ? unitName : ''}`}
-        />
-        {trackingNumber != null && (
-          <LabelText
-            iconName="qrcode"
-            title={`${I18n.t('Manufacturing_TrackingNumber')}:`}
-            value={trackingNumber}
-          />
-        )}
-        <Badge
-          color={
-            availableQty > 0
-              ? missingQty > 0
-                ? Colors.cautionColor
-                : Colors.primaryColor
-              : Colors.errorColor
-          }
-          title={
-            availableQty > 0
-              ? missingQty > 0
-                ? I18n.t('Stock_Partially')
-                : I18n.t('Stock_Available')
-              : I18n.t('Stock_Unavailable')
-          }
-        />
-      </Card>
-    </TouchableOpacity>
+    <View style={style}>
+      <ObjectCard
+        onPress={onPress}
+        style={borderStyle}
+        showArrow={false}
+        upperTexts={{
+          items: [
+            {isTitle: true, displayText: productName},
+            {
+              indicatorText: `${I18n.t('Manufacturing_PlannedQty')}:`,
+              displayText: `${parseFloat(plannedQty.toString()).toFixed(2)} ${
+                unitName != null ? unitName : ''
+              }`,
+            },
+            {
+              indicatorText: `${I18n.t('Manufacturing_ConsumedQty')}:`,
+              displayText: `${
+                consumedQty == null
+                  ? parseFloat('0').toFixed(2)
+                  : parseFloat(consumedQty.toString()).toFixed(2)
+              } ${unitName != null ? unitName : ''}`,
+            },
+            {
+              iconName: 'qrCode',
+              displayText: trackingNumber,
+              indicatorText: `${I18n.t('Manufacturing_TrackingNumber')}:`,
+              hideIf: trackingNumber == null,
+            },
+          ],
+        }}
+        lowerBadges={{
+          items: [
+            {
+              showIf: increment.incrementVisible,
+              displayText: `+ ${increment.addedQty}`,
+              color: Colors.priorityColor,
+            },
+            {
+              displayText:
+                availableQty > 0
+                  ? missingQty > 0
+                    ? I18n.t('Stock_Partially')
+                    : I18n.t('Stock_Available')
+                  : I18n.t('Stock_Unavailable'),
+              color:
+                availableQty > 0
+                  ? missingQty > 0
+                    ? Colors.cautionColor
+                    : Colors.primaryColor
+                  : Colors.errorColor,
+            },
+          ],
+        }}
+      />
+    </View>
   );
 };
 
 const getBorderStyles = color =>
   StyleSheet.create({
-    border: {borderLeftWidth: 7, borderLeftColor: color},
+    border: {
+      borderLeftWidth: 7,
+      borderLeftColor: color,
+      marginHorizontal: 0,
+      marginVertical: 0,
+    },
   });
-
-const styles = StyleSheet.create({
-  cardContainer: {
-    flexDirection: 'column',
-    paddingLeft: 10,
-    paddingRight: 10,
-    margin: 2,
-    elevation: 0,
-    shadowOpacity: 0,
-  },
-  topContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 20,
-  },
-  txtImportant: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  badge: {
-    height: 20,
-    width: 40,
-    borderRadius: 30,
-  },
-  badgeInfos: {
-    fontWeight: 'bold',
-  },
-});
 
 export default ConsumedProductCard;
