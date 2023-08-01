5a806b28519737e20c707f9e82fcf5e825e89bcc
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-08-01 13:41:37 +0200

    refactor: simplify manufacturing object cards (#107)
    * RM#66783

--- packages/apps/manufacturing/src/components/organisms/WasteProductCard/WasteProductCard.tsx
+++ packages/apps/manufacturing/src/components/organisms/WasteProductCard/WasteProductCard.tsx
@@ -17,8 +17,7 @@
  */
 
 import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
-import {Card, LabelText, Text} from '@axelor/aos-mobile-ui';
+import {ObjectCard} from '@axelor/aos-mobile-ui';
 import {useTranslator} from '@axelor/aos-mobile-core';
 
 interface WasteProductCardProps {
@@ -39,34 +38,24 @@ const WasteProductCard = ({
   const I18n = useTranslator();
 
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
-      <Card style={[styles.cardContainer, style]}>
-        <Text style={styles.txtImportant}>{productName}</Text>
-        <LabelText
-          title={`${I18n.t('Manufacturing_WasteQty')}:`}
-          value={`${
-            wasteQty == null
-              ? parseFloat('0').toFixed(2)
-              : parseFloat(wasteQty.toString()).toFixed(2)
-          } ${unitName != null ? unitName : ''}`}
-        />
-      </Card>
-    </TouchableOpacity>
+    <ObjectCard
+      onPress={onPress}
+      style={style}
+      upperTexts={{
+        items: [
+          {displayText: productName, isTitle: true},
+          {
+            indicatorText: `${I18n.t('Manufacturing_WasteQty')}:`,
+            displayText: `${
+              wasteQty == null
+                ? parseFloat('0').toFixed(2)
+                : parseFloat(wasteQty.toString()).toFixed(2)
+            } ${unitName != null ? unitName : ''}`,
+          },
+        ],
+      }}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  cardContainer: {
-    marginHorizontal: 16,
-    marginVertical: 4,
-    flexDirection: 'column',
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
-  txtImportant: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
 
 export default WasteProductCard;
