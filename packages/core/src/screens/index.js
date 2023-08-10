47d8ffd654a7b9e9378e43c70dcc9dd672d348fe
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-08-10 16:23:19 +0200

    feat: add form generator system (#108)
    * RM#66938
    * create new provider to generator form views
    * manage custom fields on object with specific form view.

--- packages/core/src/screens/index.js
+++ packages/core/src/screens/index.js
@@ -1,5 +1,24 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import AttachedFilesScreen from './AttachedFilesScreen';
 import BarcodeDisplayScreen from './BarcodeDisplayScreen';
+import JsonFieldScreen from './JsonFieldScreen';
 import MailMessageScreen from './MailMessageScreen';
 
 export default {
@@ -23,6 +42,14 @@ export default {
     title: 'Base_Barcode',
     component: BarcodeDisplayScreen,
     actionID: 'core_barcode_details',
+    options: {
+      shadedHeader: true,
+    },
+  },
+  JsonFieldScreen: {
+    title: 'Base_MetaJsonFields',
+    component: JsonFieldScreen,
+    actionID: 'core_metaJsonFields_details',
     options: {
       shadedHeader: true,
     },
