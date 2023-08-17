5fb7c2d0779c0d445953266694536c479e042909
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-08-17 16:59:08 +0200

    feat: add display of dates on MO cards and details view
    * RM#66959

--- packages/apps/manufacturing/src/components/molecules/index.js
+++ packages/apps/manufacturing/src/components/molecules/index.js
@@ -16,4 +16,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {default as ManufacturingOrderDatesCard} from './ManufacturingOrderDatesCard/ManufacturingOrderDatesCard';
 export {default as OperationOrderDatesCard} from './OperationOrderDatesCard/OperationOrderDatesCard';
