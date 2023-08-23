a16f135dc5c94c9947f26fc26dd1bd7bf50b1f4e
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-08-23 14:21:26 +0200

    style: remove animation on check

--- packages/ui/src/components/molecules/Checkbox/Checkbox.tsx
+++ packages/ui/src/components/molecules/Checkbox/Checkbox.tsx
@@ -16,18 +16,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useThemeColor} from '../../../theme/ThemeContext';
 import {Icon, Text} from '../../atoms';
+import {checkNullString} from '../../../utils/strings';
 
 interface CheckboxProps {
   style?: any;
   styleTxt?: any;
   iconSize?: number;
   disabled?: boolean;
-  title: string;
-  isDefaultChecked: boolean;
+  title?: string;
+  isDefaultChecked?: boolean;
   onChange: (any: any) => void;
 }
 
@@ -41,6 +42,7 @@ const Checkbox = ({
   onChange,
 }: CheckboxProps) => {
   const Colors = useThemeColor();
+
   const [isChecked, setIsChecked] = useState(isDefaultChecked);
 
   const iconName = useMemo(
@@ -53,41 +55,58 @@ const Checkbox = ({
     setIsChecked(!isChecked);
   };
 
+  const styles = useMemo(() => getStyles(iconSize), [iconSize]);
+
+  useEffect(() => {
+    setIsChecked(isDefaultChecked);
+  }, [isDefaultChecked]);
+
   return (
     <View style={[styles.container, style]}>
-      <Icon
-        name={iconName}
-        FontAwesome5={false}
-        color={
-          disabled
-            ? Colors.secondaryColor.background
-            : Colors.primaryColor.background
-        }
-        size={iconSize}
-        touchable={!disabled}
-        onPress={handleToggle}
-      />
-      <Text
-        style={[styles.title, styleTxt]}
-        textColor={Colors.text}
-        fontSize={14}>
-        {title}
-      </Text>
+      <View style={styles.iconContainer}>
+        <Icon
+          name={iconName}
+          FontAwesome5={false}
+          color={
+            disabled
+              ? Colors.secondaryColor.background
+              : Colors.primaryColor.background
+          }
+          size={iconSize}
+          touchable={!disabled}
+          onPress={handleToggle}
+        />
+      </View>
+      {!checkNullString(title) && (
+        <Text
+          style={[styles.title, styleTxt]}
+          textColor={Colors.text}
+          fontSize={14}>
+          {title}
+        </Text>
+      )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 16,
-    marginLeft: 5,
-    fontWeight: '600',
-  },
-});
+const getStyles = iconSize =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+    iconContainer: {
+      width: iconSize,
+      height: iconSize,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: 16,
+      marginLeft: 5,
+      fontWeight: '600',
+    },
+  });
 
 export default Checkbox;
