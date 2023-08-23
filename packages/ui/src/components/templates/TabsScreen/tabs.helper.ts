1cd18847f2aee38e02114beaff44a775b60ddbc9
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-08-23 09:55:11 +0200

    feat: add TabsScreen component (#121)
    * RM#67966

---
+++ packages/ui/src/components/templates/TabsScreen/tabs.helper.ts
@@ -0,0 +1,57 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export interface Tab {
+  key: number;
+  title: string;
+  view?: JSX.Element;
+  hidden?: boolean;
+  isActive?: boolean;
+  disabled?: boolean;
+  count?: number;
+  showBadge?: boolean;
+}
+
+export interface TabProps {
+  style?: any;
+  title: string;
+  onPress: () => void;
+  isActive?: boolean;
+  disabled?: boolean;
+  count?: number;
+  showBadge?: boolean;
+}
+
+export interface TabsScreenProps {
+  style?: any;
+  viewStyle?: any;
+  position?: TabsPosition;
+  tabHeight?: number;
+  items?: Tab[];
+}
+
+export enum TabsPosition {
+  Bottom = 'bottom',
+  Top = 'top',
+}
+
+export const BADGE_SIZE = 27;
+
+export const capitalizeFirstLetter = (string: string): string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
