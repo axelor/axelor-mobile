80540023951b7d1aaa5f4b2d122cdc341d87ff56
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-08-23 15:02:34 +0200

    feat: add orphan expense lines menu entry in HR (#115)
    * RM#67878

---
+++ packages/apps/hr/src/types/expenseLine.tsx
@@ -0,0 +1,45 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class ExpenseLineType {
+  static kilomectricTypeSelect = {
+    OneWay: 1,
+    RoundTrip: 2,
+  };
+
+  static getKilomectricTypeSelect = (
+    select: number,
+    I18n: {t: (key: string) => string},
+  ) => {
+    if (I18n) {
+      switch (select) {
+        case this.kilomectricTypeSelect.OneWay:
+          return I18n.t('Hr_kilomectricTypeSelect_OneWay');
+        case this.kilomectricTypeSelect.RoundTrip:
+          return I18n.t('Hr_kilomectricTypeSelect_RoundTrip');
+        default:
+          console.warn(
+            `KilomectricTypeSelect provided with value ${select} is not supported by ExpenseLine`,
+          );
+          return null;
+      }
+    }
+  };
+}
+
+export default ExpenseLineType;
