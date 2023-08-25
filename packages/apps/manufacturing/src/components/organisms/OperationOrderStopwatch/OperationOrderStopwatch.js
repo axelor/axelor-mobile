d8a1fdae6143fcfb2d09a7e89b6b1b405c43d618
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2023-08-25 16:17:58 +0200

    feat: add refresh before update and label display for response

--- packages/apps/manufacturing/src/components/organisms/OperationOrderStopwatch/OperationOrderStopwatch.js
+++ packages/apps/manufacturing/src/components/organisms/OperationOrderStopwatch/OperationOrderStopwatch.js
@@ -16,7 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {Label} from '@axelor/aos-mobile-ui';
 import {
   useTranslator,
   Stopwatch,
@@ -27,13 +28,18 @@ import {
 } from '@axelor/aos-mobile-core';
 import {OperationOrder} from '../../../types';
 import {updateOperationOrder} from '../../../features/operationOrderSlice';
+import {StyleSheet, View} from 'react-native';
 
 const OperationOrderStopwatch = ({}) => {
   const I18n = useTranslator();
   const dispatch = useDispatch();
 
-  const {operationOrder} = useSelector(state => state.operationOrder);
+  const {operationOrder, updateMessage} = useSelector(
+    state => state.operationOrder,
+  );
   const {user} = useSelector(state => state.user);
+
+  const [labelVisible, setLabelVisible] = useState(false);
 
   const {status: timerStatus, time} = useMemo(() => {
     if (!isEmpty(operationOrder)) {
@@ -55,21 +61,50 @@ const OperationOrderStopwatch = ({}) => {
     [dispatch, operationOrder],
   );
 
+  useEffect(() => {
+    setLabelVisible(updateMessage?.message != null);
+  }, [updateMessage]);
+
   return (
-    <Stopwatch
-      startTime={time}
-      status={timerStatus}
-      timerFormat={I18n.t('Stopwatch_TimerFormat')}
-      onPlay={() => updateStatus(OperationOrder.status.InProgress)}
-      onPause={() => updateStatus(OperationOrder.status.StandBy)}
-      onStop={() => updateStatus(OperationOrder.status.Finished)}
-      disableStop={
-        timerStatus !== StopwatchType.status.InProgress ||
-        timerStatus !== StopwatchType.status.Paused
-      }
-      hideCancel={true}
-    />
+    <View style={styles.container}>
+      <Label
+        style={styles.label}
+        message={updateMessage?.message}
+        type={updateMessage?.code !== 200 ? 'error' : 'info'}
+        showClose={true}
+        visible={labelVisible}
+        onClose={() => setLabelVisible(false)}
+      />
+      <Stopwatch
+        style={styles.timer}
+        startTime={time}
+        status={timerStatus}
+        timerFormat={I18n.t('Stopwatch_TimerFormat')}
+        onPlay={() => updateStatus(OperationOrder.status.InProgress)}
+        onPause={() => updateStatus(OperationOrder.status.StandBy)}
+        onStop={() => updateStatus(OperationOrder.status.Finished)}
+        disableStop={
+          timerStatus !== StopwatchType.status.InProgress &&
+          timerStatus !== StopwatchType.status.Paused
+        }
+        hideCancel={true}
+        useObjectStatus={true}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: '30%',
+  },
+  label: {
+    width: '90%',
+    marginLeft: 20,
+  },
+  timer: {
+    marginVertical: 5,
+  },
+});
 
 export default OperationOrderStopwatch;
