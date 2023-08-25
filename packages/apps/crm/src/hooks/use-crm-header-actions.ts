8f00ec52c0202ac0fcc639f7a6f3026c58af4bd6
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-08-25 09:16:00 +0100

    feat: add header action to save contact for crm lead, prospects, clients, and contacts

--- packages/apps/crm/src/hooks/use-crm-header-actions.ts
+++ packages/apps/crm/src/hooks/use-crm-header-actions.ts
@@ -17,6 +17,7 @@
  */
 
 import {
+  contactProvider,
   headerActionsProvider,
   useSelector,
   useNavigation,
@@ -53,6 +54,7 @@ const useCatalogListActions = () => {
 const useClientDetailsActions = () => {
   const {mobileSettings} = useSelector((state: any) => state.config);
   const {client} = useSelector((state: any) => state.client);
+  const I18n = useTranslator();
 
   useEffect(() => {
     headerActionsProvider.registerModel('crm_client_details', {
@@ -60,13 +62,38 @@ const useClientDetailsActions = () => {
       modelId: client?.id,
       disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
       attachedFileScreenTitle: client?.simpleFullName,
+      actions: [
+        {
+          key: 'client-saveContact',
+          order: 10,
+          iconName: 'user-plus',
+          title: I18n.t('Crm_SaveContact'),
+          onPress: () =>
+            contactProvider.saveContact({
+              firstName: client.simpleFullName,
+              mobilePhone: client.mobilePhone,
+              fixedPhone: client.fixedPhone,
+              email: client.emailAddress?.address,
+              address: {
+                street:
+                  client.mainAddress?.addressL4 ?? client.mainAddress?.fullName,
+                country: client.mainAddress?.addressL7Country,
+                city: client.mainAddress?.primaryCity,
+                postCode: client.mainAddress?.zip,
+              },
+              notes: client.notes,
+            }),
+          showInHeader: true,
+        },
+      ],
     });
-  }, [mobileSettings, client]);
+  }, [mobileSettings, client, I18n]);
 };
 
 const useContactDetailsActions = () => {
   const {mobileSettings} = useSelector((state: any) => state.config);
   const {contact} = useSelector((state: any) => state.contact);
+  const I18n = useTranslator();
 
   useEffect(() => {
     headerActionsProvider.registerModel('crm_contact_details', {
@@ -74,8 +101,34 @@ const useContactDetailsActions = () => {
       modelId: contact?.id,
       disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
       attachedFileScreenTitle: contact?.simpleFullName,
+      actions: [
+        {
+          key: 'contact-saveContact',
+          order: 10,
+          iconName: 'user-plus',
+          title: I18n.t('Crm_SaveContact'),
+          onPress: () =>
+            contactProvider.saveContact({
+              firstName: contact.firstName,
+              lastName: contact.name,
+              mobilePhone: contact.mobilePhone,
+              fixedPhone: contact.fixedPhone,
+              email: contact.emailAddress?.address,
+              address: {
+                street:
+                  contact.mainAddress?.addressL4 ??
+                  contact.mainAddress?.fullName,
+                country: contact.mainAddress?.addressL7Country,
+                city: contact.mainAddress?.primaryCity,
+                postCode: contact.mainAddress?.zip,
+              },
+              notes: contact.notes,
+            }),
+          showInHeader: true,
+        },
+      ],
     });
-  }, [mobileSettings, contact]);
+  }, [mobileSettings, contact, I18n]);
 };
 
 const useLeadListActions = () => {
@@ -101,6 +154,7 @@ const useLeadListActions = () => {
 };
 
 const useLeadDetailsActions = () => {
+  const I18n = useTranslator();
   const {mobileSettings} = useSelector((state: any) => state.config);
   const {lead} = useSelector((state: any) => state.lead);
 
@@ -110,8 +164,33 @@ const useLeadDetailsActions = () => {
       modelId: lead?.id,
       disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
       attachedFileScreenTitle: lead?.simpleFullName,
+      actions: [
+        {
+          key: 'lead-saveContact',
+          order: 10,
+          iconName: 'user-plus',
+          title: I18n.t('Crm_SaveContact'),
+          onPress: () =>
+            contactProvider.saveContact({
+              firstName: lead.firstName,
+              lastName: lead.name,
+              mobilePhone: lead.mobilePhone,
+              fixedPhone: lead.fixedPhone,
+              email: lead.emailAddress?.address,
+              address: {
+                street:
+                  lead.mainAddress?.addressL4 ?? lead.mainAddress?.fullName,
+                country: lead.mainAddress?.addressL7Country,
+                city: lead.mainAddress?.primaryCity,
+                postCode: lead.mainAddress?.zip,
+              },
+              notes: lead.notes,
+            }),
+          showInHeader: true,
+        },
+      ],
     });
-  }, [mobileSettings, lead]);
+  }, [mobileSettings, lead, I18n]);
 };
 
 const useOpportunityListActions = () => {
@@ -153,6 +232,7 @@ const useOpportunityDetailsActions = () => {
 const useProspectDetailsActions = () => {
   const {mobileSettings} = useSelector((state: any) => state.config);
   const {prospect} = useSelector((state: any) => state.prospect);
+  const I18n = useTranslator();
 
   useEffect(() => {
     headerActionsProvider.registerModel('crm_prospect_details', {
@@ -160,8 +240,33 @@ const useProspectDetailsActions = () => {
       modelId: prospect?.id,
       disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
       attachedFileScreenTitle: prospect?.simpleFullName,
+      actions: [
+        {
+          key: 'contact-saveContact',
+          order: 10,
+          iconName: 'user-plus',
+          title: I18n.t('Crm_SaveContact'),
+          onPress: () =>
+            contactProvider.saveContact({
+              firstName: prospect.simpleFullName,
+              mobilePhone: prospect.mobilePhone,
+              fixedPhone: prospect.fixedPhone,
+              email: prospect.emailAddress?.address,
+              address: {
+                street:
+                  prospect.mainAddress?.addressL4 ??
+                  prospect.mainAddress?.fullName,
+                country: prospect.mainAddress?.addressL7Country,
+                city: prospect.mainAddress?.primaryCity,
+                postCode: prospect.mainAddress?.zip,
+              },
+              notes: prospect.notes,
+            }),
+          showInHeader: true,
+        },
+      ],
     });
-  }, [mobileSettings, prospect]);
+  }, [mobileSettings, prospect, I18n]);
 };
 
 const useEventDetailsActions = () => {
