bd2bddf4e0de90ff2b162aa54c05a64c1b487345
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-09-14 11:33:04 +0100

    fix: application test * add enzyme lib for ui testing  * complete mock external libraries  * add jest-ts transformer * add axelor module name mapper to map axelor packages with there implementation refrerence  * mock schema constructor

--- jest/setup.js
+++ jest/setup.js
@@ -1,12 +1,11 @@
 import 'react-native-gesture-handler/jestSetup';
+import 'react-native/jest/setup';
 
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-  Reanimated.default.call = () => {};
-  return Reanimated;
-});
-
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native-reanimated', () => ({
+  default: {
+    call: jest.fn(),
+  },
+}));
 
 jest.useFakeTimers();
 
@@ -25,4 +24,91 @@ jest.mock('react-native-fs', () => ({
 
 jest.mock('react-native-device-info', () => ({
   getManufacturer: 'FAKE-MANUFACTURER',
+}));
+
+jest.mock(
+  'react-native-toast-message',
+  () => ({
+    show: jest.fn(),
+    BaseToast: 'BaseToastMock',
+    ErrorToast: 'ErrorToastMock',
+  }),
+  {virtual: true},
+);
+
+jest.mock(
+  'react-redux',
+  () => ({
+    useSelector: jest.fn(),
+  }),
+  {virtual: true},
+);
+
+jest.mock(
+  'react-native-mmkv',
+  () => ({
+    MMKV: () => ({
+      set: jest.fn(),
+      getString: jest.fn(),
+      clearAll: jest.fn(),
+    }),
+  }),
+  {virtual: true},
+);
+
+jest.mock('react-native-system-navigation-bar', () => ({
+  navigationHide: () => jest.fn(),
+}));
+
+jest.mock('react-native-static-safe-area-insets', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  getSafeAreaInsets: jest.fn(() => ({
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  })),
+}));
+
+jest.mock('react-native-calendars', () => ({
+  Agenda: jest.fn(),
+  AgendaEntry: jest.fn(),
+  DateData: jest.fn(),
+}));
+
+jest.mock('react-native-date-picker', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('react-native/Libraries/StyleSheet/StyleSheet', () => ({
+  create: jest.fn(() => ({})),
+}));
+
+jest.mock('rn-fetch-blob', () => ({
+  DocumentDir: 'FAKE-DIRECTORY-PATH',
+}));
+
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+
+jest.mock('react-native-webview', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('react-native-system-navigation-bar', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('react-native-pell-rich-editor', () => ({
+  actions: jest.fn(),
+  RichEditor: jest.fn(),
+  RichToolbar: jest.fn(),
+}));
+
+jest.mock('react-native-vector-icons/FontAwesome', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('react-native-vector-icons/FontAwesome5', () => ({
+  default: jest.fn(),
 }));
