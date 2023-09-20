1643b895dd2410459bbf8795b69e323ef68fbe68
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-09-20 17:15:48 +0200

    feat: manage kap on Date (#147)
    * RM#68732

--- packages/apps/hr/src/models/forms.ts
+++ packages/apps/hr/src/models/forms.ts
@@ -25,6 +25,7 @@ import {
   ToggleSwitchMode,
 } from '../components';
 import {ExpenseLine} from '../types';
+import {updateExpenseDate} from '../features/kilometricAllowParamSlice';
 
 export const hr_formsRegister: FormConfigs = {
   hr_Expenseline: {
@@ -77,6 +78,12 @@ export const hr_formsRegister: FormConfigs = {
           objectState.manageMode === ExpenseLine.modes.general,
         requiredIf: ({objectState}) =>
           objectState.manageMode === ExpenseLine.modes.kilometric,
+        dependsOn: {
+          fieldName: 'expenseDate',
+          onChange: ({newValue, dispatch}) => {
+            dispatch(updateExpenseDate(newValue));
+          },
+        },
       },
       kilometricTypeSelect: {
         titleKey: 'Hr_KilometricTypeSelect',
