710339aba3371935ec0d93a38e63c500206afb34
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-09-29 10:44:18 +0100

    fix: application test * add enzyme lib for ui testing * complete mock external libraries * add jest-ts transformer * add axelor module name mapper to map axelor packages with there implementation refrerence * mock schema constructor

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
+const {modulePackageNameMapping} = require('./axelor-module-name-mapper');
+
 module.exports = {
   preset: 'react-native',
+  testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['<rootDir>/jest/setup.js'],
+  setupFiles: ['./jest/setup-env.js', './jest/setup-mocks.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-native-toast-message|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
+    'node_modules/(?!(@react-native|(jest-)?react-native|react-native-toast-message|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fontawesome|@react-native|@react-navigation|react-native-pell-rich-editor|react-native-vector-icons/FontAwesome|react-native-vector-icons/FontAwesome5)/|react-native-webview)',
   ],
+  moduleNameMapper: modulePackageNameMapping,
 };
---
+++ jest/setup-env.js
@@ -0,0 +1,27 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Adapter from 'enzyme-adapter-react-16';
+import {configure} from 'enzyme';
+import {TextEncoder} from 'text-encoding';
+
+configure({adapter: new Adapter()});
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
