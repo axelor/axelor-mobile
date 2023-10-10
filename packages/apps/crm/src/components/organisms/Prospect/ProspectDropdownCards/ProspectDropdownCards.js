fd8aac8c68170c9faae34b3812ee83b861733c83
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-10-10 08:11:22 +0100

    feat: display last opportunity on prospect&client (#145)
    * RM#66968

--- packages/apps/crm/src/components/organisms/Prospect/ProspectDropdownCards/ProspectDropdownCards.js
+++ packages/apps/crm/src/components/organisms/Prospect/ProspectDropdownCards/ProspectDropdownCards.js
@@ -25,6 +25,7 @@ import {
   DropdownEmployeeView,
   DropdownEventView,
   DropdownGeneralView,
+  DropdownOpportunityView,
 } from '../../../organisms';
 import {searchContactById} from '../../../../features/contactSlice';
 import {fetchPartnerEventById} from '../../../../features/eventSlice';
@@ -85,6 +86,11 @@ const ProspectDropdownCards = ({}) => {
             title: I18n.t('Crm_Events'),
             key: 4,
             childrenComp: <DropdownEventView eventList={listEventPartner} />,
+          },
+          {
+            title: I18n.t('Crm_Opportunity'),
+            key: 5,
+            childrenComp: <DropdownOpportunityView partnerId={prospect?.id} />,
           },
         ]}
       />
