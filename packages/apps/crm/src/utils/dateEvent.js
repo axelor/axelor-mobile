664dc0a5ded876fded35313f8e1a80fabf929915
hel-axelor <101327380+hel-axelor@users.noreply.github.com>
2023-10-10 08:11:22 +0100

    feat: display last opportunity on prospect&client (#145)
    * RM#66968

--- packages/apps/crm/src/utils/dateEvent.js
+++ packages/apps/crm/src/utils/dateEvent.js
@@ -16,30 +16,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {getLastItem, getNextItem} from './list';
+
 export const getLastEvent = listEvent => {
-  const today = new Date();
-  if (listEvent && listEvent.length > 0) {
-    const filtredList = listEvent.filter(
-      event => new Date(event.startDateTime) < today,
-    );
-    const getLatest = arr =>
-      arr.sort(
-        (a, b) => new Date(b.startDateTime) - new Date(a.startDateTime),
-      )[0];
-    return getLatest(filtredList);
-  }
+  return getLastItem(listEvent, 'startDateTime');
 };
 
 export const getNextEvent = listEvent => {
-  const today = new Date();
-  if (listEvent && listEvent.length > 0) {
-    const filtredList = listEvent.filter(
-      event => new Date(event.startDateTime) > today,
-    );
-    const getRecent = arr =>
-      arr.sort(
-        (a, b) => new Date(a.startDateTime) - new Date(b.startDateTime),
-      )[0];
-    return getRecent(filtredList);
-  }
+  return getNextItem(listEvent, 'startDateTime');
 };
