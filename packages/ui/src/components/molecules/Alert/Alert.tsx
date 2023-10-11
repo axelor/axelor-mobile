a3bf371bd36648189280be0f41fc21f511d9f321
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-10-11 11:13:52 +0200

    feat: creation of new Alert component (#181)
    * RM#69666

---
+++ packages/ui/src/components/molecules/Alert/Alert.tsx
@@ -0,0 +1,170 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {StyleSheet, View, Modal} from 'react-native';
+import {useThemeColor} from '../../../theme';
+import {Card, Text, Icon} from '../../atoms';
+import Button, {ButtonProps} from '../Button/Button';
+import {checkNullString} from '../../../utils';
+
+const DEFAULT_BUTTON_WIDTH = 115;
+
+interface ButtonConfig extends ButtonProps {
+  hide?: boolean;
+}
+interface CancelButtonConfig extends ButtonConfig {
+  showInHeader?: boolean;
+}
+
+interface AlertProps {
+  style?: any;
+  visible: boolean;
+  title?: string;
+  children: any;
+  cancelButtonConfig?: CancelButtonConfig;
+  confirmButtonConfig?: ButtonConfig;
+  translator?: (key: string) => string;
+}
+
+const Alert = ({
+  style,
+  visible = false,
+  title,
+  children,
+  cancelButtonConfig,
+  confirmButtonConfig,
+  translator = key => key,
+}: AlertProps) => {
+  const Colors = useThemeColor();
+
+  const _cancelButtonConfig = useMemo(() => {
+    if (!cancelButtonConfig) {
+      return null;
+    }
+
+    return {
+      title: translator('Base_Cancel'),
+      color: Colors.errorColor,
+      iconName: 'times',
+      hide: false,
+      showInHeader: false,
+      width: DEFAULT_BUTTON_WIDTH,
+      ...cancelButtonConfig,
+    };
+  }, [translator, Colors, cancelButtonConfig]);
+
+  const _confirmButtonConfig = useMemo(() => {
+    if (!confirmButtonConfig) {
+      return null;
+    }
+
+    return {
+      title: translator('Base_OK'),
+      color: Colors.primaryColor,
+      iconName: 'check',
+      hide: false,
+      width: DEFAULT_BUTTON_WIDTH,
+      ...confirmButtonConfig,
+    };
+  }, [translator, Colors, confirmButtonConfig]);
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="fade"
+      onRequestClose={_cancelButtonConfig?.onPress}>
+      <View style={styles.modalBackground}>
+        <Card style={[styles.container, style]}>
+          <View style={styles.headerContainer}>
+            {!checkNullString(title) && (
+              <Text writingType="title" fontSize={20}>
+                {title}
+              </Text>
+            )}
+            {!_cancelButtonConfig?.hide &&
+              _cancelButtonConfig?.showInHeader && (
+                <Icon
+                  name="times"
+                  size={20}
+                  style={styles.headerCancelButton}
+                  touchable
+                  onPress={_cancelButtonConfig?.onPress}
+                />
+              )}
+          </View>
+          {children}
+          {(!_cancelButtonConfig?.hide || !_confirmButtonConfig?.hide) && (
+            <View style={styles.buttonsContainer}>
+              {!_cancelButtonConfig?.hide &&
+                !_cancelButtonConfig?.showInHeader && (
+                  <Button
+                    {..._cancelButtonConfig}
+                    style={styles.cancelButton}
+                  />
+                )}
+              {!_confirmButtonConfig?.hide && (
+                <Button {..._confirmButtonConfig} />
+              )}
+            </View>
+          )}
+        </Card>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  container: {
+    width: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingRight: 15,
+    paddingVertical: 15,
+  },
+  headerContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  headerCancelButton: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  buttonsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 10,
+  },
+  cancelButton: {
+    marginRight: 10,
+  },
+});
+
+export default Alert;
