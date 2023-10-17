187b4ce232a89215cc18810d6201148968c0e38e
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-10-17 17:05:31 +0200

    fix: product unit display (#193)
    * RM#70434

--- packages/apps/stock/src/components/organisms/SmallPropertyCard/SmallPropertyCard.tsx
+++ packages/apps/stock/src/components/organisms/SmallPropertyCard/SmallPropertyCard.tsx
@@ -30,6 +30,7 @@ interface SmallPropertyCardProps {
   style?: any;
   title: string;
   value: string;
+  formatValueToNumber?: boolean;
   unit?: string;
   interactive?: boolean;
 }
@@ -38,6 +39,7 @@ const SmallPropertyCard = ({
   style,
   title,
   value,
+  formatValueToNumber = true,
   unit = null,
   interactive = false,
 }: SmallPropertyCardProps) => {
@@ -49,7 +51,10 @@ const SmallPropertyCard = ({
     setPopUp(true);
   };
 
-  const _value = useMemo(() => formatNumber(value), [formatNumber, value]);
+  const _value = useMemo(
+    () => (formatValueToNumber ? formatNumber(value) : value),
+    [formatNumber, formatValueToNumber, value],
+  );
 
   return (
     <Card style={[styles.card, style]}>
