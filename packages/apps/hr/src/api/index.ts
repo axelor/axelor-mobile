7c4ef28a0c19dc2e1105ee12d6b6409b2d3f0c59
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-10-20 14:11:35 +0200

    fix: export content of hr package to allow external package to use it (#203)
    * RM#70558

---
+++ packages/apps/hr/src/api/index.ts
@@ -0,0 +1,41 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export {searchCurrencies as searchCurrenciesApi} from './currency-api';
+export {
+  createExpense as createExpenseApi,
+  getExpense,
+  refuseExpense as refuseExpenseApi,
+  searchExpenseDraft as searchExpenseDraftApi,
+  searchExpenseToValidate as searchExpenseToValidateApi,
+  searchMyExpense as searchMyExpenseApi,
+  sendExpense as sendExpenseApi,
+  updateExpense as updateExpenseApi,
+  validateExpense as validateExpenseApi,
+} from './expense-api';
+export {
+  createExpenseLine as createExpenseLineApi,
+  deleteExpenseLine as deleteExpenseLineApi,
+  searchExpenseLines as searchExpenseLinespi,
+  searchGeneralExpenseLines as searchGeneralExpenseLinesApi,
+  searchKilometricExpenseLines as searchKilometricExpenseLinesApi,
+  updateExpenseLine as updateExpenseLineApi,
+} from './expense-line-api';
+export {searchExpenseType as searchExpenseTypeApi} from './expense-type-api';
+export {searchKilometricAllowParam as searchKilometricAllowParamApi} from './kilometric-allow-param-api';
+export {searchProject as searchProjectApi} from './project-api';
