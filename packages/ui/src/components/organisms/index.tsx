c3f0581667a742f4d2166e1332243af4c8ccfce6
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-10-20 08:41:50 +0200

    refactor: remove PopUp, PopUpOneButton and PopUpTwoButton components (#206)
    * RM#70214
    * This component should be replaced by Alert component from this version

--- packages/ui/src/components/organisms/index.tsx
+++ packages/ui/src/components/organisms/index.tsx
@@ -26,8 +26,6 @@ export {default as ImageBubble} from './ImageBubble/ImageBubble';
 export {default as MultiSelectValue} from './MultiSelectValue/MultiSelectValue';
 export {default as MultiValuePicker} from './MultiValuePicker/MultiValuePicker';
 export {default as Picker} from './Picker/Picker';
-export {default as PopUpOneButton} from './PopUpOneButton/PopUpOneButton';
-export {default as PopUpTwoButton} from './PopUpTwoButton/PopUpTwoButton';
 export {default as ProgressBar} from './ProgressBar/ProgressBar';
 export {default as ScrollList} from './ScrollList/ScrollList';
 export {default as SearchBar} from './SearchBar/SearchBar';
