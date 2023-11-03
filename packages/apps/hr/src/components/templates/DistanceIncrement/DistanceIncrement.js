5f79bb57814186372d2e17105fd924ea0b4b24d0
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-11-03 13:46:17 +0100

    feat: compute distance with API on ExpenseLine form (#195)
    * RM#68577

---
+++ packages/apps/hr/src/components/templates/DistanceIncrement/DistanceIncrement.js
@@ -0,0 +1,88 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {useSelector} from '@axelor/aos-mobile-core';
+import {FormIncrementInput} from '@axelor/aos-mobile-ui';
+
+const DistanceIncrementAux = ({
+  title = 'Hr_Distance',
+  defaultValue = null,
+  onChange = () => {},
+  readonly = false,
+  required = false,
+}) => {
+  const [value, setValue] = useState(defaultValue);
+
+  const {distance} = useSelector(state => state.distance);
+
+  useEffect(() => {
+    if (distance != null) {
+      setValue(Number(distance));
+    }
+  }, [distance]);
+
+  const handleChange = useCallback(
+    e => {
+      setValue(e);
+      onChange(e);
+    },
+    [onChange],
+  );
+
+  return (
+    <FormIncrementInput
+      title={title}
+      style={styles.input}
+      onChange={handleChange}
+      defaultValue={value}
+      readOnly={readonly}
+      required={required}
+    />
+  );
+};
+
+const DistanceIncrement = ({
+  style,
+  title,
+  defaultValue,
+  onChange,
+  readOnly,
+  required,
+}) => {
+  return (
+    <DistanceIncrementAux
+      defaultValue={defaultValue}
+      onChange={onChange}
+      style={style}
+      title={title}
+      readonly={readOnly}
+      required={required}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: '90%',
+    alignSelf: 'center',
+  },
+});
+
+export default DistanceIncrement;
---
+++ packages/apps/hr/src/features/distanceSlice.js
@@ -0,0 +1,82 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {handlerApiCall, isEmpty} from '@axelor/aos-mobile-core';
+import {getDistance as _getDistance} from '../api/distance-api';
+
+export const getDistance = createAsyncThunk(
+  'distance/getDistance',
+  async function (data = {}, {getState}) {
+    return handlerApiCall({
+      fetchFunction: _getDistance,
+      data,
+      action: 'Hr_SliceAction_GetDistance',
+      getState,
+      responseOptions: {isArrayResponse: false},
+      errorOptions: {errorTracing: false, showErrorToast: true},
+    });
+  },
+);
+
+const initialState = {
+  loadingDistance: true,
+  distance: null,
+  fromCity: null,
+  toCity: null,
+  showCityError: false,
+  needUpdateDistance: false,
+};
+
+const distanceSlice = createSlice({
+  name: 'distance',
+  initialState,
+  reducers: {
+    updateFromCity: (state, action) => {
+      state.fromCity = action.payload;
+    },
+    updateToCity: (state, action) => {
+      state.toCity = action.payload;
+    },
+    resetDistance: state => {
+      state.distance = null;
+    },
+    needUpdateDistance: (state, action) => {
+      state.needUpdateDistance = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(getDistance.pending, (state, action) => {
+      state.loadingDistance = true;
+    });
+    builder.addCase(getDistance.fulfilled, (state, action) => {
+      state.loadingDistance = false;
+      if (action.payload?.error) {
+        state.showCityError = true;
+      } else if (!isEmpty(action.payload?.distance)) {
+        state.distance = action.payload?.distance;
+      } else {
+        state.distance = null;
+      }
+    });
+  },
+});
+export const {updateFromCity, updateToCity, resetDistance, needUpdateDistance} =
+  distanceSlice.actions;
+
+export const distanceReducer = distanceSlice.reducer;
