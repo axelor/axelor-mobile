7da18f974b7d52967e374826c1d285bb002eac2d
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-11-08 09:42:35 +0100

    feat: remove useless test

--- packages/ui/__tests__/components/molecules/SwitchCard.test.js
+++ packages/ui/__tests__/components/molecules/SwitchCard.test.js
@@ -1,6 +1,24 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import {shallow} from 'enzyme';
-import {SwitchCard, Text} from '@axelor/aos-mobile-ui';
+import {SwitchCard, Text, Switch} from '@axelor/aos-mobile-ui';
 
 describe('SwitchCard', () => {
   it('should render without crashing', () => {
@@ -39,9 +57,6 @@ describe('SwitchCard', () => {
       <SwitchCard title="Test" defaultValue={true} onToggle={onPress} />,
     );
 
-    wrapper.find('Switch').prop('handleToggle')();
-
-    //wrapper.find(Switch).simulate('press');
-    expect(onPress).toHaveBeenCalled();
+    expect(wrapper.find('Switch').prop('handleToggle')).toBe(onPress);
   });
 });
