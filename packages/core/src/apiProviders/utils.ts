ab4dbdfaf68ce3eeb0c1da33b831a3d1694b69f6
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-11-09 13:42:55 +0100

    fix: manage AOP errors on API request (#245)
    * RM#71686

--- packages/core/src/apiProviders/utils.ts
+++ packages/core/src/apiProviders/utils.ts
@@ -81,7 +81,7 @@ const manageError = (
       });
     }
 
-    return error.response;
+    return null;
   }
 };
 
@@ -163,7 +163,18 @@ export const handlerApiCall = ({
   errorOptions = {showErrorToast: true, errorTracing: true},
 }: ApiHandlerProps) => {
   return fetchFunction(data)
-    .then(handlerSuccess(action, responseOptions))
+    .then(res => {
+      if (res?.data?.status === -1) {
+        throw {
+          response: {
+            status: 403,
+            statusText: `${res.data.data?.title}: ${res.data.data?.message}`,
+          },
+        };
+      } else {
+        return handlerSuccess(action, responseOptions)(res);
+      }
+    })
     .catch(handlerError(action, {getState}, errorOptions));
 };
 
