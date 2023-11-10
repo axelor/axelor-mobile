af07303878ac6b6c58869e7623f0ee330b5805ec
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-10 10:12:51 +0100

    feat: add test cases for ViewAllContainer component

---
+++ packages/ui/__tests__/components/molecules/ViewAllContainer.test.js
@@ -0,0 +1,107 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {shallow} from 'enzyme';
+import {Card, Icon, Text, ViewAllContainer} from '@axelor/aos-mobile-ui';
+import {getGlobalStyles} from '../../tools';
+
+describe('ViewAllContainer Component', () => {
+  const props = {
+    children: <View testID="children" />,
+    onViewPress: jest.fn(),
+  };
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<ViewAllContainer {...props} />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should render children', () => {
+    const wrapper = shallow(<ViewAllContainer {...props} />);
+
+    expect(wrapper.find('[testID="children"]').exists()).toBe(true);
+  });
+
+  it('should call onViewPress when TouchableOpacity is pressed', () => {
+    const wrapper = shallow(<ViewAllContainer {...props} />);
+
+    wrapper.find(TouchableOpacity).simulate('press');
+    expect(props.onViewPress).toHaveBeenCalledTimes(1);
+  });
+
+  it('should apply custom style when provided', () => {
+    const customStyle = {width: 200};
+    const wrapper = shallow(
+      <ViewAllContainer {...props} style={customStyle} />,
+    );
+
+    expect(getGlobalStyles(wrapper.find(Card))).toMatchObject(customStyle);
+  });
+
+  it('should render title when provided', () => {
+    const title = 'Title';
+    const wrapper = shallow(<ViewAllContainer {...props} title={title} />);
+
+    expect(wrapper.find(Text).at(0).prop('children')).toBe(title);
+  });
+
+  it('should render first and second data whith renderFirstTwoItems function when provided', () => {
+    const data = ['Data 0', 'Data 1', 'Data 2'];
+    const renderFirstTwoItems = (item, index) => (
+      <Text>
+        `${index} : ${item}`
+      </Text>
+    );
+    const wrapper = shallow(
+      <ViewAllContainer
+        {...props}
+        data={data}
+        renderFirstTwoItems={renderFirstTwoItems}
+      />,
+    );
+
+    expect(wrapper.find(View).at(0).find(Text).length).toBe(2);
+  });
+
+  it('should not render TouchableOpacity when disabled is true', () => {
+    const wrapper = shallow(<ViewAllContainer {...props} disabled />);
+
+    expect(wrapper.find(TouchableOpacity).length).toBe(0);
+  });
+
+  it('should render a top View with Text and plus Icon when isHeaderExist is true', () => {
+    const wrapper = shallow(<ViewAllContainer {...props} isHeaderExist />);
+
+    expect(wrapper.find(View).at(0).find(Text).length).toBe(1);
+    expect(wrapper.find(View).at(0).find(Icon).length).toBe(1);
+    expect(wrapper.find(View).at(0).find(Icon).prop('name')).toBe('plus');
+  });
+
+  it('should call onNewIcon when plus Icon is pressed', () => {
+    const onNewIcon = jest.fn();
+    const wrapper = shallow(
+      <ViewAllContainer {...props} isHeaderExist onNewIcon={onNewIcon} />,
+    );
+
+    wrapper.find(View).at(0).find(Icon).simulate('press');
+    expect(onNewIcon).toHaveBeenCalledTimes(1);
+  });
+});
