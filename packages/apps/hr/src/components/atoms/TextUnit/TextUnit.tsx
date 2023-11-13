210f4c5113e2b2aa18171d0596ef91c13ac04bc0
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-13 15:31:09 +0100

    feat: creation of TimeSheetLineCard (#221)
    * RM#71300

---
+++ packages/apps/hr/src/components/atoms/TextUnit/TextUnit.tsx
@@ -0,0 +1,58 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {StyleSheet} from 'react-native';
+import {Color, Text, useThemeColor} from '@axelor/aos-mobile-ui';
+
+interface TextUnitProps {
+  value: string;
+  unit: string;
+  color?: Color;
+  fontSize?: number;
+  style?: any;
+}
+
+const TextUnit = ({
+  value,
+  unit,
+  color,
+  fontSize = 22,
+  style,
+}: TextUnitProps) => {
+  const Colors = useThemeColor();
+
+  const _color = useMemo(() => color || Colors.primaryColor, [color, Colors]);
+
+  return (
+    <Text
+      textColor={_color.background}
+      fontSize={fontSize}
+      style={[styles.text, style]}>
+      {value} {unit}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: '900',
+  },
+});
+
+export default TextUnit;
