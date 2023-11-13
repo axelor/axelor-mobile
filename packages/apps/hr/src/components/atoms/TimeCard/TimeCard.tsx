98c617e88cd3db5fe357092cf5c7d701ce85c8c0
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-13 15:31:09 +0100

    feat: creation of TimeSheetLineCard (#221)
    * RM#71300

---
+++ packages/apps/hr/src/components/atoms/TimeCard/TimeCard.tsx
@@ -0,0 +1,135 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {ObjectCard, useThemeColor} from '@axelor/aos-mobile-ui';
+import {useTranslator} from '@axelor/aos-mobile-core';
+import DateDisplay from '../DateDisplay/DateDisplay';
+import TextUnit from '../TextUnit/TextUnit';
+import {Time} from '../../../types';
+
+interface TimeCardProps {
+  statusSelect: number;
+  project?: string;
+  task?: string;
+  manufOrder?: string;
+  operation?: string;
+  comment?: string;
+  date: string;
+  duration: string;
+  durationUnit: string;
+  isBorderColor?: boolean;
+  style?: any;
+}
+
+const TimeCard = ({
+  statusSelect,
+  project,
+  task,
+  manufOrder,
+  operation,
+  comment,
+  date,
+  duration,
+  durationUnit,
+  isBorderColor = true,
+  style,
+}: TimeCardProps) => {
+  const I18n = useTranslator();
+  const Colors = useThemeColor();
+
+  const styles = useMemo(
+    () => getStyles(Time.getStatusColor(statusSelect, Colors)),
+    [Colors, statusSelect],
+  );
+
+  return (
+    <View style={style}>
+      <ObjectCard
+        touchable={false}
+        showArrow={false}
+        leftContainerFlex={1.3}
+        style={[styles.container, isBorderColor && styles.borderColor]}
+        upperTexts={{
+          items: [
+            {
+              displayText: (project || manufOrder) ?? '-',
+              isTitle: true,
+              numberOfLines: 2,
+            },
+            {
+              displayText: (task || operation) ?? '-',
+              numberOfLines: 2,
+              style: styles.subTitle,
+            },
+            {
+              displayText: comment,
+              numberOfLines: 2,
+              hideIfNull: true,
+              style: styles.subTitle,
+            },
+          ],
+        }}
+        sideBadges={{
+          style: styles.badges,
+          items: [
+            {
+              customComponent: <DateDisplay date={date} />,
+            },
+            {
+              customComponent: (
+                <TextUnit
+                  value={duration}
+                  unit={Time.getDurationUnit(durationUnit, I18n)}
+                  style={styles.textUnit}
+                />
+              ),
+            },
+          ],
+        }}
+      />
+    </View>
+  );
+};
+
+const getStyles = color =>
+  StyleSheet.create({
+    container: {
+      minHeight: 100,
+      justifyContent: 'center',
+      marginHorizontal: 1,
+      marginVertical: 1,
+    },
+    borderColor: {
+      borderLeftWidth: 7,
+      borderLeftColor: color.background,
+    },
+    subTitle: {
+      fontStyle: 'italic',
+      marginTop: 5,
+    },
+    badges: {
+      alignItems: 'flex-end',
+    },
+    textUnit: {
+      marginTop: 5,
+    },
+  });
+
+export default TimeCard;
