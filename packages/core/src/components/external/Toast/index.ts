74c70c679382d1e254d3e784f0b6c3849b08b3ce
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-14 11:12:25 +0100

    feat: toast open pop-up with the whole message (#214)
    * RM#65391

--- packages/core/src/components/external/Toast/index.ts
+++ packages/core/src/components/external/Toast/index.ts
@@ -16,12 +16,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {default as Camera} from './Camera/Camera';
-export {default as CameraScanner} from './CameraScanner/CameraScanner';
-export {default as CodeHighlighter} from './CodeHighlighter/CodeHighlighter';
-export * from './CodeHighlighter/code-highlighter.helper';
-export {default as PlanningView} from './PlanningView/PlanningView';
-export {default as Scanner} from './Scanner/Scanner';
-export {default as Stopwatch} from './Stopwatch/Stopwatch';
-export {default as Timer} from './Timer/Timer';
-export * from './Toast';
+export {default as Toast} from './Toast';
+export {default as toastProvider} from './ToastProvider';
