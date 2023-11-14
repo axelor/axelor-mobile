dd7846806a35e8073eed8f9e34994a0d379b9727
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-14 13:06:40 +0100

    test: add cases for WarningCard component (#247)
    * RM#71784

---
+++ packages/ui/__tests__/components/molecules/WarningCard.test.js
@@ -0,0 +1,45 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import {Label, WarningCard} from '@axelor/aos-mobile-ui';
+import {getGlobalStyles} from '../../tools';
+
+describe('WarningCard Component', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<WarningCard />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should apply custom style when provided', () => {
+    const customStyle = {width: 200};
+    const wrapper = shallow(<WarningCard style={customStyle} />);
+
+    expect(getGlobalStyles(wrapper.find(Label))).toMatchObject(customStyle);
+  });
+
+  it('should render errorMessage when provided', () => {
+    const errorMessage = 'Error';
+    const wrapper = shallow(<WarningCard errorMessage={errorMessage} />);
+
+    expect(wrapper.find(Label).prop('message')).toBe(errorMessage);
+    expect(wrapper.find(Label).prop('type')).toBe('error');
+  });
+});
