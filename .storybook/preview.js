ab7d71be10ae670079c6d0740568cba62a02050f
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-11-14 10:57:35 +0100

    fix: add parameters

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -27,6 +27,16 @@ if (Platform.OS === 'web') {
   document.head.appendChild(style);
 }
 
+export const parameters = {
+  actions: {argTypesRegex: '^on[A-Z].*'},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
 export const decorators = [
   Story => (
     <OutsideAlerterProvider>
