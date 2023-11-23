e486be343897e85264e8b30636be3bfccb522900
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2023-11-23 10:52:50 +0100

    fix: display issue on SelectionContainer (#271)
    * RM#72195

--- packages/ui/src/components/organisms/AutoCompleteSearch/AutoCompleteSearch.tsx
+++ packages/ui/src/components/organisms/AutoCompleteSearch/AutoCompleteSearch.tsx
@@ -111,7 +111,7 @@ const AutoCompleteSearch = ({
 
   useEffect(() => {
     if (clickOutside === OUTSIDE_INDICATOR && displayList) {
-      inputRef.current.blur();
+      inputRef?.current?.blur();
       setDisplayList(false);
     }
   }, [clickOutside, displayList]);
@@ -138,6 +138,7 @@ const AutoCompleteSearch = ({
   const handleSelect = useCallback(
     item => {
       if (item !== null) {
+        inputRef?.current?.blur();
         setDisplayList(false);
         setSelected(true);
         setSearchText(changeScreenAfter ? '' : displayValue(item));
@@ -155,7 +156,6 @@ const AutoCompleteSearch = ({
   }, [navigate, oneFilter]);
 
   const handleClear = useCallback(() => {
-    setDisplayList(false);
     setSelected(false);
     setPreviousState(searchText);
     setSearchText('');
