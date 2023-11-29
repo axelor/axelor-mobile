b8c2efd2b5a6de3bffaccab86affd647acf40efc
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-11-29 13:55:58 +0100

    feat: add ToggleButton stories

--- packages/ui/src/components/organisms/ToggleButton/ToggleButton.tsx
+++ packages/ui/src/components/organisms/ToggleButton/ToggleButton.tsx
@@ -1,4 +1,22 @@
-import React, {useState, useCallback, useMemo} from 'react';
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useState, useCallback, useMemo, useEffect} from 'react';
 import {Color} from '../../../theme/themes';
 import {useThemeColor} from '../../../theme/ThemeContext';
 import {Button} from '../../molecules';
@@ -23,6 +41,10 @@ const ToggleButton = ({
   const Colors = useThemeColor();
 
   const [isSelected, setIsSelected] = useState(isActive);
+
+  useEffect(() => {
+    setIsSelected(isActive);
+  }, [isActive]);
 
   const _activeColor = useMemo(() => {
     return activeColor != null ? activeColor : Colors.primaryColor;
