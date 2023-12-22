3ee23d29fc1b9c92e27cd4d3e9ce9c4ff1156c18
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2023-12-22 14:03:16 +0100

    feat: add stories for Dashboard components (#324)
    * RM#73163

---
+++ packages/ui/stories/templates/Dashboard/PieChart.stories.tsx
@@ -0,0 +1,89 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import {PieChart} from '../../../src/components/templates';
+
+const datasets = [
+  {
+    label: 'T0001 - APOLLO',
+    value: 14,
+  },
+  {
+    label: 'T0002 - MICHEL Loic',
+    value: 18,
+  },
+  {
+    label: 'Loic',
+    value: 15,
+  },
+  {
+    label: 'Michel',
+    value: 18,
+  },
+  {
+    label: 'Jean',
+    value: 14,
+  },
+];
+
+storiesOf('ui/templates/Dashboard/PieChart', module)
+  .addParameters({
+    viewport: {
+      defaultViewport: 'responsive',
+    },
+  })
+  .add(
+    'Default',
+    args => {
+      return <PieChart datasets={datasets} {...args} />;
+    },
+    {
+      argTypes: {
+        widthGraph: {
+          control: {type: 'number'},
+          defaultValue: 425,
+        },
+        radius: {
+          control: {type: 'number'},
+          defaultValue: 90,
+        },
+        innerRadius: {
+          control: {type: 'number'},
+          defaultValue: 60,
+        },
+        legend: {
+          control: {type: 'boolean'},
+          defaultValue: false,
+        },
+        donut: {
+          control: {type: 'boolean'},
+          defaultValue: false,
+        },
+        showGradient: {
+          control: {type: 'boolean'},
+          defaultValue: false,
+        },
+        title: {
+          control: 'text',
+          defaultValue: 'Title',
+        },
+      },
+    },
+  );
