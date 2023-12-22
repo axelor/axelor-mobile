3ba1fbf79b06b73ef12a2ebe6a4f74e965cf820a
Gregory <117281520+gca-axelor@users.noreply.github.com>
2023-12-22 15:52:17 +0100

    fix: format

--- packages/apps/helpdesk/src/screens/MyTicketListScreen.js
+++ packages/apps/helpdesk/src/screens/MyTicketListScreen.js
@@ -97,13 +97,8 @@ const MyTicketListScreen = ({navigation}) => {
 
   const filterOnStatus = useCallback(
     list => {
-<<<<<<< HEAD
       if (!Array.isArray(list) || list.length === 0) {
         return [];
-=======
-      if (list == null || list?.length === 0) {
-        return list;
->>>>>>> fix: add missing searchValue on searchBar
       } else {
         if (selectedStatus.length > 0) {
           return list?.filter(item =>
