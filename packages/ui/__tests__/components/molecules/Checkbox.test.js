ee48870fc0d245b0c73b57f2a1f055e99ce4b8a9
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-01-09 10:39:24 +0100

    feat: add indicator of partial selection on CheckBoxScrollList (#331)
    * RM#73378

--- packages/ui/__tests__/components/molecules/Checkbox.test.js
+++ packages/ui/__tests__/components/molecules/Checkbox.test.js
@@ -18,7 +18,7 @@
 
 import React from 'react';
 import {shallow} from 'enzyme';
-import {Checkbox, Icon} from '@axelor/aos-mobile-ui';
+import {Checkbox, Icon, Text} from '@axelor/aos-mobile-ui';
 import {getDefaultThemeColors} from '../../tools';
 
 describe('Checkbox Component', () => {
@@ -55,7 +55,7 @@ describe('Checkbox Component', () => {
     expect(iconComponent.prop('touchable')).toBe(false);
   });
 
-  it('renders correct icon name based on isChecked prop', () => {
+  it('renders correct icon when checkbox is pressed', () => {
     const wrapper = shallow(<Checkbox {...props} />);
 
     expect(wrapper.find(Icon).prop('name')).toBe('square');
@@ -65,10 +65,22 @@ describe('Checkbox Component', () => {
     expect(wrapper.find(Icon).prop('name')).toBe('check-square-fill');
   });
 
+  it('renders correct icon if isDefaultChecked is true', () => {
+    const wrapper = shallow(<Checkbox {...props} isDefaultChecked />);
+
+    expect(wrapper.find(Icon).prop('name')).toBe('check-square-fill');
+  });
+
+  it('renders correct icon if isDefaultPartialChecked is true', () => {
+    const wrapper = shallow(<Checkbox {...props} isDefaultPartialChecked />);
+
+    expect(wrapper.find(Icon).prop('name')).toBe('dash-square-fill');
+  });
+
   it('renders with correct title', () => {
     const wrapper = shallow(<Checkbox {...props} />);
 
-    expect(wrapper.find('Text').prop('children')).toBe(props.title);
+    expect(wrapper.find(Text).prop('children')).toBe(props.title);
   });
 
   it('renders with correct icon color based on disabled prop', () => {
