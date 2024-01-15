eb892b259c351bf7949435fb8fd14de96e435176
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-01-15 15:14:12 +0100

    feat: create DurationInput component (#336)
    * RM#73740

--- packages/ui/src/components/organisms/index.tsx
+++ packages/ui/src/components/organisms/index.tsx
@@ -21,6 +21,7 @@ export {default as CheckboxScrollList} from './CheckboxScrollList/CheckboxScroll
 export {default as ChipSelect} from './ChipSelect/ChipSelect';
 export {default as DropdownCardSwitch} from './DropdownCardSwitch/DropdownCardSwitch';
 export {default as DropdownMenuItem} from './DropdownMenuItem/DropdownMenuItem';
+export {default as DurationInput} from './DurationInput/DurationInput';
 export {default as FloatingButton} from './FloatingButton/FloatingButton';
 export {default as GroupByScrollList} from './GroupByScrollList/GroupByScrollList';
 export {default as HeaderContainer} from './HeaderContainer/HeaderContainer';
