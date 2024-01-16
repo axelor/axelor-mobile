c9d5f9f0358337c84e90dd0a33494dcf756028e1
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-01-16 15:08:21 +0100

    refactor: use DurationInput in DurationFormInput

--- packages/apps/helpdesk/src/components/molecules/DurationFormInput/DurationFormInput.tsx
+++ packages/apps/helpdesk/src/components/molecules/DurationFormInput/DurationFormInput.tsx
@@ -16,19 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {StyleSheet} from 'react-native';
-import {FormInput, checkNullString} from '@axelor/aos-mobile-ui';
+import {DurationInput} from '@axelor/aos-mobile-ui';
 import {useTranslator} from '@axelor/aos-mobile-core';
-import {
-  MAX_HOUR_REGEX,
-  NEED_SPACER_REGEX,
-  PERFECT_REGEX,
-  SPACER,
-  formatDuration,
-  formatDurationFromSeconds,
-  parseDuration,
-} from './duration.helpers';
 
 interface DurationFormInputProps {
   style?: any;
@@ -49,61 +40,14 @@ const DurationFormInput = ({
 }: DurationFormInputProps) => {
   const I18n = useTranslator();
 
-  const [formattedDuration, setFormattedDuration] = useState(
-    formatDurationFromSeconds(defaultValue),
-  );
-
-  const handleDurationChange = (value: string) => {
-    if (NEED_SPACER_REGEX.test(value)) {
-      handleResult(`${value.slice(0, 3)}${SPACER}${value.slice(3)}`);
-    } else if (MAX_HOUR_REGEX.test(value)) {
-      handleResult(`${value}${SPACER}`);
-    } else if (PERFECT_REGEX.test(value)) {
-      handleResult(value);
-    } else {
-      handleResult(value);
-    }
-  };
-
-  const handleEndFocus = () => {
-    let hours, minutes;
-
-    if (checkNullString(formattedDuration)) {
-      hours = 0;
-      minutes = 0;
-    } else if (formattedDuration.includes(SPACER)) {
-      const splitDuraction = formattedDuration.split(SPACER);
-      hours = parseInt(splitDuraction[0], 10);
-      minutes = splitDuraction[1] ? parseInt(splitDuraction[1], 10) : 0;
-    } else {
-      hours = parseInt(formattedDuration, 10);
-      minutes = 0;
-    }
-
-    const result = formatDuration(hours, minutes);
-    handleResult(result);
-  };
-
-  const handleResult = (value: string) => {
-    setFormattedDuration(value);
-    onChange(parseDuration(value));
-  };
-
-  useEffect(() => {
-    setFormattedDuration(formatDurationFromSeconds(defaultValue ?? 0));
-  }, [defaultValue]);
-
   return (
-    <FormInput
+    <DurationInput
       style={[styles.input, style]}
       required={required}
-      readOnly={readonly}
+      readonly={readonly}
       title={I18n.t(title)}
-      onChange={handleDurationChange}
-      onEndFocus={handleEndFocus}
-      defaultValue={formattedDuration}
-      multiline={false}
-      keyboardType="numeric"
+      onChange={onChange}
+      defaultValue={defaultValue}
     />
   );
 };
