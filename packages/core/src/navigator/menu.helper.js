c2b8ce9a98849729634eb56713196304d960af36
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-01-16 14:56:45 +0100

    feat: manage new request to get user authorizations (#351)
    * RM#73629

--- packages/core/src/navigator/menu.helper.js
+++ packages/core/src/navigator/menu.helper.js
@@ -17,7 +17,6 @@
  */
 
 import {checkNullString} from '@axelor/aos-mobile-ui';
-import {userHaveAccessToConfig} from './roles.helper';
 import {formatVersionString} from '../utils/string';
 
 export function findIndexAndRouteOfMenu(routes, menuName) {
@@ -29,35 +28,6 @@ export function findIndexAndRouteOfMenu(routes, menuName) {
 
   return {route: routes[index], index: index};
 }
-
-const userHaveAccessToMenu = ({menuConfig, user}) => {
-  return userHaveAccessToConfig({config: menuConfig, user: user});
-};
-
-const areMenusRestricted = ({listMenu}) => {
-  if (listMenu == null || listMenu.length === 0) {
-    return false;
-  }
-  return true;
-};
-
-const doesMenuHasRestriction = ({menuConfig}) => {
-  if (menuConfig == null) {
-    return false;
-  }
-  return true;
-};
-
-export const isMenuEnabled = ({listMenu, menuKey, user}) => {
-  if (areMenusRestricted({listMenu})) {
-    const menuConfig = listMenu.find(menu => menu.technicalName === menuKey);
-    if (doesMenuHasRestriction({menuConfig})) {
-      return userHaveAccessToMenu({menuConfig, user});
-    }
-    return true;
-  }
-  return true;
-};
 
 export function getMenuTitle(menu, {I18n}) {
   if (menu.title != null) {
