56fbda1fa37c755dbb18ed4172d8dc21be43ee0a
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-01-16 17:47:52 +0100

    feat: add ControlEntryList header

--- packages/apps/quality/src/screens/ControlEntryListScreen.js
+++ packages/apps/quality/src/screens/ControlEntryListScreen.js
@@ -17,14 +17,57 @@
  */
 
 import React from 'react';
-import {Screen, Text} from '@axelor/aos-mobile-ui';
+import {StyleSheet, View} from 'react-native';
+import {
+  HeaderContainer,
+  Screen,
+  Text,
+  ToggleButton,
+  useThemeColor,
+} from '@axelor/aos-mobile-ui';
+import {DateInput} from '@axelor/aos-mobile-core';
 
 const ControlEntryListScreen = ({navigation}) => {
+  const Colors = useThemeColor();
+
   return (
     <Screen removeSpaceOnTop={true}>
+      <HeaderContainer
+        expandableFilter={false}
+        fixedItems={
+          <View style={styles.headerContainer}>
+            <ToggleButton
+              isActive={false}
+              activeColor={Colors.successColor}
+              buttonConfig={{
+                iconName: 'person-fill',
+                width: '10%',
+                style: styles.toggleButton,
+              }}
+            />
+            <DateInput style={styles.dateInput} />
+          </View>
+        }
+      />
       <Text>test</Text>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  toggleButton: {
+    height: 40,
+    top: '16%',
+    left: 5,
+  },
+  dateInput: {
+    width: '80%',
+    marginRight: 5,
+  },
+});
 
 export default ControlEntryListScreen;
