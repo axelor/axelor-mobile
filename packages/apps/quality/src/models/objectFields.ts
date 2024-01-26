b3d416bc9601fb2fb3f6cc34c4845d733b34adaa
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-01-26 11:25:28 +0100

    feat: add ControlEntry list view (#362)
    * RM#74126

---
+++ packages/apps/quality/src/models/objectFields.ts
@@ -0,0 +1,38 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ObjectFields, schemaContructor} from '@axelor/aos-mobile-core';
+
+export const quality_modelAPI: ObjectFields = {
+  quality_controlEntry: schemaContructor.object({
+    inspector: schemaContructor.subObject(),
+    name: schemaContructor.string(),
+    controlEntrySamplesList: schemaContructor
+      .array()
+      .of(schemaContructor.subObject()),
+    entryDateTime: schemaContructor.string(),
+    statusSelect: schemaContructor.number(),
+    sampleCount: schemaContructor.number(),
+    controlPlan: schemaContructor.subObject(),
+  }),
+  quality_controlEntrySample: schemaContructor.object({
+    fullName: schemaContructor.string(),
+    resultSelect: schemaContructor.number(),
+    entrySampleNbr: schemaContructor.number(),
+  }),
+};
---
+++ packages/apps/quality/src/screens/ControlEntryListScreen.js
@@ -0,0 +1,125 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {
+  HeaderContainer,
+  Screen,
+  ScrollList,
+  ToggleButton,
+  useThemeColor,
+} from '@axelor/aos-mobile-ui';
+import {
+  DateInput,
+  useDispatch,
+  useSelector,
+  useTranslator,
+} from '@axelor/aos-mobile-core';
+import {searchControlEntry} from '../features/controlEntrySlice';
+import {ControlEntryCard} from '../components';
+
+const ControlEntryListScreen = ({}) => {
+  const Colors = useThemeColor();
+  const I18n = useTranslator();
+  const dispatch = useDispatch();
+
+  const {userId} = useSelector(state => state.auth);
+  const {controlEntryList, loadingControlEntryList, moreLoading, isListEnd} =
+    useSelector(state => state.controlEntry);
+
+  const [isInspectorFilter, setIsInspectorFilter] = useState(false);
+  const [dateFilter, setDateFilter] = useState(null);
+
+  const fetchControlEntryAPI = useCallback(
+    (page = 0) => {
+      dispatch(
+        searchControlEntry({
+          page: page,
+          isInspector: isInspectorFilter,
+          userId: userId,
+          date: dateFilter,
+        }),
+      );
+    },
+    [dispatch, isInspectorFilter, userId, dateFilter],
+  );
+
+  return (
+    <Screen removeSpaceOnTop={true}>
+      <HeaderContainer
+        expandableFilter={false}
+        fixedItems={
+          <View style={styles.headerContainer}>
+            <ToggleButton
+              isActive={isInspectorFilter}
+              onPress={() => setIsInspectorFilter(current => !current)}
+              activeColor={Colors.successColor}
+              buttonConfig={{
+                iconName: 'person-fill',
+                width: '10%',
+                style: styles.toggleButton,
+              }}
+            />
+            <DateInput
+              style={styles.dateInput}
+              nullable={true}
+              onDateChange={setDateFilter}
+              mode="date"
+              popup
+            />
+          </View>
+        }
+      />
+      <ScrollList
+        loadingList={loadingControlEntryList}
+        data={controlEntryList}
+        renderItem={({item}) => (
+          <ControlEntryCard
+            sampleCount={item.sampleCount}
+            entryDateTime={item.entryDateTime}
+            statusSelect={item.statusSelect}
+            name={item.name}
+            controlEntryId={item.id}
+          />
+        )}
+        fetchData={fetchControlEntryAPI}
+        moreLoading={moreLoading}
+        isListEnd={isListEnd}
+        translator={I18n.t}
+      />
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: '-5%',
+  },
+  toggleButton: {
+    height: 40,
+    top: '16%',
+  },
+  dateInput: {
+    width: '80%',
+  },
+});
+
+export default ControlEntryListScreen;
