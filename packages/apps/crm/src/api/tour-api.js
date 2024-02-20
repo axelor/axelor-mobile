cf55300a7f28d9b1ec8332c1531367dbbc224c6c
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-02-20 08:50:16 +0100

    feat: add tour list screen (#424)
    * RM#75336

---
+++ packages/apps/crm/src/api/tour-api.js
@@ -0,0 +1,60 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+  createStandardSearch,
+  getSearchCriterias,
+} from '@axelor/aos-mobile-core';
+
+const createToursCriteria = (searchValue, userId, isSalesperson, date) => {
+  const criteria = [getSearchCriterias('crm_tour', searchValue)];
+
+  if (isSalesperson && userId != null) {
+    criteria.push({
+      fieldName: 'salespersonUser.id',
+      operator: '=',
+      value: userId,
+    });
+  }
+
+  if (date != null) {
+    criteria.push({
+      fieldName: 'date',
+      operator: '=',
+      value: date,
+    });
+  }
+
+  return criteria;
+};
+
+export async function searchTour({
+  searchValue = null,
+  userId,
+  date,
+  isSalesperson,
+  page = 0,
+}) {
+  return createStandardSearch({
+    model: 'com.axelor.apps.crm.db.Tour',
+    criteria: createToursCriteria(searchValue, userId, isSalesperson, date),
+    fieldKey: 'crm_tour',
+    sortKey: 'crm_tour',
+    page: page,
+  });
+}
