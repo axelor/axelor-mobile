3d42064a91d61f3f66932f91797871192acb79d5
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-02-20 08:50:16 +0100

    feat: add tour list screen (#424)
    * RM#75336

--- packages/apps/crm/src/screens/tour/index.js
+++ packages/apps/crm/src/screens/tour/index.js
@@ -16,14 +16,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {catalogReducer as catalog} from './catalogSlice';
-export {clientReducer as client} from './clientSlice';
-export {companyReducer as crm_company} from './companySlice';
-export {contactReducer as contact} from './contactSlice';
-export {eventReducer as event} from './eventSlice';
-export {functionReducer as function} from './functionSlice';
-export {leadReducer as lead} from './leadSlice';
-export {opportunityReducer as opportunity} from './opportunitySlice';
-export {partnerReducer as partner} from './partnerSlice';
-export {prospectReducer as prospect} from './prospectSlice';
-export {tourReducer as tour} from './tourSlice';
+import TourListScreen from './TourListScreen';
+
+export default {
+  TourListScreen: {
+    title: 'Crm_Tours',
+    component: TourListScreen,
+    options: {
+      shadedHeader: false,
+    },
+  },
+};
+
+export {TourListScreen};
