d808b863770ea131ff163a1af57a25e5fa33ecdd
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-02-23 15:52:30 +0100

    feat: creation of SearchListScreen to search and scroll

---
+++ packages/core/src/components/templates/SearchListScreen/SearchListScreen.tsx
@@ -0,0 +1,132 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useMemo, useState} from 'react';
+import {
+  AutoCompleteSearch,
+  HeaderContainer,
+  Screen,
+  ScrollList,
+} from '@axelor/aos-mobile-ui';
+import {ScannerAutocompleteSearch} from '../../organisms';
+import {useDispatch} from '../../../redux/hooks';
+import useTranslator from '../../../i18n/hooks/use-translator';
+
+interface SearchListScreenProps {
+  list: any[];
+  loading: boolean;
+  moreLoading: boolean;
+  isListEnd: boolean;
+  sliceFunction: any;
+  sliceFunctionData?: Object;
+  onChangeSearchValue?: () => void;
+  displaySearchValue?: () => string;
+  searchPlaceholder?: string;
+  searchNavigate?: boolean;
+  scanKeySearch?: string;
+  chipComponent?: any;
+  headerChildren?: any;
+  renderListItem: (item: any) => any;
+}
+
+const SearchListScreen = ({
+  list,
+  loading,
+  moreLoading,
+  isListEnd,
+  sliceFunction,
+  sliceFunctionData,
+  onChangeSearchValue,
+  displaySearchValue,
+  searchPlaceholder,
+  searchNavigate,
+  scanKeySearch = null,
+  chipComponent,
+  headerChildren,
+  renderListItem,
+}: SearchListScreenProps) => {
+  const I18n = useTranslator();
+  const dispatch = useDispatch();
+
+  const [filter, setFilter] = useState(null);
+
+  const fetchApi = useCallback(
+    ({page, searchValue}) => {
+      dispatch(
+        sliceFunction({
+          ...(sliceFunctionData ?? {}),
+          searchValue: searchValue,
+          page: page,
+        }),
+      );
+    },
+    [dispatch, sliceFunction, sliceFunctionData],
+  );
+
+  const fetchSearchAPI = useCallback(
+    ({page = 0, searchValue}) => {
+      setFilter(searchValue);
+      fetchApi({page, searchValue});
+    },
+    [fetchApi],
+  );
+
+  const fetchListAPI = useCallback(
+    page => {
+      fetchApi({page, searchValue: filter});
+    },
+    [fetchApi, filter],
+  );
+
+  const SearchBar = useMemo(
+    () => (scanKeySearch ? ScannerAutocompleteSearch : AutoCompleteSearch),
+    [scanKeySearch],
+  );
+
+  return (
+    <Screen removeSpaceOnTop={true}>
+      <HeaderContainer
+        fixedItems={
+          <SearchBar
+            objectList={list}
+            onChangeValue={onChangeSearchValue}
+            fetchData={fetchSearchAPI}
+            displayValue={displaySearchValue}
+            placeholder={searchPlaceholder}
+            oneFilter={true}
+            navigate={searchNavigate}
+            scanKeySearch={scanKeySearch}
+          />
+        }
+        chipComponent={chipComponent}>
+        {headerChildren}
+      </HeaderContainer>
+      <ScrollList
+        loadingList={loading}
+        data={list}
+        renderItem={renderListItem}
+        fetchData={fetchListAPI}
+        moreLoading={moreLoading}
+        isListEnd={isListEnd}
+        translator={I18n.t}
+      />
+    </Screen>
+  );
+};
+
+export default SearchListScreen;
