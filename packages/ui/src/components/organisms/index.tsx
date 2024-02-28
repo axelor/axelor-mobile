b491cb48067948325315f596399173733895baec
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-02-28 10:08:02 +0100

    feat: add DistributionBar component (#434)
    * RM#75624

--- packages/ui/src/components/organisms/index.tsx
+++ packages/ui/src/components/organisms/index.tsx
@@ -19,6 +19,7 @@
 export {default as AutoCompleteSearch} from './AutoCompleteSearch/AutoCompleteSearch';
 export {default as CheckboxScrollList} from './CheckboxScrollList/CheckboxScrollList';
 export {default as ChipSelect} from './ChipSelect/ChipSelect';
+export {default as DistributionBar} from './DistributionBar/DistributionBar';
 export {default as DropdownCardSwitch} from './DropdownCardSwitch/DropdownCardSwitch';
 export {default as DropdownMenuItem} from './DropdownMenuItem/DropdownMenuItem';
 export {default as DurationInput} from './DurationInput/DurationInput';
