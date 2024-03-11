7f4a551ad5836e5bc200c377b4787bf37c3aafed
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-03-11 14:57:42 +0100

    test: add cases for CardIndicator

---
+++ packages/ui/__tests__/components/molecules/CardIndicator.test.js
@@ -0,0 +1,49 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import {shallow} from 'enzyme';
+import {Card, CardIndicator, Text} from '@axelor/aos-mobile-ui';
+
+describe('CardIconButton Component', () => {
+  const props = {
+    indication: 'heart',
+    children: <View testID="children" />,
+    isVisible: true,
+    handleClose: () => {},
+  };
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<CardIndicator {...props} />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders the right informations', () => {
+    const wrapper = shallow(<CardIndicator {...props} />);
+
+    expect(wrapper.contains(props.children)).toBe(true);
+    expect(wrapper.find(Card).exists()).toBe(true);
+    expect(wrapper.find(Text).prop('children')).toBe(props.indication);
+
+    wrapper.setProps({isVisible: false});
+
+    expect(wrapper.find(Card).exists()).toBe(false);
+  });
+});
