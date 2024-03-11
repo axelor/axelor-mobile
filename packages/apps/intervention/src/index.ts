564b58b22ced4fe6940b31a2534975ab85b3cc82
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-03-11 16:39:26 +0100

    feat: creation of intervention module

---
+++ packages/apps/intervention/src/index.ts
@@ -0,0 +1,12 @@
+import {Module} from '@axelor/aos-mobile-core';
+
+export const InterventionModule: Module = {
+  name: 'app-intervention',
+  title: 'Intervention_Intervention',
+  subtitle: 'Intervention_Intervention',
+  icon: 'car-front-fill',
+  compatibilityAOS: {
+    moduleName: 'axelor-intervention',
+    downToVersion: '8.1.0',
+  },
+};
