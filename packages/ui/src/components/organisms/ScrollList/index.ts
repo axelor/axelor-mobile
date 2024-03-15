f091dbe30690cf5b701f57f5d04c184b84e185c4
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-03-15 16:37:24 +0100

    feat: add actions props to components which use ScrollList

--- packages/ui/src/components/organisms/ScrollList/index.ts
+++ packages/ui/src/components/organisms/ScrollList/index.ts
@@ -16,5 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {Action as ActionType} from './ScrollList';
 export {default as ScrollList} from './ScrollList';
 export {default as TopActions} from './TopActions';
