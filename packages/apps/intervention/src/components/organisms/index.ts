3c93a78c79705482ff84808a603c5704f987039d
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-03-25 09:45:55 +0100

    fix: organisms index file

--- packages/apps/intervention/src/components/organisms/index.ts
+++ packages/apps/intervention/src/components/organisms/index.ts
@@ -16,4 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {default as ClientProspectSearchBar} from './ClientProspectSearchBar/ClientProspectSearchBar';
 export {default as DropdownCards} from './DropdownCards/DropdownCards';
+export {default as PlaceEquipmentSearchBar} from './PlaceEquipmentSearchBar/PlaceEquipmentSearchBar';
