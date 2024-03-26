5f9e66362e84a70e45f653fb15537f64b8f9b88a
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-03-26 16:50:55 +0100

    feat: add Equipment form view (#466)
    * RM#76570

---
+++ packages/apps/intervention/src/models/forms.ts
@@ -0,0 +1,125 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {FormConfigs, customComponentOptions} from '@axelor/aos-mobile-core';
+import {
+  ClientProspectSearchBar,
+  ContractSearchBar,
+  EquipmentFamilySearchBar,
+  PlaceEquipmentSearchBar,
+  TypePicker,
+} from '../components';
+
+const CustomerComponentWrapper = component => {
+  return ({objectState, ...props}: customComponentOptions) =>
+    component({...props, customerId: objectState.partner?.id});
+};
+
+export const intervention_formsRegister: FormConfigs = {
+  intervention_equipment: {
+    modelName: 'com.axelor.apps.intervention.db.Equipment',
+    fields: {
+      partner: {
+        titleKey: 'Intervention_Customer',
+        type: 'object',
+        widget: 'custom',
+        required: true,
+        customComponent: ClientProspectSearchBar,
+        options: {
+          showTitle: true,
+          readonly: true,
+        },
+      },
+      parentEquipment: {
+        titleKey: 'Intervention_ParentPlace',
+        type: 'object',
+        widget: 'custom',
+        customComponent: CustomerComponentWrapper(PlaceEquipmentSearchBar),
+        options: {
+          showTitle: true,
+        },
+      },
+      code: {
+        titleKey: 'Intervention_Code',
+        type: 'string',
+        widget: 'default',
+      },
+      name: {
+        titleKey: 'Intervention_Designation',
+        type: 'string',
+        widget: 'default',
+        required: true,
+      },
+      typeSelect: {
+        titleKey: 'Intervention_Type',
+        type: 'string',
+        widget: 'custom',
+        customComponent: TypePicker,
+        required: true,
+      },
+      inService: {
+        titleKey: 'Intervention_Status_InService',
+        type: 'boolean',
+        widget: 'checkbox',
+      },
+      commissioningDate: {
+        titleKey: 'Intervention_CommissioningDate',
+        type: 'date',
+        widget: 'date',
+      },
+      customerWarrantyOnPartEndDate: {
+        titleKey: 'Intervention_CustomerWarrantyOnPartEndDate',
+        type: 'date',
+        widget: 'date',
+      },
+      equipmentFamily: {
+        titleKey: 'Intervention_EquipmentFamily',
+        type: 'object',
+        widget: 'custom',
+        required: true,
+        customComponent: EquipmentFamilySearchBar,
+      },
+      scheduleOfOperation: {
+        titleKey: 'Intervention_ScheduleOfOperation',
+        type: 'string',
+        widget: 'default',
+      },
+      specificAccessSchedule: {
+        titleKey: 'Intervention_SpecificAccessSchedule',
+        type: 'string',
+        widget: 'default',
+      },
+      contract: {
+        titleKey: 'Intervention_Contract',
+        type: 'object',
+        widget: 'custom',
+        customComponent: CustomerComponentWrapper(ContractSearchBar),
+      },
+      comments: {
+        titleKey: 'Intervention_Comments',
+        type: 'string',
+        widget: 'default',
+        options: {
+          multiline: true,
+          adjustHeightWithLines: true,
+          style: {marginBottom: 100, width: '90%', alignSelf: 'center'},
+        },
+      },
+    },
+  },
+};
---
+++ packages/apps/intervention/src/screens/equipment/EquipmentFormView.tsx
@@ -0,0 +1,78 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useEffect, useMemo} from 'react';
+import {FormView, useDispatch, useSelector} from '@axelor/aos-mobile-core';
+import {useThemeColor} from '@axelor/aos-mobile-ui';
+import {getEquipmentById, updateEquipment} from '../../features/equipmentSlice';
+
+const EquipmentFormView = ({navigation, route}) => {
+  const idEquipment = route.params.idEquipment;
+
+  const Colors = useThemeColor();
+  const _dispatch = useDispatch();
+
+  const {equipment} = useSelector((state: any) => state.intervention_equipment);
+
+  useEffect(() => {
+    _dispatch((getEquipmentById as any)({equipmentId: idEquipment}));
+  }, [_dispatch, idEquipment]);
+
+  const updateEquipmentAPI = useCallback(
+    (_equipment, dispatch) => {
+      dispatch(
+        (updateEquipment as any)({
+          equipment: _equipment,
+          partnerId: _equipment?.partner?.id,
+        }),
+      );
+
+      navigation.pop();
+    },
+    [navigation],
+  );
+
+  const _defaultValue = useMemo(() => {
+    return {...equipment};
+  }, [equipment]);
+
+  return (
+    <FormView
+      formKey="intervention_equipment"
+      defaultValue={_defaultValue}
+      actions={[
+        {
+          key: 'update-equipment',
+          type: 'update',
+          needRequiredFields: true,
+          needValidation: true,
+          customAction: ({dispatch, objectState}) => {
+            updateEquipmentAPI(objectState, dispatch);
+          },
+        },
+        {
+          color: Colors.cautionColor,
+          key: 'reset-equipment',
+          type: 'reset',
+        },
+      ]}
+    />
+  );
+};
+
+export default EquipmentFormView;
