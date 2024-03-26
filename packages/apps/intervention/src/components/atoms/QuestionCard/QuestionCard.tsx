de0ad519cd7ea693e2f86833cac67687c9dbee17
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-03-26 12:21:19 +0100

    feat: creation of SurveyView (#470)
    * RM#76316

---
+++ packages/apps/intervention/src/components/atoms/QuestionCard/QuestionCard.tsx
@@ -0,0 +1,96 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {StyleSheet} from 'react-native';
+import {Icon, ObjectCard, useThemeColor} from '@axelor/aos-mobile-ui';
+import {Question} from '../../../types';
+
+interface QuestionCardProps {
+  status: number;
+  interventionRange: any;
+  title: string;
+  isPrivate?: boolean;
+  onPress: () => void;
+}
+
+const QuestionCard = ({
+  status,
+  interventionRange,
+  title,
+  isPrivate = false,
+  onPress,
+}: QuestionCardProps) => {
+  const Colors = useThemeColor();
+
+  const styles = useMemo(
+    () => getStyles(Question.getStatusColor(status, Colors)?.background),
+    [Colors, status],
+  );
+
+  if (status === Question.status.Hidden) {
+    return null;
+  }
+
+  return (
+    <ObjectCard
+      style={styles.border}
+      leftContainerFlex={6}
+      onPress={onPress}
+      upperTexts={{
+        items: [
+          {
+            displayText: interventionRange?.rangeVal?.title,
+            style: styles.details,
+            hideIfNull: true,
+          },
+          {displayText: interventionRange?.equipment?.name, hideIfNull: true},
+          {
+            displayText: title,
+            isTitle: true,
+            hideIfNull: true,
+          },
+        ],
+      }}
+      sideBadges={{
+        style: styles.badges,
+        items: [
+          isPrivate && {
+            customComponent: <Icon name="lock" />,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+const getStyles = (color: string) =>
+  StyleSheet.create({
+    border: {
+      borderLeftWidth: 7,
+      borderLeftColor: color,
+    },
+    badges: {
+      alignItems: 'flex-end',
+    },
+    details: {
+      fontStyle: 'italic',
+    },
+  });
+
+export default QuestionCard;
