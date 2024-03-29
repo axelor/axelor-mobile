eaa4b1a27709a65ad2a475878a169ab266b947c0
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-03-29 15:36:37 +0100

    feat: add Equipment pictures view (#474)
    * RM#76571

---
+++ packages/apps/intervention/src/api/equipment-picture-api.js
@@ -0,0 +1,69 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {axiosApiProvider, createStandardSearch} from '@axelor/aos-mobile-core';
+
+const createEquipmentPictureCriteria = equipmentId => {
+  const criteria = [
+    {
+      fieldName: 'equipment.id',
+      operator: '=',
+      value: equipmentId,
+    },
+  ];
+
+  return criteria;
+};
+
+export async function searchEquipmentPicture({equipmentId, page = 0}) {
+  return createStandardSearch({
+    model: 'com.axelor.apps.intervention.db.Picture',
+    criteria: createEquipmentPictureCriteria(equipmentId),
+    fieldKey: 'intervention_equipmentPicture',
+    sortKey: 'intervention_equipmentPicture',
+    page,
+  });
+}
+
+export async function createEquipmentPicture({
+  equipmentId,
+  version,
+  metaFileId,
+}) {
+  return axiosApiProvider.put({
+    url: `ws/aos/equipment/add-picture/${equipmentId}`,
+    data: {
+      pictureId: metaFileId,
+      version,
+    },
+  });
+}
+
+export async function deleteEquipmentPicture({
+  equipmentId,
+  version,
+  pictureId,
+}) {
+  return axiosApiProvider.put({
+    url: `ws/aos/equipment/remove-picture/${equipmentId}`,
+    data: {
+      pictureId,
+      version,
+    },
+  });
+}
