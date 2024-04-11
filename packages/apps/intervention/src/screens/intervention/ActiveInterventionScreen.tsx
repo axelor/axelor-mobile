9eac3c6f4d11729b4978a20b13a5c4a477e30d86
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-04-11 15:19:09 +0200

    feat: add loading on screen

--- packages/apps/intervention/src/screens/intervention/ActiveInterventionScreen.tsx
+++ packages/apps/intervention/src/screens/intervention/ActiveInterventionScreen.tsx
@@ -17,24 +17,25 @@
  */
 
 import React, {useEffect} from 'react';
-import {StyleSheet} from 'react-native';
+import {ActivityIndicator, StyleSheet} from 'react-native';
 import {
   useDispatch,
   useIsFocused,
   useSelector,
   useTranslator,
 } from '@axelor/aos-mobile-core';
-import {Label, Screen} from '@axelor/aos-mobile-ui';
+import {Label, Screen, useThemeColor} from '@axelor/aos-mobile-ui';
 import {fetchActiveIntervention} from '../../features/interventionSlice';
 import InterventionDetailsScreen from './InterventionDetailsScreen';
 
 const ActiveInterventionScreen = ({}) => {
+  const Colors = useThemeColor();
   const I18n = useTranslator();
   const isFocused = useIsFocused();
   const dispatch = useDispatch();
 
   const {userId} = useSelector((state: any) => state.auth);
-  const {activeIntervention} = useSelector(
+  const {loadingActiveIntervention, activeIntervention} = useSelector(
     (state: any) => state.intervention_intervention,
   );
 
@@ -42,24 +43,37 @@ const ActiveInterventionScreen = ({}) => {
     isFocused && dispatch((fetchActiveIntervention as any)({userId}));
   }, [dispatch, isFocused, userId]);
 
-  if (activeIntervention?.id) {
+  if (loadingActiveIntervention) {
+    return (
+      <ActivityIndicator
+        style={styles.activityIndicator}
+        size="large"
+        color={Colors.inverseColor.background}
+      />
+    );
+  } else if (activeIntervention?.id) {
     return (
       <InterventionDetailsScreen interventionId={activeIntervention?.id} />
     );
+  } else {
+    return (
+      <Screen removeSpaceOnTop={true}>
+        <Label
+          style={styles.label}
+          message={I18n.t('Intervention_NoActiveIntervention')}
+          type="success"
+        />
+      </Screen>
+    );
   }
-
-  return (
-    <Screen removeSpaceOnTop={true}>
-      <Label
-        style={styles.label}
-        message={I18n.t('Intervention_NoActiveIntervention')}
-        type="success"
-      />
-    </Screen>
-  );
 };
 
 const styles = StyleSheet.create({
+  activityIndicator: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+  },
   label: {
     width: '90%',
     alignSelf: 'center',
