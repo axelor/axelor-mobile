137cd3dacf9f934b5b70d66aca3d95726851f0bf
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-04-11 11:16:34 +0200

    fix: add default value for currency (#505)
    * RM#75564

--- packages/apps/hr/src/screens/expense/ExpenseLineFormScreen.js
+++ packages/apps/hr/src/screens/expense/ExpenseLineFormScreen.js
@@ -49,6 +49,7 @@ const ExpenseLineFormScreen = ({route, navigation}) => {
   const _dispatch = useDispatch();
 
   const {user} = useSelector(state => state.user);
+  const {mobileSettings} = useSelector(state => state.appConfig);
 
   useEffect(() => {
     _dispatch(fetchCompanies());
@@ -162,6 +163,10 @@ const ExpenseLineFormScreen = ({route, navigation}) => {
   const defaultValue = useMemo(() => {
     const _defaultDate = new Date().toISOString().split('T')[0];
 
+    const defaultCurrency = mobileSettings?.isMultiCurrencyEnabled
+      ? user.activeCompany?.currency
+      : undefined;
+
     const _default = {
       manageMode: modeExpense || ExpenseLine.modes.general,
       isFromExpense: idExpense != null && expenseLine?.id != null,
@@ -171,6 +176,7 @@ const ExpenseLineFormScreen = ({route, navigation}) => {
       totalAmount: 0,
       totalTax: 0,
       distance: 0,
+      currency: defaultCurrency,
     };
 
     if (justificationMetaFile != null) {
@@ -238,7 +244,9 @@ const ExpenseLineFormScreen = ({route, navigation}) => {
     expenseLine,
     idExpense,
     justificationMetaFile,
+    mobileSettings?.isMultiCurrencyEnabled,
     modeExpense,
+    user.activeCompany?.currency,
     user.activeCompany?.name,
   ]);
 
