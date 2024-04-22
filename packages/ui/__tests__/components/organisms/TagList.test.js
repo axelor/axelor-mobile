0aa442ea18c18ac19a4bf64a0d6a0fc169fdb07a
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-04-22 16:35:34 +0200

    feat: add test case

---
+++ packages/ui/__tests__/components/organisms/TagList.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {TagList, Badge, Text} from '@axelor/aos-mobile-ui';
+import {getDefaultThemeColors} from '../../tools';
+
+describe('TagList Component', () => {
+  const Colors = getDefaultThemeColors();
+
+  const tags = [
+    {title: 'tag1', order: 1},
+    {title: 'tag2', color: Colors.plannedColor, order: 2},
+    {title: 'tag13', order: 3},
+  ];
+
+  it('renders without crashing', () => {
+    const wrapper = shallow(<TagList tags={tags} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders correctly with tags', () => {
+    const wrapper = shallow(<TagList tags={tags} />);
+    expect(wrapper.find(Badge).length).toBe(3);
+    expect(wrapper.find(Text).length).toBe(0);
+  });
+
+  it('renders a title when provided', () => {
+    const title = 'title';
+    const wrapper = shallow(<TagList tags={tags} title={title} />);
+    const textChildren = wrapper.find(Text).prop('children');
+    expect(textChildren.join('')).toBe(title + ' :');
+  });
+
+  it('applies defaultColor to badges when tag-specific color is not provided', () => {
+    const defaultColor = Colors.cautionColor;
+    const wrapper = shallow(
+      <TagList tags={tags} defaultColor={defaultColor} />,
+    );
+
+    expect(wrapper.find(Badge).at(0).prop('color')).toBe(defaultColor);
+    expect(wrapper.find(Badge).at(1).prop('color')).toBe(Colors.plannedColor);
+    expect(wrapper.find(Badge).at(2).prop('color')).toBe(defaultColor);
+  });
+
+  it('uses theme color if no defaultColor is specified', () => {
+    const wrapper = shallow(<TagList tags={tags} />);
+
+    expect(wrapper.find(Badge).at(0).prop('color')).toBe(Colors.infoColor);
+    expect(wrapper.find(Badge).at(2).prop('color')).toBe(Colors.infoColor);
+  });
+
+  it('does not render if all tags are hidden', () => {
+    const hiddenTags = tags.map(tag => ({...tag, hide: true}));
+    const wrapper = shallow(<TagList tags={hiddenTags} />);
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('sorts tags based on their order', () => {
+    const unorderedTags = [
+      {title: 'Last', order: 3},
+      {title: 'First', order: 1},
+      {title: 'Middle', order: 2},
+    ];
+    const wrapper = shallow(<TagList tags={unorderedTags} />);
+    expect(wrapper.find(Badge).at(0).prop('title')).toBe('First');
+    expect(wrapper.find(Badge).at(1).prop('title')).toBe('Middle');
+    expect(wrapper.find(Badge).at(2).prop('title')).toBe('Last');
+  });
+});
