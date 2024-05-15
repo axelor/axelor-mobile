4148ed02132196b18feae5457727c3411363c322
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-05-15 11:58:49 +0200

    refactor: use new selections system on helpdesk (#556)
    * RM#78689

---
+++ packages/apps/helpdesk/src/models/typeObjects.ts
@@ -0,0 +1,55 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ModuleSelections} from '@axelor/aos-mobile-core';
+
+export const helpdesk_typeObjects: ModuleSelections = [
+  {
+    modelName: 'com.axelor.apps.helpdesk.db.Ticket',
+    fields: {
+      prioritySelect: {
+        content: [
+          {
+            key: 'Low',
+            value: 1,
+            title: 'Helpdesk_Priority_Low',
+            color: 'successColor',
+          },
+          {
+            key: 'Normal',
+            value: 2,
+            title: 'Helpdesk_Priority_Normal',
+            color: 'priorityColor',
+          },
+          {
+            key: 'High',
+            value: 3,
+            title: 'Helpdesk_Priority_High',
+            color: 'cautionColor',
+          },
+          {
+            key: 'Urgent',
+            value: 4,
+            title: 'Helpdesk_Priority_Urgent',
+            color: 'importantColor',
+          },
+        ],
+      },
+    },
+  },
+];
