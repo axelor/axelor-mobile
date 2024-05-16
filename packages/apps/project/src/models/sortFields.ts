91bfc99e88a528e0a2a2bbf12f392b5b01201f42
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-05-16 13:38:43 +0200

    fix: change sort fields

--- packages/apps/project/src/models/sortFields.ts
+++ packages/apps/project/src/models/sortFields.ts
@@ -19,5 +19,5 @@
 import {SortFields} from '@axelor/aos-mobile-core';
 
 export const project_sortFields: SortFields = {
-  project_project: ['projectStatus.id', 'name'],
+  project_project: ['projectStatus', 'name'],
 };
