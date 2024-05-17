63ad366fede3c0ee2cd7a28d999260f2ea4533dc
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-05-17 10:35:28 +0200

    feat: add license

--- packages/ui/__tests__/components/organisms/TagList.test.js
+++ packages/ui/__tests__/components/organisms/TagList.test.js
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2024 Axelor (<http://axelor.com>).
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or  modify
  * it under the terms of the GNU Affero General Public License, version 3,
  * as published by the Free Software Foundation.
  *
