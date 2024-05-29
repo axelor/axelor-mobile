fd35655693a20d827c614faec9d7cc4193bebf3d
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-05-29 16:46:30 +0200

    fix: infinite loading after deleting/updating an expense line (#577)
    * RM#79535

--- packages/apps/hr/src/features/expenseLineSlice.js
+++ packages/apps/hr/src/features/expenseLineSlice.js
@@ -232,6 +232,7 @@ const expenseLineSlice = createSlice({
     });
     builder.addCase(deleteExpenseLine.fulfilled, (state, action) => {
       if (action?.meta?.arg?.expenseId == null) {
+        state.loadingExpenseLine = false;
         state.expenseLineList = action.payload;
       }
     });
@@ -246,7 +247,12 @@ const expenseLineSlice = createSlice({
     });
     builder.addCase(updateExpenseLine.fulfilled, (state, action) => {
       if (action?.meta?.arg?.expenseId == null) {
+        state.loadingExpenseLine = false;
         state.expenseLineList = action.payload;
+      } else if (action?.meta?.arg?.mode === ExpenseLine.modes.general) {
+        state.loadingGeneralExpenseLine = false;
+      } else if (action?.meta?.arg?.mode === ExpenseLine.modes.kilometric) {
+        state.loadingKilometricExpenseLine = false;
       }
     });
   },
