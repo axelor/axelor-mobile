350e952c64319673f34a4be54d421a37ce7a7dd4
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-06-06 12:18:59 +0200

    refactor: use AOS request for user permissions (#587)
    * RM#79725

--- packages/core/src/permissions/api.helpers.ts
+++ packages/core/src/permissions/api.helpers.ts
@@ -16,19 +16,48 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {createStandardSearch} from '../apiProviders';
+import {axiosApiProvider, createStandardSearch} from '../apiProviders';
+
+async function getAOPUserPerms({userId}) {
+  return createStandardSearch({
+    model: 'com.axelor.auth.db.Permission',
+    domain:
+      'self IN (SELECT p FROM User u join u.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.group g join g.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.roles r join r.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.group g join g.roles r join r.permissions p WHERE u.id = :userId)',
+    domainContext: {userId},
+    fieldKey: 'core_permissions',
+    numberElementsByPage: null,
+    page: 0,
+  });
+}
+
+async function getAOPUserMetaPerms({userId}) {
+  return createStandardSearch({
+    model: 'com.axelor.meta.db.MetaPermissionRule',
+    criteria: [
+      {fieldName: 'metaPermission.active', operator: '=', value: true},
+    ],
+    domain:
+      'self.metaPermission IN (SELECT p FROM User u join u.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.group g join g.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.roles r join r.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.group g join g.roles r join r.metaPermissions p WHERE u.id = :userId)',
+    domainContext: {userId},
+    fieldKey: 'core_metaPermissionRule',
+    numberElementsByPage: null,
+    page: 0,
+  });
+}
+
+async function getAOSUserPerms() {
+  return axiosApiProvider.get({url: 'ws/aos/user/permissions'});
+}
+
+async function getAOSUserMetaPerms() {
+  return axiosApiProvider.get({url: 'ws/aos/user/meta-permission-rules'});
+}
 
 export async function getAllPerms({userId}) {
   if (userId != null) {
-    return createStandardSearch({
-      model: 'com.axelor.auth.db.Permission',
-      domain:
-        'self IN (SELECT p FROM User u join u.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.group g join g.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.roles r join r.permissions p WHERE u.id = :userId) OR self IN (SELECT p FROM User u join u.group g join g.roles r join r.permissions p WHERE u.id = :userId)',
-      domainContext: {userId},
-      fieldKey: 'core_permissions',
-      numberElementsByPage: null,
-      page: 0,
-    });
+    return getAOSUserPerms()
+      .then(({data}) => ({data: {data: data?.permissionResponseList}}))
+      .catch(() => getAOPUserPerms({userId}));
   }
 
   return null;
@@ -36,18 +65,9 @@ export async function getAllPerms({userId}) {
 
 export async function getAllFieldsPerms({userId}) {
   if (userId != null) {
-    return createStandardSearch({
-      model: 'com.axelor.meta.db.MetaPermissionRule',
-      criteria: [
-        {fieldName: 'metaPermission.active', operator: '=', value: true},
-      ],
-      domain:
-        'self.metaPermission IN (SELECT p FROM User u join u.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.group g join g.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.roles r join r.metaPermissions p WHERE u.id = :userId) OR self.metaPermission IN (SELECT p FROM User u join u.group g join g.roles r join r.metaPermissions p WHERE u.id = :userId)',
-      domainContext: {userId},
-      fieldKey: 'core_metaPermissionRule',
-      numberElementsByPage: null,
-      page: 0,
-    });
+    return getAOSUserMetaPerms()
+      .then(({data}) => ({data: {data: data?.metaPermissionRuleResponses}}))
+      .catch(() => getAOPUserMetaPerms({userId}));
   }
 
   return null;
