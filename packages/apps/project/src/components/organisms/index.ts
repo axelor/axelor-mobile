e31322552e8eef7ab5d7fbe16c32c68a013687b7
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-06-24 16:04:33 +0200

    feat: add TimeView component (#582)
    * RM#78178

--- packages/apps/project/src/components/organisms/index.ts
+++ packages/apps/project/src/components/organisms/index.ts
@@ -16,5 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {default as LogTimeButton} from './LogTimeButton/LogTimeButton';
 export {default as ProjectDropdownCards} from './ProjectDropdownCards/ProjectDropdownCards';
 export {default as ProjectSearchBar} from './ProjectSearchBar/ProjectSearchBar';
