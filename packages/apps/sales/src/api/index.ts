4a58c5faaa6a63185d397135ee9298ae696c84f4
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-06-25 16:02:55 +0200

    feat: create SaleOrder list view (#601)
    * RM#80376

--- packages/apps/sales/src/api/index.ts
+++ packages/apps/sales/src/api/index.ts
@@ -16,5 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {fetchSaleConfig as fetchSaleConfigApi} from './sale-config-api';
+export {searchCustomer as searchCustomerApi} from './customer-api';
 export {searchProduct as searchProductApi} from './product-api';
+export {fetchSaleConfig as fetchSaleConfigApi} from './sale-config-api';
+export {fetchSaleOrder as fetchSaleOrderApi} from './sale-order-api';
