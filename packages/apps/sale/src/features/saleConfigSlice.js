08d40e74500c458cd964ff4de6d7ba3edb0277c6
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-07-02 11:12:12 +0200

    refactor: rename package from sales to sale (#623)
    * RM#80877

---
+++ packages/apps/sale/src/features/saleConfigSlice.js
@@ -0,0 +1,55 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {handlerApiCall} from '@axelor/aos-mobile-core';
+import {fetchSaleConfig as _fetchSaleConfig} from '../api/sale-config-api';
+
+export const fetchSaleConfig = createAsyncThunk(
+  'sale_saleConfig/fetchSaleConfig',
+  async function (data, {getState}) {
+    return handlerApiCall({
+      fetchFunction: _fetchSaleConfig,
+      data,
+      action: 'Sale_SliceAction_FetchSaleConfig',
+      getState,
+      responseOptions: {isArrayResponse: true},
+    });
+  },
+);
+
+const initialState = {
+  loadingSaleConfig: true,
+  saleConfig: {},
+};
+
+const saleConfigSlice = createSlice({
+  name: 'sale_saleConfig',
+  initialState,
+  extraReducers: builder => {
+    builder.addCase(fetchSaleConfig.pending, (state, action) => {
+      state.loadingSaleConfig = true;
+    });
+    builder.addCase(fetchSaleConfig.fulfilled, (state, action) => {
+      state.loadingSaleConfig = false;
+      state.saleConfig = action.payload?.[0];
+    });
+  },
+});
+
+export const saleConfigReducer = saleConfigSlice.reducer;
---
+++ packages/apps/sale/src/hooks/use-sale-header-actions.ts
@@ -0,0 +1,112 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {useEffect, useMemo} from 'react';
+import {
+  headerActionsProvider,
+  useSelector,
+  useTranslator,
+  useTypes,
+} from '@axelor/aos-mobile-core';
+
+export const useSaleHeaders = () => {
+  useSaleOrderDetailsActions();
+  useSaleOrderLineDetailsActions();
+  useProductDetailsActions();
+  useClientDetailsActions();
+};
+
+const useSaleOrderDetailsActions = () => {
+  const I18n = useTranslator();
+  const {SaleOrder} = useTypes();
+
+  const {mobileSettings} = useSelector((state: any) => state.appConfig);
+  const {saleOrder} = useSelector((state: any) => state.sale_saleOrder);
+
+  const title = useMemo(
+    () =>
+      saleOrder?.statusSelect > SaleOrder?.statusSelect.Finalized
+        ? I18n.t('Sale_SaleOrder')
+        : I18n.t('Sale_SaleQuotation'),
+    [I18n, saleOrder, SaleOrder?.statusSelect.Finalized],
+  );
+
+  useEffect(() => {
+    headerActionsProvider.registerModel('sale_saleOrder_details', {
+      model: 'com.axelor.apps.sale.db.SaleOrder',
+      modelId: saleOrder?.id,
+      headerTitle: title,
+      disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
+      attachedFileScreenTitle: title,
+    });
+  }, [mobileSettings, saleOrder?.id, title]);
+};
+
+const useSaleOrderLineDetailsActions = () => {
+  const I18n = useTranslator();
+  const {SaleOrder} = useTypes();
+
+  const {mobileSettings} = useSelector((state: any) => state.appConfig);
+  const {saleOrder} = useSelector((state: any) => state.sale_saleOrder);
+  const {saleOrderLine} = useSelector((state: any) => state.sale_saleOrderLine);
+
+  const title = useMemo(
+    () =>
+      saleOrder?.statusSelect > SaleOrder?.statusSelect.Finalized
+        ? I18n.t('Sale_SaleOrder')
+        : I18n.t('Sale_SaleQuotation'),
+    [I18n, saleOrder, SaleOrder?.statusSelect.Finalized],
+  );
+
+  useEffect(() => {
+    headerActionsProvider.registerModel('sale_saleOrderLine_details', {
+      model: 'com.axelor.apps.sale.db.SaleOrderLine',
+      modelId: saleOrderLine?.id,
+      headerTitle: title,
+      disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
+      attachedFileScreenTitle: title,
+    });
+  }, [mobileSettings, saleOrderLine?.id, title]);
+};
+
+const useProductDetailsActions = () => {
+  const {mobileSettings} = useSelector((state: any) => state.appConfig);
+  const {product} = useSelector((state: any) => state.sale_product);
+
+  useEffect(() => {
+    headerActionsProvider.registerModel('sale_product_details', {
+      model: 'com.axelor.apps.base.db.Product',
+      modelId: product?.id,
+      disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
+    });
+  }, [mobileSettings, product?.id]);
+};
+
+const useClientDetailsActions = () => {
+  const {mobileSettings} = useSelector((state: any) => state.appConfig);
+  const {customer} = useSelector((state: any) => state.sale_customer);
+
+  useEffect(() => {
+    headerActionsProvider.registerModel('sale_client_details', {
+      model: 'com.axelor.apps.base.db.Partner',
+      modelId: customer?.id,
+      disableMailMessages: !mobileSettings?.isTrackerMessageEnabled,
+      attachedFileScreenTitle: customer?.name,
+    });
+  }, [customer, mobileSettings]);
+};
