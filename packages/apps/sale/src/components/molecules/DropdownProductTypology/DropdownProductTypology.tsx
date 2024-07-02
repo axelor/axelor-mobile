c403bc4db01f4f6f05f9936551e993c470852d27
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-07-02 11:12:12 +0200

    refactor: rename package from sales to sale (#623)
    * RM#80877

---
+++ packages/apps/sale/src/components/molecules/DropdownProductTypology/DropdownProductTypology.tsx
@@ -0,0 +1,73 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback} from 'react';
+import {View} from 'react-native';
+import {
+  useSelector,
+  useTranslator,
+  useTypeHelpers,
+  useTypes,
+} from '@axelor/aos-mobile-core';
+import {LabelText, checkNullString} from '@axelor/aos-mobile-ui';
+
+const DropdownProductTypology = ({}) => {
+  const I18n = useTranslator();
+  const {SaleProduct: Product} = useTypes();
+  const {getItemTitle} = useTypeHelpers();
+
+  const {product} = useSelector((state: any) => state.sale_product);
+
+  const renderLabelText = useCallback(
+    (titleKey: string, value: string | number) => {
+      if (checkNullString(value)) {
+        return null;
+      }
+
+      return <LabelText title={I18n.t(titleKey)} value={value} />;
+    },
+    [I18n],
+  );
+
+  return (
+    <View>
+      {renderLabelText(
+        'Sale_Type',
+        getItemTitle(Product?.productTypeSelect, product.productTypeSelect),
+      )}
+      {renderLabelText(
+        'Sale_SubType',
+        getItemTitle(
+          Product?.productSubTypeSelect,
+          product.productSubTypeSelect,
+        ),
+      )}
+      {renderLabelText('Sale_ProductFamily', product.productFamily?.name)}
+      {renderLabelText('Sale_CategoryFamily', product.productCategory?.name)}
+      {renderLabelText(
+        'Sale_ProcurementMethod',
+        getItemTitle(
+          Product?.procurementMethodSelect,
+          product.procurementMethodSelect,
+        ),
+      )}
+    </View>
+  );
+};
+
+export default DropdownProductTypology;
---
+++ packages/apps/sale/src/models/objectFields.ts
@@ -0,0 +1,181 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ObjectFields, schemaContructor} from '@axelor/aos-mobile-core';
+
+export const sale_modelAPI: ObjectFields = {
+  sale_saleConfig: schemaContructor.object({
+    saleOrderInAtiSelect: schemaContructor.number(),
+  }),
+  sale_product: schemaContructor.object({
+    name: schemaContructor.string(),
+    code: schemaContructor.string(),
+    productFamily: schemaContructor.subObject('name'),
+    productCategory: schemaContructor.subObject('name'),
+    description: schemaContructor.string(),
+    salePrice: schemaContructor.number(),
+    saleCurrency: schemaContructor.subObject('symbol'),
+    picture: schemaContructor.subObject('fileName'),
+    configurator: schemaContructor.subObject(),
+    inAti: schemaContructor.boolean(),
+    internalDescription: schemaContructor.string(),
+    productTypeSelect: schemaContructor.string(),
+    productSubTypeSelect: schemaContructor.number(),
+    procurementMethodSelect: schemaContructor.string(),
+    startDate: schemaContructor.string(),
+    endDate: schemaContructor.string(),
+    salesUnit: schemaContructor.subObject(),
+    isPrototype: schemaContructor.boolean(),
+    isUnrenewed: schemaContructor.boolean(),
+    allowToForceSaleQty: schemaContructor.boolean(),
+    saleProductMultipleQtyList: schemaContructor
+      .array()
+      .of(schemaContructor.subObject()),
+  }),
+  sale_productCompany: schemaContructor.object({
+    company: schemaContructor.subObject(),
+    product: schemaContructor.subObject(),
+    salePrice: schemaContructor.number(),
+    saleCurrency: schemaContructor.subObject('symbol'),
+    procurementMethodSelect: schemaContructor.string(),
+  }),
+  auth_user: schemaContructor.object({
+    companySet: schemaContructor.array().of(schemaContructor.subObject()),
+  }),
+  sale_saleOrder: schemaContructor.object({
+    statusSelect: schemaContructor.number(),
+    saleOrderSeq: schemaContructor.string(),
+    orderBeingEdited: schemaContructor.boolean(),
+    externalReference: schemaContructor.string(),
+    clientPartner: schemaContructor.subObject().concat(
+      schemaContructor.object({
+        fullName: schemaContructor.string(),
+        picture: schemaContructor.subObject(),
+        simpleFullName: schemaContructor.string(),
+        name: schemaContructor.string(),
+        partnerSeq: schemaContructor.string(),
+        jobTitleFunction: schemaContructor.subObject('name'),
+        mainAddress: schemaContructor.subObject('fullName'),
+        fixedPhone: schemaContructor.string(),
+        mobilePhone: schemaContructor.string(),
+      }),
+    ),
+    contactPartner: schemaContructor.subObject().concat(
+      schemaContructor.object({
+        picture: schemaContructor.subObject(),
+        simpleFullName: schemaContructor.string(),
+        name: schemaContructor.string(),
+        partnerSeq: schemaContructor.string(),
+        jobTitleFunction: schemaContructor.subObject('name'),
+        fixedPhone: schemaContructor.string(),
+        mobilePhone: schemaContructor.string(),
+      }),
+    ),
+    company: schemaContructor.subObject('name'),
+    tradingName: schemaContructor.subObject('name'),
+    orderDate: schemaContructor.string(),
+    exTaxTotal: schemaContructor.number(),
+    taxTotal: schemaContructor.number(),
+    inTaxTotal: schemaContructor.number(),
+    amountInvoiced: schemaContructor.number(),
+    advanceTotal: schemaContructor.number(),
+    currency: schemaContructor.subObject('symbol'),
+    deliveryState: schemaContructor.number(),
+    invoicingState: schemaContructor.number(),
+    oneoffSale: schemaContructor.boolean(),
+    saleOrderTypeSelect: schemaContructor.number(),
+    inAti: schemaContructor.boolean(),
+    description: schemaContructor.string(),
+    internalNote: schemaContructor.string(),
+    stockLocation: schemaContructor.subObject('name'),
+    fiscalPosition: schemaContructor.subObject('name'),
+    invoicedPartner: schemaContructor.subObject('fullName'),
+    mainInvoicingAddressStr: schemaContructor.string(),
+    deliveredPartner: schemaContructor.subObject('fullName'),
+    deliveryAddressStr: schemaContructor.string(),
+    accountedRevenue: schemaContructor.number(),
+    totalCostPrice: schemaContructor.number(),
+    totalGrossMargin: schemaContructor.number(),
+    marginRate: schemaContructor.number(),
+    markup: schemaContructor.number(),
+    opportunity: schemaContructor.subObject().concat(
+      schemaContructor.object({
+        amount: schemaContructor.number(),
+        expectedCloseDate: schemaContructor.string(),
+        name: schemaContructor.string(),
+        opportunityRating: schemaContructor.number(),
+        opportunitySeq: schemaContructor.string(),
+        currency: schemaContructor.subObject('symbol'),
+        opportunityStatus: schemaContructor.subObject('name'),
+      }),
+    ),
+    salespersonUser: schemaContructor.subObject('fullName'),
+    team: schemaContructor.subObject('name'),
+    creationDate: schemaContructor.string(),
+    expectedRealisationDate: schemaContructor.string(),
+    endOfValidityDate: schemaContructor.string(),
+    lastReminderDate: schemaContructor.string(),
+    lastReminderComments: schemaContructor.string(),
+  }),
+  sale_customer: schemaContructor.object({
+    fullName: schemaContructor.string(),
+    picture: schemaContructor.subObject(),
+    name: schemaContructor.string(),
+    partnerSeq: schemaContructor.string(),
+    mainAddress: schemaContructor.subObject('fullName'),
+    mobilePhone: schemaContructor.string(),
+    fixedPhone: schemaContructor.string(),
+    emailAddress: schemaContructor.subObject('address'),
+    description: schemaContructor.string(),
+    factorizedCustomer: schemaContructor.boolean(),
+    hasBlockedAccount: schemaContructor.boolean(),
+  }),
+  sale_saleOrderLine: schemaContructor.object({
+    typeSelect: schemaContructor.number(),
+    product: schemaContructor.subObject().concat(
+      schemaContructor.object({
+        name: schemaContructor.string(),
+        picture: schemaContructor.subObject(),
+        code: schemaContructor.string(),
+        productFamily: schemaContructor.subObject('name'),
+        productCategory: schemaContructor.subObject('name'),
+        description: schemaContructor.string(),
+        configurator: schemaContructor.subObject(),
+      }),
+    ),
+    productName: schemaContructor.string(),
+    price: schemaContructor.number(),
+    unit: schemaContructor.subObject('name'),
+    qty: schemaContructor.number(),
+    inTaxTotal: schemaContructor.number(),
+    exTaxTotal: schemaContructor.number(),
+    description: schemaContructor.string(),
+    isShowTotal: schemaContructor.boolean(),
+    priceDiscounted: schemaContructor.number(),
+    inTaxPrice: schemaContructor.number(),
+    subTotalCostPrice: schemaContructor.number(),
+    subTotalGrossMargin: schemaContructor.number(),
+    subMarginRate: schemaContructor.number(),
+    subTotalMarkup: schemaContructor.number(),
+    saleSupplySelect: schemaContructor.number(),
+    pricingScaleLogs: schemaContructor.string(),
+  }),
+  sale_customerCategory: schemaContructor.object({
+    name: schemaContructor.string(),
+  }),
+};
