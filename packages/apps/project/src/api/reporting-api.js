6710326d22c803c516e897789b059991aa3b3be2
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-07-05 15:47:42 +0200

    feat: test api for indicator

---
+++ packages/apps/project/src/api/reporting-api.js
@@ -0,0 +1,47 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {axiosApiProvider} from '@axelor/aos-mobile-core';
+
+export async function getReportingData({projetId}) {
+  return axiosApiProvider.post({
+    url: 'ws/meta/custom/project-spent-time-custom',
+    data: {
+      data: {
+        _id: projetId,
+        _model: 'com.axelor.apps.project.db.Project',
+        _domainAction: 'action-project-view-spent-time-custom',
+        id: projetId,
+      },
+    },
+  });
+}
+
+export async function getReportingData2({projetId}) {
+  return axiosApiProvider.post({
+    url: 'ws/meta/custom/project-total-invoiced-custom',
+    data: {
+      data: {
+        _id: projetId,
+        _model: 'com.axelor.apps.project.db.Project',
+        _domainAction: 'action-project-view-total-invoiced-custom',
+        id: projetId,
+      },
+    },
+  });
+}
