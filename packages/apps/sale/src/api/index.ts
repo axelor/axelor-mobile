471c572eb18c420eac697d6c12835da0758166df
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-07-09 15:36:50 +0200

    feat: create complementary products screen (#630)
    * RM#80372

--- packages/apps/sale/src/api/index.ts
+++ packages/apps/sale/src/api/index.ts
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {fetchComplementaryProduct as fetchComplementaryProductApi} from './complementary-product-api';
 export {
   fetchCustomerById as fetchCustomerByIdApi,
   searchCustomer as searchCustomerApi,
