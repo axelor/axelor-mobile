077781fe54ae86ba4cc657fce0b597ff2c23c005
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-07-11 11:57:39 +0200

    feat: create new component ActionCard (#640)
    * RM#81256

---
+++ packages/ui/__tests__/components/organisms/ActionCard.test.js
@@ -0,0 +1,69 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import {ActionCard, InfoButton, Card} from '@axelor/aos-mobile-ui';
+import {getGlobalStyles} from '../../tools';
+
+describe('ActionCard Component', () => {
+  const actionList = [
+    {iconName: 'edit', onPress: jest.fn(), key: '1'},
+    {iconName: 'delete', onPress: jest.fn(), key: '2'},
+    {iconName: 'share', onPress: jest.fn(), key: '3'},
+  ];
+  const props = {
+    children: <Card>Card Content</Card>,
+    actionList,
+    translator: jest.fn(key => key),
+  };
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<ActionCard {...props} />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should render children correctly', () => {
+    const wrapper = shallow(<ActionCard {...props} />);
+
+    expect(wrapper.contains(props.children)).toBe(true);
+  });
+
+  it('should render InfoButton components for each action in the list', () => {
+    const wrapper = shallow(<ActionCard {...props} />);
+
+    expect(wrapper.find(InfoButton).length).toBe(actionList.length);
+  });
+
+  it('should call the appropriate onPress function when an action is pressed', () => {
+    const wrapper = shallow(<ActionCard {...props} />);
+
+    wrapper.find(InfoButton).at(0).simulate('press');
+    expect(actionList[0].onPress).toHaveBeenCalled();
+  });
+
+  it('should apply custom style to the container if provided', () => {
+    const customStyle = {width: 200};
+    const wrapper = shallow(<ActionCard {...props} style={customStyle} />);
+
+    expect(getGlobalStyles(wrapper.find('View').at(0))).toMatchObject(
+      customStyle,
+    );
+  });
+});
--- packages/ui/src/components/organisms/index.tsx
+++ packages/ui/src/components/organisms/index.tsx
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {default as ActionCard} from './ActionCard/ActionCard';
 export {default as AutoCompleteSearch} from './AutoCompleteSearch/AutoCompleteSearch';
 export {default as CheckboxScrollList} from './CheckboxScrollList/CheckboxScrollList';
 export {default as ChipSelect} from './ChipSelect/ChipSelect';
