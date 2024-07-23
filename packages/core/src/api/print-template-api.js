f355dfb8e598b5eb6ee8f49caf5f6572d02672a9
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-07-23 10:04:43 +0200

    feat: add print template (#650)
    * RM#78046

---
+++ packages/core/src/api/print-template-api.js
@@ -0,0 +1,109 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+  axiosApiProvider,
+  createStandardSearch,
+  getSearchCriterias,
+} from '../apiProviders';
+
+const createPrintingTemplateCriteria = ({searchValue, idList}) => {
+  return [
+    getSearchCriterias('core_printTemplate', searchValue),
+    {
+      fieldName: 'id',
+      operator: 'in',
+      value: idList,
+    },
+  ];
+};
+
+export async function fetchActionPrint({id, model}) {
+  return axiosApiProvider
+    .post({
+      url: 'ws/action',
+      data: {
+        action: 'action-method-print-template',
+        model: model,
+        data: {
+          context: {
+            id: id,
+            _model: model,
+          },
+        },
+      },
+    })
+    .then(res => res?.data?.data?.[0])
+    .then(res => {
+      const templateSet = res?.view?.context?._printingTemplateIdList;
+      const fileName = res.view?.views?.[0]?.name;
+      const error = res?.error?.message;
+
+      return {templateSet, fileName, error};
+    });
+}
+
+export async function searchPrintingTemplate({
+  searchValue = null,
+  page = 0,
+  idList,
+}) {
+  if (!Array.isArray(idList) || idList.length === 0) {
+    return {data: {data: [], total: 0}};
+  }
+
+  return createStandardSearch({
+    model: 'com.axelor.apps.base.db.PrintingTemplate',
+    criteria: createPrintingTemplateCriteria({idList, searchValue}),
+    fieldKey: 'core_printTemplate',
+    page,
+  });
+}
+
+export async function fetchFileToPrint({id, model, printingTemplate}) {
+  return axiosApiProvider
+    .post({
+      url: 'ws/action',
+      data: {
+        action: 'action-method-printing-template-config-wizard-print',
+        model: 'com.axelor.utils.db.Wizard',
+        data: {
+          context: {
+            _modelId: id,
+            _fromTestWizard: false,
+            _modelClass: model,
+            _viewType: 'form',
+            _viewName: 'printing-template-print-config-wizard',
+            _views: [
+              {
+                name: 'printing-template-print-config-wizard',
+                type: 'form',
+              },
+            ],
+            _model: 'com.axelor.utils.db.Wizard',
+            printingTemplate: printingTemplate,
+          },
+        },
+      },
+    })
+    .then(res => res?.data?.data?.[0])
+    .then(res => ({
+      error: res?.error?.message,
+      fileName: res.view?.views?.[0]?.name,
+    }));
+}
---
+++ packages/core/src/components/templates/PrintTemplateSearchBar/PrintTemplateSearchBar.tsx
@@ -0,0 +1,87 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback} from 'react';
+import {AutoCompleteSearch} from '@axelor/aos-mobile-ui';
+import {searchPrintingTemplate} from '../../../features/printTemplateSlice';
+import {useTranslator} from '../../../i18n';
+import {useDispatch, useSelector} from '../../../redux/hooks';
+import {displayItemName} from '../../../utils';
+
+interface PrintTemplateSearchBarProps {
+  style?: any;
+  title?: string;
+  defaultValue?: any;
+  onChange?: (value: any) => void;
+  required?: boolean;
+  readonly?: boolean;
+  idList: number[];
+}
+
+const PrintTemplateSearchBar = ({
+  style = null,
+  title = 'Base_PrintTemplate',
+  defaultValue = null,
+  onChange = () => {},
+  required = true,
+  readonly = false,
+  idList,
+}: PrintTemplateSearchBarProps) => {
+  const I18n = useTranslator();
+  const dispatch = useDispatch();
+
+  const {printTemplateList, loadingList, moreLoading, isListEnd} = useSelector(
+    state => state.printTemplate,
+  );
+
+  const searchPrintTemplateAPI = useCallback(
+    ({page = 0, searchValue}) => {
+      dispatch(
+        (searchPrintingTemplate as any)({
+          page,
+          searchValue,
+          idList: idList,
+        }),
+      );
+    },
+    [dispatch, idList],
+  );
+
+  return (
+    <AutoCompleteSearch
+      style={style}
+      title={I18n.t(title)}
+      objectList={printTemplateList}
+      value={defaultValue}
+      required={required}
+      readonly={readonly}
+      onChangeValue={onChange}
+      fetchData={searchPrintTemplateAPI}
+      displayValue={displayItemName}
+      placeholder={I18n.t(title)}
+      showDetailsPopup={true}
+      loadingList={loadingList}
+      moreLoading={moreLoading}
+      isListEnd={isListEnd}
+      navigate={false}
+      oneFilter={false}
+    />
+  );
+};
+
+export default PrintTemplateSearchBar;
