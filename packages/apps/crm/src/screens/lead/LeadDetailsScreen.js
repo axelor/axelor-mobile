c16cd56843cc2bcd20650bf6829605c7033fabb3
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-08-02 09:49:28 +0200

    fix: add scrollview style to enable scrollview (#662)
    * RM#80721

--- packages/apps/crm/src/screens/lead/LeadDetailsScreen.js
+++ packages/apps/crm/src/screens/lead/LeadDetailsScreen.js
@@ -17,6 +17,7 @@
  */
 
 import React, {useCallback, useEffect} from 'react';
+import {StyleSheet} from 'react-native';
 import {
   Screen,
   HeaderContainer,
@@ -66,7 +67,9 @@ const LeadDetailsScreen = ({route}) => {
           />
         }
       />
-      <ScrollView refresh={{loading: loadingLead, fetcher: getLead}}>
+      <ScrollView
+        refresh={{loading: loadingLead, fetcher: getLead}}
+        style={styles.scrollView}>
         <NotesCard title={I18n.t('Crm_Description')} data={lead.description} />
         <LeadDropdownCards />
       </ScrollView>
@@ -74,5 +77,11 @@ const LeadDetailsScreen = ({route}) => {
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    height: null,
+  },
+});
 
 export default LeadDetailsScreen;
