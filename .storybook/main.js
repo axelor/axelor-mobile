a37619c3ef71a1a671dd7e7c39c705e1077ebf3b
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-08-26 12:22:09 +0200

    chore: update React Native 0.73 (#678)
    * RM#83197

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,10 +19,7 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: [
-            '@babel/preset-env',
-            'module:metro-react-native-babel-preset',
-          ],
+          presets: ['@babel/preset-env', 'module:@react-native/babel-preset'],
           plugins: ['react-native-reanimated/plugin'],
         },
       },
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-react'],
-  env: {
-    test: {
-      presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/preset-typescript',
-      ],
-    },
-  },
+  presets: ['module:@react-native/babel-preset', '@babel/preset-react'],
+  env: {test: {presets: ['@babel/preset-typescript']}},
   plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        globals: ['__scanCodes'],
-      },
-    ],
+    ['react-native-reanimated/plugin', {globals: ['__scanCodes']}],
     ['@babel/plugin-transform-private-methods', {loose: true}],
     ['@babel/plugin-transform-class-properties', {loose: true}],
     ['@babel/plugin-transform-private-property-in-object', {loose: true}],
