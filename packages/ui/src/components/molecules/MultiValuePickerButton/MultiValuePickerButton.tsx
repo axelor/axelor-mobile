ef0175333e398195fc551e2bcac63b418e4efad7
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-09-12 09:57:53 +0200

    fix: apply max width on text item instead of container
    * RM#84026

--- packages/ui/src/components/molecules/MultiValuePickerButton/MultiValuePickerButton.tsx
+++ packages/ui/src/components/molecules/MultiValuePickerButton/MultiValuePickerButton.tsx
@@ -100,6 +100,7 @@ const getStyles = (Colors: ThemeColors) =>
     text: {
       textAlign: 'center',
       marginRight: 5,
+      maxWidth: 100,
     },
     listItemContainer: {
       flexDirection: 'row',
@@ -119,7 +120,6 @@ const getStyles = (Colors: ThemeColors) =>
       marginLeft: 5,
       paddingHorizontal: 7,
       height: 22,
-      maxWidth: 110,
     },
   });
 
