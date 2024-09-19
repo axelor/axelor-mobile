0624fce0cb4b222f26265a00dc2e8047ce1dacf0
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-09-19 17:05:54 +0200

    refacgtor: restore space

--- packages/apps/project/src/screens/ProjectDetailsScreen.tsx
+++ packages/apps/project/src/screens/ProjectDetailsScreen.tsx
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 import React, {useEffect} from 'react';
 import {Screen, BottomBar, useThemeColor} from '@axelor/aos-mobile-ui';
 import {useDispatch, useSelector} from '@axelor/aos-mobile-core';
