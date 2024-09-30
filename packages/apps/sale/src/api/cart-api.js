75f456783afdfd019b788f8c98b31f92c65bda26
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-09-30 13:02:46 +0200

    feat: enable company update on Cart (#719)
    * RM#83738

--- packages/apps/sale/src/api/cart-api.js
+++ packages/apps/sale/src/api/cart-api.js
@@ -45,21 +45,30 @@ export async function searchCart({searchValue, page = 0, userId}) {
   });
 }
 
-export async function updateCart({partnerId, cartId, cartVersion}) {
+export async function updateCart({partnerId, companyId, cartId, cartVersion}) {
+  let data = {id: cartId, version: cartVersion};
+
+  if (companyId != null) {
+    data = {
+      ...data,
+      company: {
+        id: companyId,
+      },
+    };
+  }
+
+  if (partnerId != null) {
+    data = {
+      ...data,
+      partner: {
+        id: partnerId,
+      },
+    };
+  }
+
   return axiosApiProvider.post({
     url: '/ws/rest/com.axelor.apps.sale.db.Cart',
-    data: {
-      data: {
-        id: cartId,
-        version: cartVersion,
-        partner:
-          partnerId != null
-            ? {
-                id: partnerId,
-              }
-            : null,
-      },
-    },
+    data: {data},
   });
 }
 
