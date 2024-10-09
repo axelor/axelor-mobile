f4b7576e4307b59e7c1d4669b4d46d02aff83610
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-10-09 09:44:55 +0200

    fix: restrict DraggableWrapper to avoid sending it out of screen (#739)
    * RM#84909

--- packages/ui/src/components/organisms/DraggableWrapper/DraggableWrapper.tsx
+++ packages/ui/src/components/organisms/DraggableWrapper/DraggableWrapper.tsx
@@ -17,21 +17,28 @@
  */
 
 import React, {useMemo, useRef, useState} from 'react';
-import {Animated, PanResponder, StyleSheet} from 'react-native';
+import {Animated, Dimensions, PanResponder, StyleSheet} from 'react-native';
 import {useConfig} from '../../../config/ConfigContext';
 
 export interface DraggableWrapperProps {
+  style?: any;
   children: any;
 }
 
-const DraggableWrapper = ({children}: DraggableWrapperProps) => {
-  const {setIsScrollEnabled} = useConfig();
+const DraggableWrapper = ({style, children}: DraggableWrapperProps) => {
+  const {headerHeight, setIsScrollEnabled} = useConfig();
 
   const [dragging, setDragging] = useState(false);
+  const [isWindowPositionGet, setIsWindowPositionGet] = useState(false);
 
+  const wrapperRef = useRef(null);
+  const screenPosition = useRef({x: 0, y: 0});
+  const viewLayout = useRef({width: 0, height: 0});
   const position = useRef(new Animated.ValueXY()).current;
   const offsetX = useRef(0);
   const offsetY = useRef(0);
+
+  const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
   const panResponder = useRef(
     PanResponder.create({
@@ -60,8 +67,40 @@ const DraggableWrapper = ({children}: DraggableWrapperProps) => {
         setDragging(false);
         setIsScrollEnabled(true);
         position.flattenOffset();
-        offsetX.current = (position.x as any).__getValue();
-        offsetY.current = (position.y as any).__getValue();
+
+        const newPosX = (position.x as any).__getValue();
+        const newPosY = (position.y as any).__getValue();
+
+        const {width: layoutWidth, height: layoutHeight} = viewLayout.current;
+
+        let adjustedPosX = newPosX + screenPosition.current.x;
+        let adjustedPosY = newPosY + screenPosition.current.y;
+
+        if (adjustedPosX < 0) {
+          adjustedPosX = 0;
+        } else if (adjustedPosX + layoutWidth > screenWidth) {
+          adjustedPosX = screenWidth - layoutWidth;
+        }
+
+        if (adjustedPosY < headerHeight) {
+          adjustedPosY = headerHeight;
+        } else if (adjustedPosY + layoutHeight > screenHeight) {
+          adjustedPosY = screenHeight - layoutHeight;
+        }
+
+        adjustedPosX -= screenPosition.current.x;
+        adjustedPosY -= screenPosition.current.y;
+
+        Animated.spring(position, {
+          toValue: {
+            x: adjustedPosX,
+            y: adjustedPosY,
+          },
+          useNativeDriver: false,
+        }).start();
+
+        offsetX.current = adjustedPosX;
+        offsetY.current = adjustedPosY;
       },
       onPanResponderTerminate: () => {
         setDragging(false);
@@ -82,7 +121,27 @@ const DraggableWrapper = ({children}: DraggableWrapperProps) => {
           transform: position.getTranslateTransform(),
         },
         styles.container,
+        style,
       ]}
+      ref={wrapperRef}
+      onLayout={event => {
+        const {width, height} = event.nativeEvent.layout;
+
+        if (isWindowPositionGet) {
+          const screenPositionX =
+            screenPosition.current.x - width + viewLayout.current.width;
+          const screenPositionY =
+            screenPosition.current.y - height + viewLayout.current.height;
+          screenPosition.current = {x: screenPositionX, y: screenPositionY};
+        } else {
+          wrapperRef.current.measureInWindow((x, y) => {
+            screenPosition.current = {x, y};
+          });
+          setIsWindowPositionGet(true);
+        }
+
+        viewLayout.current = {width, height};
+      }}
       {...panResponder.panHandlers}>
       {children}
     </Animated.View>
