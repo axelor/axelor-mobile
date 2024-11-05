40448ae99261193e67047a48a3cd9b47381dbc84
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-11-05 15:36:42 +0100

    fix: trigger period computation on Timehseet update (#786)
    * RM#86455

--- packages/apps/hr/src/components/molecules/TimesheetDetailCard/TimesheetDetailCard.tsx
+++ packages/apps/hr/src/components/molecules/TimesheetDetailCard/TimesheetDetailCard.tsx
@@ -95,7 +95,7 @@ const TimesheetDetailCard = ({
         }
       })
       .catch(() => setConvertedPeriod(0));
-  }, [item.id]);
+  }, [item]);
 
   return (
     <View style={[styles.container, style]}>
