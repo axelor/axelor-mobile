0125cd2d68bd7effd81b403ab30cf7a841254f3c
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-11-07 15:35:18 +0100

    fix: TreeView margin issue on branch card (#795)
    * RM#86621

--- packages/ui/src/components/templates/TreeView/BranchCard.tsx
+++ packages/ui/src/components/templates/TreeView/BranchCard.tsx
@@ -77,6 +77,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginVertical: 2,
     paddingHorizontal: 15,
     paddingRight: 15,
   },
