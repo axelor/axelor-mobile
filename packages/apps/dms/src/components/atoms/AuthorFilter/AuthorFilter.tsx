5393f839429932f72966150b3f776c1a6859bb13
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-11-29 11:20:02 +0100

    feat: create all documents view (#813)
    * RM#86485

---
+++ packages/apps/dms/src/components/atoms/AuthorFilter/AuthorFilter.tsx
@@ -0,0 +1,73 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {UserSearchBar, useSelector} from '@axelor/aos-mobile-core';
+import {ToggleButton} from '@axelor/aos-mobile-ui';
+
+interface AuthorFilterProps {
+  author: any;
+  setAuthor: (author: any) => void;
+}
+
+const AuthorFilter = ({author, setAuthor}: AuthorFilterProps) => {
+  const {user} = useSelector(state => state.user);
+
+  return (
+    <View style={styles.container}>
+      <ToggleButton
+        isActive={author?.id === user?.id}
+        onPress={() =>
+          setAuthor(current => (current?.id === user?.id ? null : user))
+        }
+        buttonConfig={{
+          iconName: 'person-fill',
+          style: styles.toggleButton,
+        }}
+      />
+      <UserSearchBar
+        style={styles.userSearchBar}
+        defaultValue={author}
+        onChange={setAuthor}
+        showTitle={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    alignItems: 'center',
+    width: '90%',
+  },
+  toggleButton: {
+    width: '10%',
+    height: 40,
+    marginVertical: 0,
+  },
+  userSearchBar: {
+    width: '88%',
+  },
+});
+
+export default AuthorFilter;
