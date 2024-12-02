b1fab230b5b012441b828df6a21000dc1f783c20
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-12-02 14:25:49 +0100

    feat: add RequestListView

---
+++ packages/apps/purchase/src/models/typeObjects.ts
@@ -0,0 +1,67 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ModuleSelections} from '@axelor/aos-mobile-core';
+
+export const purchase_typeObjects: ModuleSelections = [
+  {
+    modelName: 'com.axelor.apps.purchase.db.PurchaseRequest',
+    fields: {
+      statusSelect: {
+        content: [
+          {
+            key: 'Draft',
+            value: 1,
+            title: 'Purchase_Status_Draft',
+            color: 'secondaryColor',
+          },
+          {
+            key: 'Requested',
+            value: 2,
+            title: 'Purchase_Status_Requested',
+            color: 'progressColor',
+          },
+          {
+            key: 'Accepted',
+            value: 3,
+            title: 'Purchase_Status_Planned',
+            color: 'plannedColor',
+          },
+          {
+            key: 'Purchased',
+            value: 4,
+            title: 'Purchase_Status_Purchased',
+            color: 'successColor',
+          },
+          {
+            key: 'Refused',
+            value: 5,
+            title: 'Purchase_Status_Refused',
+            color: 'cautionColor',
+          },
+          {
+            key: 'Canceled',
+            value: 6,
+            title: 'Purchase_Status_Canceled',
+            color: 'errorColor',
+          },
+        ],
+      },
+    },
+  },
+];
