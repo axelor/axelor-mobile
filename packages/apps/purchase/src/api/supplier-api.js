d536be455b27a4bda4f5fa126de279327d45bd86
Gregory <117281520+gca-axelor@users.noreply.github.com>
2024-12-06 11:05:47 +0100

    refactor: clean code

--- packages/apps/purchase/src/api/supplier-api.js
+++ packages/apps/purchase/src/api/supplier-api.js
@@ -44,7 +44,6 @@ export async function searchSupplier({page = 0, searchValue}) {
     model: 'com.axelor.apps.base.db.Partner',
     criteria: createSupplierCriteria(searchValue),
     fieldKey: 'purchase_supplier',
-    sortKey: 'purchase_supplier',
     page,
   });
 }
