51b5a2c0349cf990e934284b5225cc8ff6fa976c
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-12-09 13:29:47 +0100

    feat: create favorites files screen (#818)
    * RM#86496

--- packages/apps/dms/src/screens/MyFavoriteDocumentsScreen.tsx
+++ packages/apps/dms/src/screens/MyFavoriteDocumentsScreen.tsx
@@ -16,13 +16,62 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
-import {Screen, Text} from '@axelor/aos-mobile-ui';
+import React, {useMemo, useState} from 'react';
+import {
+  useSelector,
+  SearchListView,
+  useTranslator,
+} from '@axelor/aos-mobile-core';
+import {ChipSelect, Screen, useThemeColor} from '@axelor/aos-mobile-ui';
+import {AuthorFilter, DocumentActionCard} from '../components';
+import {searchFavoriteDocument} from '../features/documentSlice';
+import {File} from '../types';
 
 const MyFavoriteDocumentsScreen = ({}) => {
+  const I18n = useTranslator();
+  const Colors = useThemeColor();
+
+  const [author, setAuthor] = useState(null);
+  const [selectedExtensions, setSelectedExtensions] = useState([]);
+
+  const {user} = useSelector(state => state.user);
+  const {
+    loadingFavoriteDocument,
+    moreLoadingFavoriteDocument,
+    isListEndFavoriteDocument,
+    favoriteDocumentList,
+  } = useSelector(state => state.dms_document);
+
+  const sliceFunctionData = useMemo(
+    () => ({
+      authorId: author?.id,
+      extensions: selectedExtensions.map(_extension => _extension.key),
+      favoriteFileIds: user?.favouriteFileSet?.map(file => file.id),
+    }),
+    [author?.id, selectedExtensions, user?.favouriteFileSet],
+  );
+
   return (
     <Screen removeSpaceOnTop={true}>
-      <Text>MyFavoriteDocumentsScreen</Text>
+      <SearchListView
+        list={favoriteDocumentList}
+        loading={loadingFavoriteDocument}
+        moreLoading={moreLoadingFavoriteDocument}
+        isListEnd={isListEndFavoriteDocument}
+        sliceFunction={searchFavoriteDocument}
+        sliceFunctionData={sliceFunctionData}
+        displaySearchValue={item => item.fileName}
+        searchPlaceholder={I18n.t('Base_Search')}
+        headerChildren={<AuthorFilter author={author} setAuthor={setAuthor} />}
+        chipComponent={
+          <ChipSelect
+            mode="multi"
+            selectionItems={File.getFileExtensionList(Colors)}
+            onChangeValue={setSelectedExtensions}
+          />
+        }
+        renderListItem={({item}) => <DocumentActionCard document={item} />}
+      />
     </Screen>
   );
 };
