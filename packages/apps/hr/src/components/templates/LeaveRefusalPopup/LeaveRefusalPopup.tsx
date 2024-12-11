ee9c3d5473394f18e622d6d33bb0f9d0f89aa35a
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-12-11 12:01:04 +0100

    feat: add action on leave request (#830)
    * RM#88008

---
+++ packages/apps/hr/src/components/templates/LeaveRefusalPopup/LeaveRefusalPopup.tsx
@@ -0,0 +1,66 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback} from 'react';
+import {useDispatch, useSelector} from '@axelor/aos-mobile-core';
+import {RefusalPopup} from '../../molecules';
+import {rejectLeave} from '../../../features/leaveSlice';
+
+interface LeaveRefusalPopupProps {
+  isOpen: boolean;
+  leaveId: number;
+  leaveVersion: number;
+  handleClose: () => void;
+}
+
+const LeaveRefusalPopup = ({
+  isOpen,
+  leaveId,
+  leaveVersion,
+  handleClose,
+}: LeaveRefusalPopupProps) => {
+  const dispatch = useDispatch();
+
+  const {user} = useSelector(state => state.user);
+
+  const rejectLeaveAPI = useCallback(
+    (refusalMessage: string) => {
+      handleClose();
+      dispatch(
+        (rejectLeave as any)({
+          leaveRequestId: leaveId,
+          version: leaveVersion,
+          user: user,
+          userId: user.id,
+          groundForRefusal: refusalMessage,
+        }),
+      );
+    },
+    [dispatch, handleClose, leaveId, leaveVersion, user],
+  );
+
+  return (
+    <RefusalPopup
+      isOpen={isOpen}
+      onCancel={handleClose}
+      onValidate={rejectLeaveAPI}
+    />
+  );
+};
+
+export default LeaveRefusalPopup;
--- packages/apps/hr/src/features/leaveSlice.js
+++ packages/apps/hr/src/features/leaveSlice.js
@@ -22,10 +22,15 @@ import {
   generateInifiniteScrollCases,
 } from '@axelor/aos-mobile-core';
 import {
+  cancelLeave as _cancelLeave,
+  deleteLeave as _deleteLeave,
   fetchLeave as _fetchLeave,
   fetchLeaveById as _fetchLeaveById,
   fetchLeaveReason as _fetchLeaveReason,
   fetchLeaveToValidate as _fetchLeaveToValidate,
+  rejectLeave as _rejectLeave,
+  sendLeave as _sendLeave,
+  validateLeave as _validateLeave,
 } from '../api/leave-api';
 
 export const fetchLeave = createAsyncThunk(
@@ -76,6 +81,89 @@ export const fetchLeaveReason = createAsyncThunk(
       action: 'Hr_SliceAction_FetchLeaveReason',
       getState,
       responseOptions: {isArrayResponse: true},
+    });
+  },
+);
+
+export const sendLeave = createAsyncThunk(
+  'hr_leave/sendLeave',
+  async function (data, {getState, dispatch}) {
+    return handlerApiCall({
+      fetchFunction: _sendLeave,
+      data,
+      action: 'Hr_SliceAction_SendLeave',
+      getState,
+      responseOptions: {isArrayResponse: false},
+    }).then(() => {
+      dispatch(fetchLeave(data));
+      dispatch(fetchLeaveToValidate({user: data.user}));
+      dispatch(fetchLeaveById({leaveId: data.leaveRequestId}));
+    });
+  },
+);
+
+export const validateLeave = createAsyncThunk(
+  'hr_leave/validateLeave',
+  async function (data, {getState, dispatch}) {
+    return handlerApiCall({
+      fetchFunction: _validateLeave,
+      data,
+      action: 'Hr_SliceAction_ValidateLeave',
+      getState,
+      responseOptions: {isArrayResponse: false},
+    }).then(() => {
+      dispatch(fetchLeave(data));
+      dispatch(fetchLeaveToValidate({user: data.user}));
+      dispatch(fetchLeaveById({leaveId: data.leaveRequestId}));
+    });
+  },
+);
+
+export const cancelLeave = createAsyncThunk(
+  'hr_leave/cancelLeave',
+  async function (data, {getState, dispatch}) {
+    return handlerApiCall({
+      fetchFunction: _cancelLeave,
+      data,
+      action: 'Hr_SliceAction_CancelLeave',
+      getState,
+      responseOptions: {isArrayResponse: false},
+    }).then(() => {
+      dispatch(fetchLeave(data));
+      dispatch(fetchLeaveToValidate({user: data.user}));
+      dispatch(fetchLeaveById({leaveId: data.leaveRequestId}));
+    });
+  },
+);
+
+export const rejectLeave = createAsyncThunk(
+  'hr_leave/rejectLeave',
+  async function (data, {getState, dispatch}) {
+    return handlerApiCall({
+      fetchFunction: _rejectLeave,
+      data,
+      action: 'Hr_SliceAction_RejectLeave',
+      getState,
+      responseOptions: {isArrayResponse: false},
+    }).then(() => {
+      dispatch(fetchLeave(data));
+      dispatch(fetchLeaveToValidate({user: data.user}));
+      dispatch(fetchLeaveById({leaveId: data.leaveRequestId}));
+    });
+  },
+);
+
+export const deleteLeave = createAsyncThunk(
+  'hr_leave/deleteLeave',
+  async function (data, {getState, dispatch}) {
+    return handlerApiCall({
+      fetchFunction: _deleteLeave,
+      data,
+      action: 'Hr_SliceAction_DeleteLeave',
+      getState,
+      responseOptions: {isArrayResponse: false},
+    }).then(() => {
+      dispatch(fetchLeave(data));
     });
   },
 );
