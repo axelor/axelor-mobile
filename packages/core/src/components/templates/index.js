92c7d67053b9f9c4f104ba008488bd3649896a57
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-12-17 10:21:38 +0100

    refactor: remove view & move logic to screen

--- packages/core/src/components/templates/index.js
+++ packages/core/src/components/templates/index.js
@@ -17,7 +17,6 @@
  */
 
 export {default as GlobalToolBox} from './GlobalToolBox/GlobalToolBox';
-export {default as MailMessageAttachedFilesView} from './MailMessageAttachedFilesView/MailMessageAttachedFilesView';
 export {default as MailMessageView} from './MailMessageView/MailMessageView';
 export {default as PeriodInput} from './PeriodInput/PeriodInput';
 export {default as PopupApplicationInformation} from './PopupApplicationInformation/PopupApplicationInformation';
--- packages/core/src/screens/MailMessageAttachedFilesScreen.tsx
+++ packages/core/src/screens/MailMessageAttachedFilesScreen.tsx
@@ -16,13 +16,120 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
-import {MailMessageAttachedFilesView} from '../components';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {
+  AttachmentCard,
+  ChipSelect,
+  File,
+  HeaderContainer,
+  Screen,
+  ScrollList,
+  useThemeColor,
+} from '@axelor/aos-mobile-ui';
+import {useTranslator} from '../i18n';
+import {useDispatch, useSelector} from '../redux/hooks';
+import {openFileInExternalApp} from '../tools';
+import {getAttachedFilesDetails} from '../features/attachedFilesSlice';
 
 const MailMessageAttachedFilesScreen = ({route}) => {
   const {files} = route.params;
+  const Colors = useThemeColor();
+  const I18n = useTranslator();
+  const dispatch = useDispatch();
 
-  return <MailMessageAttachedFilesView files={files} />;
+  const {baseUrl, token, jsessionId} = useSelector(state => state.auth);
+  const {loading, attachedFilesList} = useSelector(
+    state => state.attachedFiles,
+  );
+
+  const [selectedStatus, setSelectedStatus] = useState([]);
+  const [extensionList, setExtensionList] = useState([]);
+
+  const handleShowFile = async item => {
+    await openFileInExternalApp(
+      {fileName: item.fileName, id: item.id, isMetaFile: true},
+      {baseUrl: baseUrl, token: token, jsessionId: jsessionId},
+      I18n,
+    );
+  };
+
+  const fetchFilesAPI = useCallback(() => {
+    dispatch(
+      (getAttachedFilesDetails as any)({
+        listFiles: files,
+      }),
+    );
+  }, [dispatch, files]);
+
+  const filterOnSelectExtension = useCallback(
+    list => {
+      if (!Array.isArray(list) || list.length === 0) {
+        return list;
+      } else if (selectedStatus.length > 0) {
+        return list.filter(item =>
+          selectedStatus.find(
+            status => File.getFileExtension(item.fileName) === status.key,
+          ),
+        );
+      } else {
+        return list;
+      }
+    },
+    [selectedStatus],
+  );
+
+  const filteredList = useMemo(
+    () => filterOnSelectExtension(attachedFilesList),
+    [filterOnSelectExtension, attachedFilesList],
+  );
+
+  useEffect(() => {
+    setExtensionList(
+      Array.from(
+        new Set(
+          attachedFilesList?.map(item => File.getFileExtension(item.fileName)),
+        ),
+      ),
+    );
+  }, [files, attachedFilesList]);
+
+  return (
+    <Screen removeSpaceOnTop={true}>
+      <HeaderContainer
+        expandableFilter={false}
+        chipComponent={
+          <ChipSelect
+            mode="multi"
+            onChangeValue={chiplist => setSelectedStatus(chiplist)}
+            selectionItems={extensionList.map(ext => {
+              return {
+                title: `${ext}`.toUpperCase(),
+                color: Colors.primaryColor,
+                key: ext,
+              };
+            })}
+          />
+        }
+      />
+      <ScrollList
+        loadingList={loading}
+        data={filteredList}
+        renderItem={({item}) => (
+          <AttachmentCard
+            fileName={item.fileName}
+            onPress={() => handleShowFile(item)}
+            creationDate={item.createdOn}
+            translator={I18n.t}
+          />
+        )}
+        fetchData={fetchFilesAPI}
+        filter={true}
+        moreLoading={false}
+        isListEnd={true}
+        translator={I18n.t}
+      />
+    </Screen>
+  );
 };
 
 export default MailMessageAttachedFilesScreen;
