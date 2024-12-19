093f785480e54bb0bf192166bf99adc8a24f3393
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2024-12-19 11:08:37 +0100

    feat: add requestDetails screen (#834)
    * RM#87191

---
+++ packages/apps/purchase/src/components/atoms/DropdownRequestCharacteristics/DropdownRequestCharacteristics.tsx
@@ -0,0 +1,80 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import {checkNullString, Label, LabelText} from '@axelor/aos-mobile-ui';
+import {useSelector, useTranslator} from '@axelor/aos-mobile-core';
+
+const DropdownRequestCharacteristics = () => {
+  const I18n = useTranslator();
+
+  const {purchaseRequest} = useSelector(
+    state => state.purchase_purchaseRequest,
+  );
+
+  if (
+    checkNullString(purchaseRequest.supplierPartner?.fullName) &&
+    checkNullString(purchaseRequest.stockLocation?.name) &&
+    checkNullString(purchaseRequest.requesterUser?.fullName) &&
+    checkNullString(purchaseRequest.validatorUser?.fullName)
+  ) {
+    return (
+      <Label
+        message={I18n.t('Purchase_NoInformationAvailable')}
+        type="info"
+        iconName="info-circle-fill"
+      />
+    );
+  }
+
+  return (
+    <View>
+      {!checkNullString(purchaseRequest.supplierPartner?.fullName) && (
+        <LabelText
+          title={purchaseRequest.supplierPartner?.fullName}
+          iconName="person-fill"
+          textSize={16}
+        />
+      )}
+      {!checkNullString(purchaseRequest.stockLocation?.name) && (
+        <LabelText
+          title={`${I18n.t('Purchase_StockLocation')} :`}
+          value={purchaseRequest.stockLocation?.name}
+          textSize={16}
+        />
+      )}
+      {!checkNullString(purchaseRequest.requesterUser?.fullName) && (
+        <LabelText
+          title={`${I18n.t('Purchase_RequesterUser')} :`}
+          value={purchaseRequest.requesterUser?.fullName}
+          textSize={16}
+        />
+      )}
+      {!checkNullString(purchaseRequest.validatorUser?.fullName) && (
+        <LabelText
+          title={`${I18n.t('Purchase_ValidatorUser')} :`}
+          value={purchaseRequest.validatorUser?.fullName}
+          textSize={16}
+        />
+      )}
+    </View>
+  );
+};
+
+export default DropdownRequestCharacteristics;
--- packages/apps/purchase/src/components/atoms/index.ts
+++ packages/apps/purchase/src/components/atoms/index.ts
@@ -16,4 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export {default as DropdownRequestCharacteristics} from './DropdownRequestCharacteristics/DropdownRequestCharacteristics';
+export {default as RequestSeeLinesButton} from './RequestSeeLinesButton/RequestSeeLinesButton';
 export {default as RequestCard} from './RequestCard/RequestCard';
+export {default as RequestHeader} from './RequestHeader/RequestHeader';
