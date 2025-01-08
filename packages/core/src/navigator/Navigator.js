f7e22e1a3a060b4fbc8ffc19d6f106476a1ed7ab
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-01-08 11:12:39 +0100

    refactor: stack not reset on navigation (#861)
    * RM#69728

--- packages/core/src/navigator/Navigator.js
+++ packages/core/src/navigator/Navigator.js
@@ -262,6 +262,7 @@ const Navigator = ({mainMenu, onRefresh, versionCheckConfig}) => {
                 name={key}
                 options={{
                   title: getMenuTitle(menu, {I18n}),
+                  unmountOnBlur: true,
                 }}>
                 {props =>
                   menu.screen && (
@@ -280,6 +281,7 @@ const Navigator = ({mainMenu, onRefresh, versionCheckConfig}) => {
                       name={subMenukey}
                       options={{
                         title: getMenuTitle(subMenu, {I18n}),
+                        unmountOnBlur: true,
                       }}>
                       {props => (
                         <ModulesScreensStackNavigator
