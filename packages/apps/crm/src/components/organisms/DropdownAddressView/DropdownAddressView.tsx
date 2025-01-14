a27e2449292240efe76f2cca8e3c820e0df837d9
Gregory <117281520+gca-axelor@users.noreply.github.com>
2025-01-14 11:50:01 +0100

    feat: add logic for icons

--- packages/apps/crm/src/components/organisms/DropdownAddressView/DropdownAddressView.tsx
+++ packages/apps/crm/src/components/organisms/DropdownAddressView/DropdownAddressView.tsx
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useEffect} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useMemo} from 'react';
+import {View, StyleSheet} from 'react-native';
 import {
   useTranslator,
   linkingProvider,
@@ -25,7 +25,7 @@ import {
   useSelector,
   clipboardProvider,
 } from '@axelor/aos-mobile-core';
-import {Icon, Text} from '@axelor/aos-mobile-ui';
+import {Icon, Text, useThemeColor} from '@axelor/aos-mobile-ui';
 import {fetchPartnerAddressByIds} from '../../../features/partnerSlice';
 
 interface Address {
@@ -46,45 +46,76 @@ const DropdownAddressView = ({
   partnerAddressList,
 }: DropdownAddressViewProps) => {
   const I18n = useTranslator();
+  const Colors = useThemeColor();
   const dispatch = useDispatch();
 
   const {partnerAddress} = useSelector(state => state.partner);
 
-  console.log(partnerAddress);
+  const styles = useMemo(() => {
+    return getStyles(Colors);
+  }, [Colors]);
 
   useEffect(() => {
     const idList = partnerAddressList?.map(item => item.id);
     dispatch((fetchPartnerAddressByIds as any)({partnerAddressIds: idList}));
   }, [dispatch, partnerAddressList]);
 
-  const renderAddressItem = (_partnerAddress: Address) => {
+  const renderAddressItem = (_partnerAddress: Address, index: number) => {
+    const isLastItem = index === partnerAddress.length - 1;
+
+    const icons = [
+      _partnerAddress.isInvoicingAddr && {name: 'star'},
+      _partnerAddress.isDefaultAddr && {name: 'card-list'},
+      _partnerAddress.isDeliveryAddr && {name: 'cart-fill'},
+    ].filter(Boolean);
+
     return (
-      <View key={_partnerAddress.id}>
-        <Text>{_partnerAddress.address?.fullName}</Text>
-        <Icon
-          name="copy"
-          touchable
-          onPress={() =>
-            clipboardProvider.copyToClipboard(_partnerAddress.address?.fullName)
-          }
-        />
-        <Icon
-          name="clipboard-fill"
-          touchable
-          onPress={() =>
-            linkingProvider.openMapApp(_partnerAddress.address?.fullName)
-          }
-        />
-        <Icon name="card-list" visible={_partnerAddress.isDefaultAddr} />
+      <View style={styles.container} key={_partnerAddress.id}>
+        <View style={styles.containerBody}>
+          {icons.length === 1 && (
+            <Icon name={icons[0].name} style={styles.leftIcon} />
+          )}
 
-        <Icon name="cart-fill" visible={_partnerAddress.isDeliveryAddr} />
+          {icons.length === 2 && (
+            <View style={styles.twoIconsStack}>
+              {icons.map((icon, idx) => (
+                <Icon key={idx} name={icon.name} />
+              ))}
+            </View>
+          )}
 
-        <Icon name="star" visible={_partnerAddress.isInvoicingAddr} />
+          {icons.length === 3 && <Icon name="star" style={styles.leftIcon} />}
+
+          <Text fontSize={14} style={styles.title}>
+            {_partnerAddress.address?.fullName}
+          </Text>
+          <View style={styles.containerItem}>
+            <Icon
+              name="pin-map-fill"
+              touchable
+              onPress={() =>
+                linkingProvider.openMapApp(_partnerAddress.address?.fullName)
+              }
+            />
+            <Icon
+              name="copy"
+              touchable
+              onPress={() =>
+                clipboardProvider.copyToClipboard(
+                  _partnerAddress.address?.fullName,
+                )
+              }
+            />
+            <Icon name="pencil-fill" touchable onPress={() => {}} />
+          </View>
+        </View>
+
+        {!isLastItem && <View style={styles.borderBottom} />}
       </View>
     );
   };
 
-  if (!partnerAddressList || partnerAddressList.length === 0) {
+  if (!partnerAddress || partnerAddress.length === 0) {
     return (
       <View>
         <Text>{I18n.t('Crm_NoAddressInformation')}</Text>
@@ -100,5 +131,42 @@ const DropdownAddressView = ({
     </View>
   );
 };
+
+const getStyles = Colors =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+      width: '95%',
+      marginHorizontal: 5,
+    },
+    containerBody: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: '2%',
+    },
+    containerItem: {
+      flexDirection: 'row',
+      gap: 10,
+    },
+    borderBottom: {
+      width: '112%',
+      left: '-5%',
+      borderBottomWidth: 1.5,
+      borderBottomColor: Colors.secondaryColor.background,
+      marginVertical: '3%',
+    },
+    leftIcon: {
+      marginRight: 10,
+    },
+    twoIconsStack: {
+      flexDirection: 'column',
+      marginRight: 10,
+      gap: 5,
+    },
+    title: {
+      flex: 1,
+    },
+  });
 
 export default DropdownAddressView;
