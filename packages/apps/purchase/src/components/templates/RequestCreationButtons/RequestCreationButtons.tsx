7c98319314a1d651c9a4adf9ee0c698f161546dd
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2025-01-14 18:00:01 +0100

    feat: add request creation screen (#829)
    * RM#87190

---
+++ packages/apps/purchase/src/components/templates/RequestCreationButtons/RequestCreationButtons.tsx
@@ -0,0 +1,118 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useMemo} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {useTranslator} from '@axelor/aos-mobile-core';
+import {Button, useThemeColor} from '@axelor/aos-mobile-ui';
+import {RequestCreation} from '../../../types';
+
+interface RequestCreationButtonsProps {
+  step: number;
+  setStep: (step: number) => void;
+  lines: any[];
+  isEditionMode?: boolean;
+  disabled?: boolean;
+  addLine: () => void;
+}
+
+const RequestCreationButtons = ({
+  step,
+  setStep,
+  lines,
+  isEditionMode = false,
+  disabled = false,
+  addLine,
+}: RequestCreationButtonsProps) => {
+  const Colors = useThemeColor();
+  const I18n = useTranslator();
+
+  const isValidateLineStep = useMemo(
+    () => step === RequestCreation.step.validateLine,
+    [step],
+  );
+
+  const handleFinishPress = () => {
+    if (isValidateLineStep) {
+      addLine();
+    }
+    setStep(RequestCreation.step.finish);
+  };
+
+  const handleRealizePress = useCallback(() => {}, []);
+
+  if (
+    (step === RequestCreation.step.addLine && lines.length >= 1) ||
+    isValidateLineStep
+  ) {
+    return (
+      <View style={styles.container}>
+        {isValidateLineStep && (
+          <Button
+            title={I18n.t(isEditionMode ? 'Base_Save' : 'Base_Add')}
+            iconName={isEditionMode ? 'floppy-fill' : 'plus-lg'}
+            color={Colors.progressColor}
+            width="45%"
+            disabled={disabled}
+            onPress={addLine}
+          />
+        )}
+        <Button
+          title={I18n.t('Base_Finish')}
+          iconName="check-lg"
+          width={isValidateLineStep ? '45%' : '90%'}
+          disabled={isValidateLineStep && disabled}
+          onPress={handleFinishPress}
+        />
+      </View>
+    );
+  }
+
+  if (step === RequestCreation.step.finish) {
+    return (
+      <View style={styles.container}>
+        <Button
+          title={I18n.t('Base_Add')}
+          iconName="arrow-left"
+          color={Colors.progressColor}
+          width="45%"
+          onPress={() => {
+            setStep(RequestCreation.step.addLine);
+          }}
+        />
+        <Button
+          title={I18n.t('Base_Realize')}
+          iconName="check-lg"
+          width="45%"
+          onPress={handleRealizePress}
+        />
+      </View>
+    );
+  }
+
+  return null;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
+
+export default RequestCreationButtons;
---
+++ packages/ui/src/components/molecules/HorizontalRuleText/HorizontalRuleText.tsx
@@ -0,0 +1,78 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useMemo} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {HorizontalRule, Text} from '../../atoms';
+import {useThemeColor} from '../../../theme';
+
+interface HorizontalRuleTextProps {
+  style?: any;
+  textStyle?: any;
+  lineStyle?: any;
+  text: string;
+  color?: string;
+}
+
+const HorizontalRuleText = ({
+  style,
+  textStyle,
+  lineStyle,
+  text,
+  color: _color,
+}: HorizontalRuleTextProps) => {
+  const Colors = useThemeColor();
+
+  const color = useMemo(
+    () => _color ?? Colors.secondaryColor.background_light,
+    [_color, Colors],
+  );
+
+  const renderLine = useCallback(() => {
+    return (
+      <HorizontalRule style={[styles.line, lineStyle, {borderColor: color}]} />
+    );
+  }, [color, lineStyle]);
+
+  return (
+    <View style={[styles.container, style]}>
+      {renderLine()}
+      <Text style={[styles.text, textStyle]} fontSize={14} textColor={color}>
+        {text}
+      </Text>
+      {renderLine()}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 2,
+  },
+  line: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
+
+export default HorizontalRuleText;
