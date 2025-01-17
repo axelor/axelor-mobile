df12b0a47122296b953ceaaaac2a808c7df08093
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-01-17 16:58:41 +0100

    feat: improve management of root element creation (#847)
    * RM#88573

--- packages/apps/dms/src/components/organisms/ParentDirectorySearchBar/ParentDirectorySearchBar.tsx
+++ packages/apps/dms/src/components/organisms/ParentDirectorySearchBar/ParentDirectorySearchBar.tsx
@@ -16,9 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback} from 'react';
+import {StyleSheet} from 'react-native';
 import {useDispatch, useSelector, useTranslator} from '@axelor/aos-mobile-core';
-import {AutoCompleteSearch} from '@axelor/aos-mobile-ui';
+import {AutoCompleteSearch, Label} from '@axelor/aos-mobile-ui';
 import {searchDirectory} from '../../../features/documentSlice';
 
 interface ParentDirectorySearchBarProps {
@@ -28,6 +29,7 @@ interface ParentDirectorySearchBarProps {
   onChange?: (any: any) => void;
   readonly?: boolean;
   required?: boolean;
+  displayRootInfo?: boolean;
 }
 
 const ParentDirectorySearchBar = ({
@@ -37,29 +39,17 @@ const ParentDirectorySearchBar = ({
   onChange = () => {},
   readonly = false,
   required = false,
+  displayRootInfo = false,
 }: ParentDirectorySearchBarProps) => {
   const I18n = useTranslator();
   const dispatch = useDispatch();
 
-  const {user} = useSelector(state => state.user);
-  const {mobileSettings} = useSelector(state => state.appConfig);
   const {
     loadingDirectory,
     moreLoadingDirectory,
     isListEndDirectory,
     directoryList,
   } = useSelector(state => state.dms_document);
-
-  const extendedDirectoryList = useMemo(
-    () => [
-      {
-        ...(user.dmsRoot ?? mobileSettings?.defaultDmsRoot),
-        fileName: I18n.t('Dms_Root'),
-      },
-      ...directoryList,
-    ],
-    [I18n, directoryList, mobileSettings?.defaultDmsRoot, user.dmsRoot],
-  );
 
   const searchParentDirectoryAPI = useCallback(
     ({searchValue, page = 0}) => {
@@ -69,26 +59,42 @@ const ParentDirectorySearchBar = ({
   );
 
   return (
-    <AutoCompleteSearch
-      style={style}
-      title={I18n.t(title)}
-      objectList={extendedDirectoryList}
-      value={defaultValue}
-      required={required}
-      readonly={readonly}
-      onChangeValue={onChange}
-      fetchData={searchParentDirectoryAPI}
-      displayValue={item => item.fileName}
-      placeholder={I18n.t(title)}
-      showDetailsPopup={true}
-      loadingList={loadingDirectory}
-      moreLoading={moreLoadingDirectory}
-      isListEnd={isListEndDirectory}
-      navigate={false}
-      oneFilter={false}
-      translator={I18n.t}
-    />
+    <>
+      {displayRootInfo && defaultValue == null && (
+        <Label
+          style={styles.label}
+          type="info"
+          message={I18n.t('Dms_SearchBarRootInfo')}
+        />
+      )}
+      <AutoCompleteSearch
+        style={style}
+        title={I18n.t(title)}
+        objectList={directoryList}
+        value={defaultValue}
+        required={required}
+        readonly={readonly}
+        onChangeValue={onChange}
+        fetchData={searchParentDirectoryAPI}
+        displayValue={item => item.fileName}
+        placeholder={I18n.t(title)}
+        showDetailsPopup={true}
+        loadingList={loadingDirectory}
+        moreLoading={moreLoadingDirectory}
+        isListEnd={isListEndDirectory}
+        navigate={false}
+        oneFilter={false}
+        translator={I18n.t}
+      />
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    width: '90%',
+    alignSelf: 'center',
+  },
+});
 
 export default ParentDirectorySearchBar;
