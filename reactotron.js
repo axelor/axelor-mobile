430fffb13c87a8a325968eb71c854089ce9ca318
Gregory <117281520+gca-axelor@users.noreply.github.com>
2025-01-27 10:02:50 +0100

    fix: add license

--- reactotron.js
+++ reactotron.js
@@ -1,3 +1,21 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const {exec} = require('child_process');
 const os = require('os');
 
@@ -37,7 +55,6 @@ const setupAdbReverse = () => {
   });
 };
 
-// Vérifie si adb est installé
 exec('command -v adb', err => {
   if (err) {
     installAdb();
