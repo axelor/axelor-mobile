796d05784049eb3ae8fd809b4df514c7e1f22451
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-02-21 17:10:21 +0100

    fix: error when onPeriodErrorChange not provided on PeriodInput (#897)
    * RM#91359

--- packages/core/src/components/templates/PeriodInput/PeriodInput.tsx
+++ packages/core/src/components/templates/PeriodInput/PeriodInput.tsx
@@ -77,8 +77,8 @@ const PeriodInput = ({
       endDateConfig.dateInputMode === 'date' ? getEndOfDay(endDate) : endDate;
 
     const isError = startDate && endDate && _startDate > _endDate;
-    setIsPeriodError(isError);
-    onPeriodErrorChange(isError);
+    setIsPeriodError(!!isError);
+    onPeriodErrorChange?.(!!isError);
   }, [
     startDate,
     endDate,
