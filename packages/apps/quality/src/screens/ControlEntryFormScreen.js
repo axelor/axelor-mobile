5a105a2be62c2e1f1b950af9f01726fe847e1d64
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-02-25 11:47:06 +0100

    fix: issue on control entry completion (#899)
    * RM#90996

--- packages/apps/quality/src/screens/ControlEntryFormScreen.js
+++ packages/apps/quality/src/screens/ControlEntryFormScreen.js
@@ -320,6 +320,7 @@ const ControlEntryFormScreen = ({navigation, route}) => {
             },
           ]}
           readonly={isReadonly}
+          key={currentIndex}
         />
       )}
     </Screen>
