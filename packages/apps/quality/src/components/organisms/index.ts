c9c53ebf0b2e02ea30b4a7cca9bc161ff993cefe
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-02-26 15:54:34 +0100

    fix: add null checks to avoid crash on Control entry line (#904)
    * RM#91004

--- packages/apps/quality/src/components/organisms/index.ts
+++ packages/apps/quality/src/components/organisms/index.ts
@@ -16,6 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from './atoms';
-export * from './molecules';
-export * from './organisms';
+export {default as SampleNotFoundView} from './SampleNotFoundView/SampleNotFoundView';
