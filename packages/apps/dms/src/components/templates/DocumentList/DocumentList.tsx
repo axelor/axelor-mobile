6f62ddc1da05459821931f92b8ff70ea32dba40d
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2025-03-11 11:01:46 +0100

    fix: add showClearButton option on DMS chipSelect (#922)
    * RM#92153

--- packages/apps/dms/src/components/templates/DocumentList/DocumentList.tsx
+++ packages/apps/dms/src/components/templates/DocumentList/DocumentList.tsx
@@ -179,6 +179,7 @@ const DocumentList = ({
             mode="multi"
             selectionItems={File.getFileExtensionList(Colors)}
             onChangeValue={setSelectedExtensions}
+            showClearButton
           />
         }
         displayBreadcrumb
