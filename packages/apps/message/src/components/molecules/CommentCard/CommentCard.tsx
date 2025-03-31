6c577e76ab161e1f0384237d6faeddd079cae146
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-03-31 14:16:43 +0200

    refactor: improve mail message files (#946)
    * RM#92809

---
+++ packages/apps/message/src/components/molecules/CommentCard/CommentCard.tsx
@@ -0,0 +1,110 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {useNavigation, useTranslator} from '@axelor/aos-mobile-core';
+import {
+  ActionCard,
+  Card,
+  LabelText,
+  useThemeColor,
+} from '@axelor/aos-mobile-ui';
+import {ExpandableText, ReadIcon} from '../../atoms';
+
+interface CommentCardProps {
+  relatedModel: string;
+  relatedId: number;
+  subject?: string;
+  files: any[];
+  value: string;
+  flags?: any;
+  style?: any;
+  navigation?: any;
+}
+
+const CommentCard = ({
+  relatedModel,
+  relatedId,
+  subject,
+  files,
+  value,
+  flags,
+  style,
+}: CommentCardProps) => {
+  const Colors = useThemeColor();
+  const I18n = useTranslator();
+  const navigation = useNavigation();
+
+  const actionList = useMemo(() => {
+    const _actions = [];
+
+    if (Array.isArray(files) && files.length > 0) {
+      _actions.push({
+        iconName: 'paperclip',
+        onPress: () =>
+          navigation.navigate('MailMessageAttachedFilesScreen', {
+            files,
+          }),
+      });
+    }
+
+    return _actions;
+  }, [files, navigation]);
+
+  return (
+    <ActionCard style={style} actionList={actionList} translator={I18n.t}>
+      <Card style={styles.container}>
+        <View style={styles.headerContainer}>
+          <LabelText
+            iconName="chat-fill"
+            size={18}
+            value={subject || I18n.t('Base_Comment')}
+            style={styles.flexOne}
+            textSize={16}
+            color={Colors.primaryColor.background}
+          />
+          {flags != null && (
+            <ReadIcon
+              mailMessageFlag={flags}
+              model={relatedModel}
+              modelId={relatedId}
+            />
+          )}
+        </View>
+        <ExpandableText value={value} />
+      </Card>
+    </ActionCard>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    paddingRight: 15,
+    paddingVertical: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+  },
+  flexOne: {
+    flex: 1,
+  },
+});
+
+export default CommentCard;
---
+++ packages/apps/message/src/hooks/use-message-header-actions.tsx
@@ -0,0 +1,111 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useCallback, useEffect} from 'react';
+import {
+  headerActionsProvider,
+  useDispatch,
+  useNavigation,
+  useSelector,
+  useTranslator,
+} from '@axelor/aos-mobile-core';
+import {useThemeColor} from '@axelor/aos-mobile-ui';
+import {getAction} from '../utils';
+import {
+  countUnreadMailMessages,
+  getModelSubscribers,
+  markAllMailMessageAsRead,
+} from '../features/mailMessageSlice';
+import {UnsubscribeIcon} from '../components';
+import {useIsSubscribed} from './use-is-subscribed';
+
+export const useMessageHeaders = () => {
+  useMailMessagesGenericAction();
+  useMailMessagesDetailsAction();
+};
+
+const useMailMessagesGenericAction = () => {
+  const I18n = useTranslator();
+  const navigation = useNavigation();
+
+  const {mobileSettings} = useSelector((state: any) => state.appConfig);
+
+  useEffect(() => {
+    headerActionsProvider.registerGenericAction(
+      'message_mailMessages',
+      async ({model, modelId}) =>
+        await getAction({
+          model,
+          modelId,
+          navigation,
+          hideIf: !mobileSettings?.isTrackerMessageEnabled,
+          translator: I18n.t,
+        }),
+    );
+  }, [I18n.t, mobileSettings, navigation, I18n]);
+};
+
+const useMailMessagesDetailsAction = () => {
+  const I18n = useTranslator();
+  const Colors = useThemeColor();
+  const isSubscribed = useIsSubscribed();
+  const dispatch = useDispatch();
+
+  const {unreadMessages, model, modelId} = useSelector(
+    state => state.mailMessages,
+  );
+
+  useEffect(() => {
+    dispatch((getModelSubscribers as any)({model, modelId}));
+    dispatch((countUnreadMailMessages as any)({model, modelId}));
+  }, [dispatch, model, modelId]);
+
+  const handleMarkAllAsRead = useCallback(() => {
+    dispatch((markAllMailMessageAsRead as any)({modelId, model}));
+  }, [dispatch, model, modelId]);
+
+  useEffect(() => {
+    headerActionsProvider.registerModel('message_mailMessage_details', {
+      actions: [
+        {
+          key: 'readMessages',
+          order: 10,
+          showInHeader: true,
+          iconName: 'check-all',
+          iconColor:
+            unreadMessages === 0
+              ? Colors.primaryColor.background
+              : Colors.secondaryColor.background,
+          title: I18n.t('Message_MarkAllAsRead'),
+          onPress: handleMarkAllAsRead,
+        },
+        {
+          key: 'subscribe',
+          order: 20,
+          showInHeader: true,
+          iconName: 'star-fill',
+          title: I18n.t(
+            isSubscribed ? 'Message_Unsubscribe' : 'Message_Subscribe',
+          ),
+          onPress: () => {},
+          customComponent: <UnsubscribeIcon />,
+        },
+      ],
+    });
+  }, [Colors, I18n, handleMarkAllAsRead, isSubscribed, unreadMessages]);
+};
