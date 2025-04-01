ce9600ebfc06b86b4ecbce67be316ebd320968f0
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2025-04-01 14:01:09 +0200

    feat: add FilterContainer component (#931)
    * RM#92032

--- packages/core/src/header/FilterProvider.tsx
+++ packages/core/src/header/FilterProvider.tsx
@@ -16,8 +16,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export interface Filter {
+  id: string;
+  name: string;
+}
+
 class FilterProvider {
-  private activeFilter: string = null;
+  private activeFilter: Filter = null;
   private listeners: Function[] = [];
 
   register(callback: Function) {
@@ -32,7 +37,7 @@ class FilterProvider {
     this.listeners.forEach(callback => callback(this.activeFilter));
   }
 
-  setActiveFilter(filter?: string) {
+  setActiveFilter(filter?: Filter) {
     this.activeFilter = filter;
     this.notify();
   }
