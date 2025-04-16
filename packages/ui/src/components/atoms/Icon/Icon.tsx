f9ab42ee7679a632ecca8f0de7f6b2e223ae0dc9
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-04-16 15:47:05 +0200

    refactor: remove font awesome support (#973)
    * RM#93869

--- packages/ui/src/components/atoms/Icon/Icon.tsx
+++ packages/ui/src/components/atoms/Icon/Icon.tsx
@@ -16,24 +16,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useMemo, useCallback} from 'react';
-import {Platform, StyleSheet, TouchableOpacity} from 'react-native';
-import Icon5 from 'react-native-vector-icons/FontAwesome5';
-import Icon4 from 'react-native-vector-icons/FontAwesome';
+import React, {useMemo} from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {useThemeColor} from '../../../theme';
 import BootstrapIcon from '../BootstrapIcon/BootstrapIcon';
 
 interface IconProps {
   style?: any;
   name: string;
-  /**
-   * @deprecated The prop should not be used anymore. FontAwesome icons will be removed in next version.
-   */
-  isFontAwesome4?: boolean;
-  /**
-   * @deprecated The prop should not be used anymore. FontAwesome icons will be removed in next version.
-   */
-  isFontAwesome5?: boolean;
   color?: string;
   size?: number;
   touchable?: boolean;
@@ -44,8 +34,6 @@ interface IconProps {
 const Icon = ({
   style,
   name,
-  isFontAwesome4 = false,
-  isFontAwesome5 = false,
   color,
   size = 18,
   touchable = false,
@@ -54,22 +42,10 @@ const Icon = ({
 }: IconProps) => {
   const Colors = useThemeColor();
 
-  const styles = useMemo(() => {
-    return getStyles(
-      color == null ? Colors.secondaryColor_dark.background : color,
-      size,
-    );
-  }, [Colors, color, size]);
-
-  const renderIcon = useCallback(() => {
-    if (isFontAwesome5 && Platform.OS !== 'web') {
-      return <Icon5 name={name} style={styles.icon} />;
-    } else if (isFontAwesome4) {
-      return <Icon4 name={name} style={styles.icon} />;
-    } else {
-      return <BootstrapIcon name={name} size={size} color={color} />;
-    }
-  }, [color, isFontAwesome4, isFontAwesome5, name, size, styles]);
+  const _color = useMemo(
+    () => color ?? Colors.secondaryColor_dark.background,
+    [Colors.secondaryColor_dark.background, color],
+  );
 
   if (!visible) {
     return null;
@@ -80,22 +56,17 @@ const Icon = ({
       style={[styles.container, style]}
       onPress={onPress}
       disabled={!touchable}>
-      {renderIcon()}
+      <BootstrapIcon name={name} size={size} color={_color} />
     </TouchableOpacity>
   );
 };
 
-const getStyles = (color, size) =>
-  StyleSheet.create({
-    icon: {
-      color: color,
-      fontSize: size,
-    },
-    container: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Icon;
