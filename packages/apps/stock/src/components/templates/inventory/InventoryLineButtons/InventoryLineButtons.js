ac2e48797807f7c1de2e13dfad0721f0e360ec3c
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-05-12 15:00:05 +0200

    fix: remove duplicated screen name on inventory line navigation (#992)
    * RM#94850

--- packages/apps/stock/src/components/templates/inventory/InventoryLineButtons/InventoryLineButtons.js
+++ packages/apps/stock/src/components/templates/inventory/InventoryLineButtons/InventoryLineButtons.js
@@ -51,7 +51,7 @@ const InventoryLineButtons = ({
     if (isScreenMounted('InventoryLineListScreen')) {
       navigation.navigate('InventoryLineListScreen', {inventory});
     } else {
-      navigation.navigate('InventoryLineListScreen', {
+      navigation.navigate('InventoryStartedDetailsScreen', {
         inventoryId: inventory?.id,
       });
     }
