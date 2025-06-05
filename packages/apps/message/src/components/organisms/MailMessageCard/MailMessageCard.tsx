9463bbed5a33f3e5348c8d5b2d894a6332fd195b
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2025-06-05 12:31:19 +0200

    feat: enable user to respond to a message in inbox view (#989)
    * RM#92653

--- packages/apps/message/src/components/organisms/MailMessageCard/MailMessageCard.tsx
+++ packages/apps/message/src/components/organisms/MailMessageCard/MailMessageCard.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {
   ActivityIndicator,
   StyleSheet,
@@ -24,9 +24,15 @@ import {
   View,
 } from 'react-native';
 import {formatDateTime, useTranslator} from '@axelor/aos-mobile-core';
-import {Icon, Text, useThemeColor} from '@axelor/aos-mobile-ui';
+import {
+  Icon,
+  OUTSIDE_INDICATOR,
+  Text,
+  useClickOutside,
+  useThemeColor,
+} from '@axelor/aos-mobile-ui';
 import {Avatar, AVATAR_SIZE, AVATAR_PADDING} from '../../atoms';
-import {CommentCard, NotificationCard} from '../../molecules';
+import {CommentCard, NotificationCard, SendMessageBox} from '../../molecules';
 import {fetchRepliesApi} from '../../../api';
 import {MailMessageType} from '../../../types';
 
@@ -47,6 +53,7 @@ interface MailMessageCardProps {
   relatedModel: string;
   isInbox?: boolean;
   numReplies?: number;
+  getParentReplies?: (newMessage?: any) => void;
 }
 
 const MailMessageCard = ({
@@ -66,17 +73,33 @@ const MailMessageCard = ({
   relatedModel,
   isInbox,
   numReplies,
+  getParentReplies,
 }: MailMessageCardProps) => {
   const I18n = useTranslator();
   const Colors = useThemeColor();
 
   const [areRepliesVisible, setAreRepliesVisible] = useState(false);
   const [replies, setReplies] = useState([]);
+  const [numberReplies, setNumberReplies] = useState(0);
+  const [isMessageBoxVisible, setIsMessageBoxVisible] = useState(false);
 
   const getReplies = useCallback(
-    () =>
+    (newMessage?: any) =>
       fetchRepliesApi({messageId})
-        .then(res => setReplies(res?.data?.data))
+        .then(res => {
+          const _replies = res?.data?.data;
+          setAreRepliesVisible(current => {
+            if (current || newMessage == null) {
+              setReplies(_replies);
+            } else {
+              const newReply = _replies.find(msg => msg.id === newMessage.id);
+              setReplies([newReply]);
+            }
+            setNumberReplies(_replies.length);
+
+            return true;
+          });
+        })
         .catch(() => setReplies([])),
     [messageId],
   );
@@ -86,30 +109,43 @@ const MailMessageCard = ({
     [Colors.secondaryColor.background],
   );
 
+  const wrapperRef = useRef(null);
+  const clickOutside = useClickOutside({
+    wrapperRef,
+  });
+
+  useEffect(() => {
+    if (isMessageBoxVisible && clickOutside === OUTSIDE_INDICATOR) {
+      setIsMessageBoxVisible(false);
+    }
+  }, [clickOutside, isMessageBoxVisible]);
+
   return (
     <View style={[styles.container, style]}>
       <View>
-        <Avatar avatar={avatar} />
-        {numReplies > 0 && (
-          <>
-            <TouchableOpacity
-              style={styles.displayRepliesContainer}
-              activeOpacity={0.9}
-              onPress={() => {
-                getReplies();
-                setAreRepliesVisible(current => !current);
-              }}>
-              <Text textColor={Colors.secondaryColor_dark.background}>
-                {numReplies}
-              </Text>
-              <Icon name="chat-dots" />
-            </TouchableOpacity>
-            {areRepliesVisible && <View style={styles.verticalRule} />}
-          </>
+        <Avatar style={styles.avatar} avatar={avatar} />
+        {(numberReplies > 0 || numReplies > 0) && (
+          <TouchableOpacity
+            style={styles.displayRepliesContainer}
+            activeOpacity={0.9}
+            onPress={() => {
+              setAreRepliesVisible(current => {
+                if (!current) {
+                  getReplies();
+                }
+                return !current;
+              });
+            }}>
+            <Text textColor={Colors.secondaryColor_dark.background}>
+              {numberReplies > 0 ? numberReplies : numReplies}
+            </Text>
+            <Icon name="chat-dots" />
+          </TouchableOpacity>
         )}
+        {areRepliesVisible && <View style={styles.verticalRule} />}
       </View>
       <View style={styles.flexOne}>
-        <View>
+        <View style={styles.flexOne}>
           <Text style={styles.author} fontSize={12}>
             {`${author} ${eventText} - ${formatDateTime(
               eventTime,
@@ -141,9 +177,34 @@ const MailMessageCard = ({
               isInbox={isInbox}
             />
           )}
+          {isInbox && !isMessageBoxVisible && (
+            <TouchableOpacity
+              style={styles.replyContainer}
+              activeOpacity={0.9}
+              onPress={() => setIsMessageBoxVisible(true)}>
+              <Icon name="arrow-90deg-left" size={12} />
+              <Text style={styles.replyText} fontSize={12}>
+                {I18n.t('Message_Respond')}
+              </Text>
+            </TouchableOpacity>
+          )}
+          <SendMessageBox
+            style={styles.messageBox}
+            hideMessageBox={!isMessageBoxVisible}
+            model={relatedModel}
+            modelId={relatedId}
+            parentId={messageId}
+            onSend={message => {
+              getParentReplies
+                ? getParentReplies(message)
+                : getReplies(message);
+              setIsMessageBoxVisible(false);
+            }}
+            wrapperRef={wrapperRef}
+          />
         </View>
         {areRepliesVisible &&
-          (replies.length === 0 ? (
+          (numberReplies === 0 ? (
             <ActivityIndicator
               size="large"
               color={Colors.inverseColor.background}
@@ -152,6 +213,7 @@ const MailMessageCard = ({
             replies.map(item => (
               <MailMessageCard
                 style={styles.replyCard}
+                messageId={item.id}
                 author={item.$author?.fullName}
                 avatar={item.$avatar}
                 body={item.body}
@@ -164,7 +226,8 @@ const MailMessageCard = ({
                 flags={item.$flags}
                 relatedId={item.relatedId}
                 relatedModel={item.relatedModel}
-                isInbox
+                isInbox={isInbox}
+                getParentReplies={getReplies}
                 key={item.id}
               />
             ))
@@ -178,18 +241,22 @@ const getStyles = (verticalRuleColor: string) =>
   StyleSheet.create({
     container: {
       flexDirection: 'row',
-      marginBottom: 10,
+      marginTop: 10,
+    },
+    avatar: {
+      paddingBottom: 5,
     },
     displayRepliesContainer: {
       flexDirection: 'row',
       justifyContent: 'center',
       gap: 5,
+      paddingVertical: 5,
     },
     verticalRule: {
       flex: 1,
       alignSelf: 'center',
       width: 1,
-      marginTop: 10,
+      marginTop: 5,
       backgroundColor: verticalRuleColor,
     },
     flexOne: {
@@ -201,8 +268,21 @@ const getStyles = (verticalRuleColor: string) =>
     card: {
       flex: 1,
       width: '100%',
-      marginVertical: 2,
+      marginTop: 2,
       paddingHorizontal: 5,
+    },
+    replyContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      gap: 5,
+      paddingTop: 3,
+      paddingHorizontal: 10,
+    },
+    replyText: {
+      textDecorationLine: 'underline',
+    },
+    messageBox: {
+      width: null,
     },
     replyCard: {
       marginLeft: -((AVATAR_SIZE + AVATAR_PADDING) / 2),
