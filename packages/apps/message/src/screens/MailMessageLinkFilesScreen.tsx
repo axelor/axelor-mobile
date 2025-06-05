1d61ec5593a9847cb975b5345b1b2ae617ad5fa8
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-06-05 15:14:12 +0200

    refactor: improve document view

--- packages/apps/message/src/screens/MailMessageLinkFilesScreen.tsx
+++ packages/apps/message/src/screens/MailMessageLinkFilesScreen.tsx
@@ -16,100 +16,89 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {useDispatch, useSelector, useTranslator} from '@axelor/aos-mobile-core';
-import {
-  Button,
-  Icon,
-  Text,
-  ThemeColors,
-  useThemeColor,
-} from '@axelor/aos-mobile-ui';
+import {Button, Icon, Screen, Text} from '@axelor/aos-mobile-ui';
 import {DocumentList} from '@axelor/aos-mobile-dms';
 import {saveLinkFiles} from '../features/mailMessageSlice';
 
 const MailMessageLinkFilesScreen = ({navigation}) => {
   const I18n = useTranslator();
-  const Colors = useThemeColor();
   const dispatch = useDispatch();
 
   const {linkFiles: _linkFiles} = useSelector(state => state.mailMessages);
 
-  const [linkFiles, setLinkFiles] = useState(_linkFiles);
+  const [linkFiles, setLinkFiles] = useState<any[]>(_linkFiles);
 
-  const styles = useMemo(() => getStyles(Colors), [Colors]);
-
-  const handleLinkFiles = (file: any) => {
+  const handleLinkFiles = useCallback((file: any) => {
     setLinkFiles(prevFiles => {
-      const exists = prevFiles.some(f => f.id === file.id);
-      if (exists) {
-        return prevFiles.filter(f => f.id !== file.id);
+      if (prevFiles.some(({id}) => id === file.id)) {
+        return prevFiles.filter(({id}) => id !== file.id);
       } else {
         return [...prevFiles, file];
       }
     });
-  };
+  }, []);
+
+  const renderFile = useCallback(
+    (file: any, idx: number) => {
+      return (
+        <TouchableOpacity
+          style={styles.linkFile}
+          onPress={() => handleLinkFiles(file)}
+          key={idx}>
+          <Icon name="x-lg" />
+          <Text style={styles.text} numberOfLines={1}>
+            {file.fileName}
+          </Text>
+        </TouchableOpacity>
+      );
+    },
+    [handleLinkFiles],
+  );
 
   return (
-    <View style={styles.container}>
+    <Screen
+      removeSpaceOnTop
+      fixedItems={
+        linkFiles.length > 0 && (
+          <View style={styles.linkFilesContainer}>
+            {linkFiles.map(renderFile)}
+            <Button
+              width="100%"
+              title={I18n.t('Base_Validate')}
+              onPress={() => {
+                dispatch(saveLinkFiles(linkFiles));
+                navigation.goBack();
+              }}
+            />
+          </View>
+        )
+      }>
       <DocumentList hideActions customOnPress={handleLinkFiles} />
-      {linkFiles.length > 0 && (
-        <View style={styles.linkFilesContainer}>
-          {linkFiles.map((file, index) => (
-            <TouchableOpacity
-              style={styles.linkFile}
-              onPress={() => handleLinkFiles(file)}
-              key={index}>
-              <Icon name="x-lg" />
-              <Text style={styles.text} numberOfLines={1}>
-                {file.fileName}
-              </Text>
-            </TouchableOpacity>
-          ))}
-          <Button
-            style={styles.button}
-            title={I18n.t('Base_Validate')}
-            onPress={() => {
-              dispatch(saveLinkFiles(linkFiles));
-              navigation.goBack();
-            }}
-          />
-        </View>
-      )}
-    </View>
+    </Screen>
   );
 };
 
-const getStyles = (Colors: ThemeColors) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.screenBackgroundColor,
-    },
-    linkFilesContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      paddingVertical: 10,
-      paddingHorizontal: 24,
-      gap: 10,
-      borderTopLeftRadius: 13,
-      borderTopRightRadius: 13,
-      backgroundColor: Colors.backgroundColor,
-    },
-    linkFile: {
-      flexShrink: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    text: {
-      flexShrink: 1,
-      marginLeft: 3,
-      textDecorationLine: 'underline',
-    },
-    button: {
-      width: '100%',
-    },
-  });
+const styles = StyleSheet.create({
+  linkFilesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingVertical: 2,
+    paddingHorizontal: 15,
+    gap: 5,
+  },
+  linkFile: {
+    flexShrink: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+  },
+  text: {
+    flexShrink: 1,
+    textDecorationLine: 'underline',
+  },
+});
 
 export default MailMessageLinkFilesScreen;
