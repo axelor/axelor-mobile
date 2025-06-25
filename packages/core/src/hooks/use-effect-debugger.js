b07774d9de18a2dd693bef9d487a53e7229954ee
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-06-25 16:48:10 +0200

    chore: add other debugger hooks

--- packages/core/src/hooks/use-effect-debugger.js
+++ packages/core/src/hooks/use-effect-debugger.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {useEffect, useRef} from 'react';
+import {useCallback, useEffect, useMemo, useRef} from 'react';
 
 const usePrevious = (value, initialValue) => {
   const ref = useRef(initialValue);
@@ -54,4 +54,64 @@ export const useEffectDebugger = (
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effectHook, dependencies);
+};
+
+export const useMemoDebugger = (
+  memoHook,
+  dependencies,
+  dependencyNames = [],
+) => {
+  const previousDeps = usePrevious(dependencies, []);
+
+  const changedDeps = dependencies.reduce((accum, dependency, index) => {
+    if (dependency !== previousDeps[index]) {
+      const keyName = dependencyNames[index] || index;
+      return {
+        ...accum,
+        [keyName]: {
+          before: previousDeps[index],
+          after: dependency,
+        },
+      };
+    }
+
+    return accum;
+  }, {});
+
+  if (Object.keys(changedDeps).length) {
+    console.log('[use-memo-debugger] ', changedDeps);
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(memoHook, dependencies);
+};
+
+export const useCallbackDebugger = (
+  callbackHook,
+  dependencies,
+  dependencyNames = [],
+) => {
+  const previousDeps = usePrevious(dependencies, []);
+
+  const changedDeps = dependencies.reduce((accum, dependency, index) => {
+    if (dependency !== previousDeps[index]) {
+      const keyName = dependencyNames[index] || index;
+      return {
+        ...accum,
+        [keyName]: {
+          before: previousDeps[index],
+          after: dependency,
+        },
+      };
+    }
+
+    return accum;
+  }, {});
+
+  if (Object.keys(changedDeps).length) {
+    console.log('[use-callback-debugger] ', changedDeps);
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useCallback(callbackHook, dependencies);
 };
