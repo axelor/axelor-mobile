898b13c8df92e7e1095b9f4e29dbb2d7ad4f2f96
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-07-16 11:37:12 +0200

    refactor: improve config fetch management (#1037)
    * RM#98130

--- packages/core/src/features/asyncFunctions-index.ts
+++ packages/core/src/features/asyncFunctions-index.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {fetchRequiredConfig} from './appConfigSlice';
+export {fetchRequiredConfig, resetConfigs} from './appConfigSlice';
 export {
   clearError,
   isUrlValid,
