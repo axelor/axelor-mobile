cc447c3deae93d05f089526656a201602fee2acc
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2025-08-06 15:44:33 +0200

    feat: add freight carrier information on customer delivery (#1054)
    * RM#97538

---
+++ packages/apps/stock/src/components/organisms/CustomerDeliveryDropdownCard/CustomerDeliveryDropdownCard.tsx
@@ -0,0 +1,43 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {DropdownCardSwitch} from '@axelor/aos-mobile-ui';
+import {useSelector, useTranslator} from '@axelor/aos-mobile-core';
+import {FreightCarrierContent} from '../../atoms';
+
+const CustomerDeliveryDropdownCard = () => {
+  const I18n = useTranslator();
+
+  const {customerDelivery} = useSelector(state => state.customerDelivery);
+
+  const dropdownItems = useMemo(
+    () => [
+      {
+        key: 1,
+        title: I18n.t('Stock_FreightCarrier'),
+        childrenComp: <FreightCarrierContent {...customerDelivery} />,
+      },
+    ],
+    [I18n, customerDelivery],
+  );
+
+  return <DropdownCardSwitch dropdownItems={dropdownItems} />;
+};
+
+export default CustomerDeliveryDropdownCard;
