982591b40c7f265eff2cc8000bd23adac5e71560
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-08-21 16:23:34 +0200

    feat: add status filter on event planning view (#1070)
    * RM#97181

---
+++ packages/apps/crm/src/components/atoms/EventStatusMultiPicker/EventStatusMultiPicker.tsx
@@ -0,0 +1,62 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2025 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, {useMemo} from 'react';
+import {useTranslator, useTypeHelpers, useTypes} from '@axelor/aos-mobile-core';
+import {MultiValuePicker} from '@axelor/aos-mobile-ui';
+
+interface EventStatusMultiPickerProps {
+  style?: any;
+  title?: string;
+  onChange: (item: any) => void;
+  readonly?: boolean;
+  required?: boolean;
+  showTitle?: boolean;
+}
+
+const EventStatusMultiPicker = ({
+  style,
+  title = 'Crm_Status',
+  onChange,
+  readonly = false,
+  required = false,
+  showTitle = false,
+}: EventStatusMultiPickerProps) => {
+  const I18n = useTranslator();
+  const {Event} = useTypes();
+  const {getSelectionItems} = useTypeHelpers();
+
+  const eventCategoryList = useMemo(
+    () => getSelectionItems(Event?.statusSelect),
+    [Event?.statusSelect, getSelectionItems],
+  );
+
+  return (
+    <MultiValuePicker
+      style={style}
+      title={showTitle && I18n.t(title)}
+      placeholder={I18n.t(title)}
+      listItems={eventCategoryList}
+      onValueChange={onChange}
+      required={required}
+      readonly={readonly}
+    />
+  );
+};
+
+export default EventStatusMultiPicker;
