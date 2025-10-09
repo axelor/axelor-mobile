394c64a1b9310db848497b9f1a82610741124aa1
gca-axelor <117281520+gca-axelor@users.noreply.github.com>
2025-10-09 09:53:47 +0200

    refactor: simplify body content to avoid permission issue in Helpdesk (#1099)
    * RM#101893

--- packages/apps/helpdesk/src/api/ticket-api.js
+++ packages/apps/helpdesk/src/api/ticket-api.js
@@ -192,13 +192,13 @@ export async function updateStatusTicket({
 }
 
 export async function updateTicket({ticket}) {
-  const {matchers} = formatRequestBody(ticket, 'data');
+  const {matchers, formattedData} = formatRequestBody(ticket, 'data');
 
   return getActionApi().send({
     url: '/ws/rest/com.axelor.apps.helpdesk.db.Ticket',
     method: 'post',
     body: {
-      data: ticket,
+      data: formattedData,
     },
     description: 'update ticket',
     matchers: {
@@ -210,13 +210,13 @@ export async function updateTicket({ticket}) {
 }
 
 export async function createTicket({ticket}) {
-  const {matchers} = formatRequestBody(ticket, 'data');
+  const {matchers, formattedData} = formatRequestBody(ticket, 'data');
 
   return getActionApi().send({
     url: '/ws/rest/com.axelor.apps.helpdesk.db.Ticket',
     method: 'put',
     body: {
-      data: ticket,
+      data: formattedData,
     },
     description: 'create ticket',
     matchers: {
