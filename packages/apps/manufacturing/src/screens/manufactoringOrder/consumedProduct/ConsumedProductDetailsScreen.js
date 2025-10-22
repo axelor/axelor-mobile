6b549e24c9f0c4615bfa8a8c87657096c532b37f
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-10-22 16:57:00 +0200

    fix: issue on tracking number computation when creating a consumed product

--- packages/apps/manufacturing/src/screens/manufactoringOrder/consumedProduct/ConsumedProductDetailsScreen.js
+++ packages/apps/manufacturing/src/screens/manufactoringOrder/consumedProduct/ConsumedProductDetailsScreen.js
@@ -61,6 +61,7 @@ const ConsumedProductDetailsScreen = ({route, navigation}) => {
 
   const trackingNumber = useMemo(() => {
     if (
+      consumedProdProduct &&
       consumedProdProduct?.stockMoveLineId === consumedProductStockMoveLine?.id
     ) {
       return consumedProductStockMoveLine.trackingNumber;
