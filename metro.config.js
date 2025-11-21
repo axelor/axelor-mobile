8875147426c5d3cb51092a8f7e2f6aedbe21be6a
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-11-21 13:50:22 +0100

    chore: upgrade React Native to 0.81 (#1154)
    * RM#103923

--- metro.config.js
+++ metro.config.js
@@ -1,34 +1,11 @@
-const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const moduleRoot = path.resolve(__dirname, '../');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://metrobundler.dev/docs/configuration
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  watchFolders: [moduleRoot],
-  resolver: {
-    extraNodeModules: {
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
-    },
-    blockList: exclusionList([
-      new RegExp(`${moduleRoot}/node_modules/react/.*`),
-      new RegExp(`${moduleRoot}/node_modules/react-native/.*`),
-    ]),
-  },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
