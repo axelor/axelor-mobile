d8a607194488fbb304661b9ce06bbfad67ea5eec
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2025-11-21 11:36:28 +0100

    chore: upgrade React Native to 0.80 (#1153)
    * RM#103111

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'prettier'],
   ignorePatterns: ['__tests__/', 'lib/'],
+  rules: {
+    'prettier/prettier': 'off',
+    '@react-native/no-deep-imports': 'off',
+  },
 };
