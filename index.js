ed6e25c63c957a52941054dd22bb44bdcd1f4aa9
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2026-01-19 15:10:44 +0100

    chore: remove reactotron

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
  * @format
  */
 
-if (__DEV__) {
-  require('./ReactotronConfig');
-}
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
