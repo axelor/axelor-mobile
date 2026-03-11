c8849f355c24926d5fa8f0c345ef55d4953481a5
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2026-03-11 16:23:56 +0100

    fix: improve Button style to avoid overlap issue (#1194)
    * RM#109318

--- packages/ui/src/components/molecules/Button/Button.tsx
+++ packages/ui/src/components/molecules/Button/Button.tsx
@@ -36,6 +36,7 @@ export interface ButtonProps {
   onPress?: () => void;
   disabled?: boolean;
   onDisabledPress?: () => void;
+  numberOfLines?: number;
 }
 
 const Button = ({
@@ -51,6 +52,7 @@ const Button = ({
   onPress = () => {},
   disabled = false,
   onDisabledPress = null,
+  numberOfLines = 2,
 }: ButtonProps) => {
   const Colors = useThemeColor();
 
@@ -95,12 +97,14 @@ const Button = ({
           name={iconName}
           size={iconSize}
           color={buttonColor.foreground}
-          style={[styles.icon, styleIcon]}
+          style={styleIcon}
         />
       )}
       {!!title && (
         <Text
           style={[styles.text, styleTxt]}
+          numberOfLines={numberOfLines}
+          adjustsFontSizeToFit
           fontSize={17}
           textColor={buttonColor.foreground}>
           {title}
@@ -116,14 +120,13 @@ const getStyles = (color: Color, width: DimensionValue) =>
       backgroundColor: color.background_light,
       borderColor: color.background,
       width: width,
+      gap: 10,
     },
     text: {
       fontWeight: 'bold',
       textAlign: 'center',
-      marginHorizontal: 5,
-    },
-    icon: {
-      marginHorizontal: 5,
+      textAlignVertical: 'center',
+      flexShrink: 1,
     },
   });
 
