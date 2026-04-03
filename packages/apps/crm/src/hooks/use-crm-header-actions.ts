0f7659b9baacf71509737bc0f7799efafb653fb3
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2026-04-03 12:35:42 +0200

    fix: add AOP filters context on CRM views (#1198)
    * RM#110563

--- packages/apps/crm/src/hooks/use-crm-header-actions.ts
+++ packages/apps/crm/src/hooks/use-crm-header-actions.ts
@@ -42,6 +42,7 @@ const useCatalogListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_catalog_list', {
       model: 'com.axelor.apps.crm.db.Catalog',
+      options: {core_modelFilters: {actionViewName: 'crm.root.catalogs'}},
       actions: [
         {
           key: 'newCatalog',
@@ -69,7 +70,12 @@ const useClientListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_client_list', {
       model: 'com.axelor.apps.base.db.Partner',
-      options: {core_modelFilters: {name: 'partner-filters'}},
+      options: {
+        core_modelFilters: {
+          name: 'partner-filters',
+          actionViewName: 'crm.root.customers',
+        },
+      },
       actions: [
         {
           key: 'client-creationForm',
@@ -146,7 +152,12 @@ const useContactListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_contact_list', {
       model: 'com.axelor.apps.base.db.Partner',
-      options: {core_modelFilters: {name: 'contact-filters'}},
+      options: {
+        core_modelFilters: {
+          name: 'contact-filters',
+          actionViewName: 'crm.root.contacts',
+        },
+      },
       actions: [
         {
           key: 'contact-creationForm',
@@ -224,6 +235,7 @@ const useLeadListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_lead_list', {
       model: 'com.axelor.apps.crm.db.Lead',
+      options: {core_modelFilters: {actionViewName: 'crm.opened.lead'}},
       actions: [
         {
           key: 'newLead',
@@ -297,6 +309,7 @@ const useOpportunityListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_opportunity_list', {
       model: 'com.axelor.apps.crm.db.Opportunity',
+      options: {core_modelFilters: {actionViewName: 'crm.opened.opportunity'}},
       actions: [
         {
           key: 'newOpportunity',
@@ -335,7 +348,12 @@ const useProspectListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_prospect_list', {
       model: 'com.axelor.apps.base.db.Partner',
-      options: {core_modelFilters: {name: 'partner-filters'}},
+      options: {
+        core_modelFilters: {
+          name: 'partner-filters',
+          actionViewName: 'crm.opened.prospects',
+        },
+      },
       actions: [
         {
           key: 'prospect-creationForm',
@@ -406,6 +424,7 @@ const useTourListActions = () => {
   useEffect(() => {
     headerActionsProvider.registerModel('crm_tour_list', {
       model: 'com.axelor.apps.crm.db.Tour',
+      options: {core_modelFilters: {actionViewName: 'crm.root.tour'}},
     });
   }, []);
 };
